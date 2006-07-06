cdf296a3396dd9842f4ed4d8acd5cf489044f52c
Marc Streit <marc.streit@jku.at>
2006-07-06 16:25:51 +0000

    KEGG test
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@2 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ src/cerberus/applications/kegg/test/KEGG_Test.java
@@ -0,0 +1,39 @@
+package cerberus.applications.kegg.test;
+
+import keggapi.*;
+
+class KEGG_Test {
+        public static void main(String[] args) throws Exception {
+                KEGGLocator    locator  = new KEGGLocator();
+                KEGGPortType   serv     = locator.getKEGGPort();
+
+                String         query    = args[0];
+                SSDBRelation[] results  = null;
+
+                results = serv.get_best_neighbors_by_gene(query, 1, 50);
+
+                for (int i = 0; i < results.length; i++) {
+                        String gene1  = results[i].getGenes_id1();
+                        String gene2  = results[i].getGenes_id2();
+                        int    score  = results[i].getSw_score();
+                        System.out.println(gene1 + "\t" + gene2 + "\t" + score);
+                }
+        }
+}
+
+/*import keggapi.*;
+
+class KEGG_Test {
+        public static void main(String[] args) throws Exception {
+                KEGGLocator  locator = new KEGGLocator();
+                KEGGPortType serv = locator.getKEGGPort();
+
+                String   query   = "eco:b1002";
+                String[] results = serv.get_genes_by_pathway(query);
+                
+                System.out.println("here we go...");
+                for (int i = 0; i < results.length; i++) {
+                        System.out.println(results[i]);
+                }
+        }
+}*/
