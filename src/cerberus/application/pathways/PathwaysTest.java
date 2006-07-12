2e9e4d35dc7cff2b2c943cd8446bd063fb56390b
Marc Streit <marc.streit@jku.at>
2006-07-12 14:02:28 +0000

    git-svn-id: svn://localhost:8080/caleydo_main/trunk@12 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ src/cerberus/application/pathways/PathwaysTest.java
@@ -0,0 +1,33 @@
+package cerberus.application.pathways;
+
+import java.io.File;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import cerberus.xml.parser.kgml.KgmlSaxHandler;
+import cerberus.pathways.PathwayGraphBuilder;
+
+public class PathwaysTest {
+	public static void main(String[] args) throws Exception 
+	{
+	  	PathwayGraphBuilder pathwayGraphBuilder = new PathwayGraphBuilder();
+	  	KgmlSaxHandler kgmlParser = new KgmlSaxHandler(pathwayGraphBuilder);
+	  	
+        // Use the default (non-validating) parser
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        try 
+        {
+            // Parse the input
+            SAXParser saxParser = factory.newSAXParser();
+            saxParser.parse( 
+            		new File("/home/marc/university/diploma/data/pathways/map00271.xml"), kgmlParser);
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        
+        pathwayGraphBuilder.showPathwayGraph();
+        //System.exit(0);
+	}
+}
---
+++ src/cerberus/xml/parser/kgml/KgmlSaxHandler.java
@@ -0,0 +1,94 @@
+package cerberus.xml.parser.kgml;
+
+import cerberus.pathways.PathwayGraphBuilder;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+//import java.lang.Integer;
+
+public class KgmlSaxHandler extends DefaultHandler 
+{
+	
+	private PathwayGraphBuilder pathwayGraphBuilder;
+	
+	public KgmlSaxHandler(PathwayGraphBuilder pathwayGraphBuilder)
+	{
+		this.pathwayGraphBuilder = pathwayGraphBuilder;
+	}
+	
+    public void startDocument()
+    throws SAXException
+    {
+        System.out.println("Start parsing the document.");
+    }
+
+    public void endDocument()
+    throws SAXException
+    {
+       	System.out.println("End parsing the document.");
+    }
+	
+    public void startElement(String namespaceURI,
+            String sSimpleName,
+            String sQualifiedName,
+            Attributes attributes)
+    throws SAXException	
+    {
+    	String sElementName = sSimpleName;
+    	
+    	if ("".equals(sElementName)) 
+    	{
+    		sElementName = sQualifiedName; // namespaceAware = false
+    	}
+    	
+    	System.out.println("Element name: " +sElementName);
+    	
+    	if (sElementName.equals("graphics"))
+    	{
+    		if (attributes != null) 
+    		{
+    			String sName = "";
+    			int iHeight = 0;
+    			int iWidth = 0;
+    			int iXPosition = 0;
+    			int iYPosition = 0;
+    		
+    			for (int iAttributeIndex = 0; iAttributeIndex < attributes.getLength(); iAttributeIndex++) 
+    			{
+    				String sAttributeName = attributes.getLocalName(iAttributeIndex);
+    			
+    				if ("".equals(sAttributeName))
+    				{
+    					sAttributeName = attributes.getQName(iAttributeIndex);
+    				}
+    				
+    				if (sAttributeName.equals("name"))
+    					sName = attributes.getValue(iAttributeIndex); 
+	    			else if (sAttributeName.equals("height"))
+	    				iHeight = new Integer(attributes.getValue(iAttributeIndex)); 
+	    			else if (sAttributeName.equals("width"))
+	    				iWidth = new Integer(attributes.getValue(iAttributeIndex)); 
+	    			else if (sAttributeName.equals("x"))
+	    				iXPosition = new Integer(attributes.getValue(iAttributeIndex)); 
+	    			else if (sAttributeName.equals("y"))
+	    				iYPosition = new Integer(attributes.getValue(iAttributeIndex)); 
+    			
+	    			System.out.println("Attribute name: " +sAttributeName);
+	    			System.out.println("Attribute value: " +attributes.getValue(iAttributeIndex));
+	    		}
+    		
+				pathwayGraphBuilder.createCell(sName, iHeight, iWidth, iXPosition, iYPosition);
+    		}
+    	}
+    }
+
+    public void endElement(String namespaceURI,
+          String sSimpleName,
+          String sQualifiedName)
+	throws SAXException
+	{
+		//emit("</"+sName+">");
+	}
+}
