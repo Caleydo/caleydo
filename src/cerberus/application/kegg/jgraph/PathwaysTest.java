375d3f247bca322c74745418af1999ad645f3211
Marc Streit <marc.streit@jku.at>
2006-07-14 11:15:38 +0000

    Example that shows how to use JGraph. The data for the nodes are parsed from /data/XML/pathways/map00271.xml
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@30 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ src/cerberus/application/kegg/jgraph/PathwaysTest.java
@@ -0,0 +1,33 @@
+package cerberus.application.kegg.jgraph;
+
+import java.io.File;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import cerberus.application.kegg.jgraph.KgmlSaxHandler;
+import cerberus.application.kegg.jgraph.PathwayGraphBuilder;
+
+public class PathwaysTest {
+	public static void main(String[] args) throws Exception 
+	{
+	  	PathwayGraphBuilder pathwayGraphBuilder = new PathwayGraphBuilder();
+	  	KgmlSaxHandler kgmlParser = new KgmlSaxHandler(pathwayGraphBuilder);
+	  	
+        // Use the default (non-validating) parser
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        try 
+        {
+            // Parse the input
+            SAXParser saxParser = factory.newSAXParser();
+            saxParser.parse( 
+            		new File("../data/pathways/map00271.xml"), kgmlParser);
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        
+        pathwayGraphBuilder.showPathwayGraph();
+        //System.exit(0);
+	}
+}
