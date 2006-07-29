7e8261f5e7415fcff8263a2237de9583675bb005
Marc Streit <marc.streit@jku.at>
2006-07-29 09:02:21 +0000

    Deleted old pathway drawing classes.
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@118 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- src/cerberus/view/gui/swing/jgraph/PathwayGraphBuilder2D.java
+++
@@ -1,6 +0,0 @@
-package cerberus.view.gui.swing.jgraph;
-
-public class PathwayGraphBuilder2D extends PathwayGraphBuilder
-{
-
-}
--- src/cerberus/view/gui/swing/jgraph/PathwayGraphBuilder3D.java
+++
@@ -1,6 +0,0 @@
-package cerberus.view.gui.swing.jgraph;
-
-public class PathwayGraphBuilder3D extends PathwayGraphBuilder
-{
-
-}
