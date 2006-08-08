82380f8d9edfb2129bdcc15663aa5b2ce9b4240a
Marc Streit <marc.streit@jku.at>
2006-08-08 13:47:39 +0000

    The prototype is now implemented in a class and divided in several methods.
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@164 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- src/cerberus/application/prototype/CerberusPrototype.java
+++ src/cerberus/application/prototype/CerberusPrototype.java
@@ -5,9 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
@@ -17,19 +14,24 @@ import cerberus.manager.GeneralManager;
 import cerberus.manager.gui.SWTGUIManagerSimple;
 import cerberus.manager.singelton.OneForAllManager;
 import cerberus.manager.view.ViewManagerSimple;
-import cerberus.command.system.CmdSystemLoadFileViaImporter;
-import cerberus.data.loader.MicroArrayLoader;
 import cerberus.manager.type.ManagerObjectType;
 import cerberus.xml.parser.CerberusDefaultSaxHandler;
 import cerberus.xml.parser.command.CommandSaxHandler;
-import cerberus.xml.parser.kgml.KgmlSaxHandler;
 
-public class CerberusPrototype 
+public class CerberusPrototype
 {
+	protected GeneralManager refGeneralManager;
+	protected SWTGUIManagerSimple refSWTGUIManager;
+	protected ViewManagerSimple refViewManager;
+	
 	public static void main(String[] args) 
 	{
-		String sRawDataFileName = "data/MicroarrayData/gpr_format/tests/test_10_values.gpr";
-		
+		CerberusPrototype prototype = new CerberusPrototype();	
+		prototype.run();
+	}
+	
+	public CerberusPrototype()
+	{
 		/**
 		 * In order to use SWT call setStateSWT( true ) to enabel SWT support!
 		 */
@@ -37,117 +39,116 @@ public class CerberusPrototype
 		oneForAllManager.setStateSWT( true );
 		oneForAllManager.initAll();
 		
-		GeneralManager generalManager = oneForAllManager.getGeneralManager();
+		refGeneralManager = oneForAllManager.getGeneralManager();
 		
+		refSWTGUIManager = (SWTGUIManagerSimple) refGeneralManager.getManagerByBaseType(ManagerObjectType.GUI_SWT);
+		
+		refViewManager = (ViewManagerSimple) refGeneralManager.getManagerByBaseType(ManagerObjectType.VIEW);		
+	}
+
+	public void run()
+	{
+		parseInputFromXML();
+		
+		createViews();
+		
+		refSWTGUIManager.runApplication();
+	}
+	
+	protected void parseInputFromXML()
+	{
 		CommandSaxHandler saxCmdHandler = 
-			new CommandSaxHandler( generalManager );
+			new CommandSaxHandler(refGeneralManager);
 		
 		String filename = "data/XML/bootstrap/cerberus_bootstrap_sample.xml";
 					
 		parseOnce( openInputStreamFromFile(filename), saxCmdHandler);
 		
-		//loading the raw data
-//		MicroArrayLoader microArrayLoader = 
-//			new MicroArrayLoader(oneForAllManager.getGeneralManager(), sRawDataFileName);
-//		microArrayLoader.loadData();
+//		 MicroArrayLoader microArrayLoader = new MicroArrayLoader(generalManager);
+//		 microArrayLoader.setTargetSet(oneForAllManager.getSingelton().getSetManager().getItemSet(25101));
+//		 microArrayLoader.setFileName(sRawDataFileName);
+//		 microArrayLoader.setTokenPattern("FLOAT;FLOAT;SKIP;STRING;STRING;ABORT");
+//		 microArrayLoader.loadData();
 		
-		//load the pathway data
-//		KgmlSaxHandler kgmlParser = new KgmlSaxHandler();
-//		SAXParserFactory factory = SAXParserFactory.newInstance();
-//		try
-//		{
-//			// Parse the input
-//			SAXParser saxParser = factory.newSAXParser();
-//			saxParser.parse(new File("data/XML/pathways/map00271.xml"),
-//					kgmlParser);
-//
-//		} catch (Throwable t)
-//		{
-//			t.printStackTrace();
-//		}
-		
-//		MicroArrayLoader microArrayLoader = new MicroArrayLoader(generalManager);
-//		microArrayLoader.setTargetSet(oneForAllManager.getSingelton().getSetManager().getItemSet(25101));
-//		microArrayLoader.setFileName(sRawDataFileName);
-//		microArrayLoader.setTokenPattern("FLOAT;FLOAT;SKIP;STRING;STRING;ABORT");
-//		microArrayLoader.loadData();
+//		 CmdSystemLoadFileViaImporter commandFileImporter =
+//		 new CmdSystemLoadFileViaImporter(
+//		 generalManager, sRawDataFileName, "SKIP;INT;SKIP;STRING;ABORT", 15101 );
+//		 commandFileImporter.doCommand();
 
-		CmdSystemLoadFileViaImporter commandFileImporter = 
-			new CmdSystemLoadFileViaImporter(
-				generalManager, sRawDataFileName, "SKIP;INT;SKIP;STRING;ABORT", 15101 );
-		commandFileImporter.doCommand();
-		
-		SWTGUIManagerSimple swtGuiManager = (SWTGUIManagerSimple) generalManager.getManagerByBaseType(ManagerObjectType.GUI_SWT);
-		
-		ViewManagerSimple viewManager = (ViewManagerSimple) generalManager.getManagerByBaseType(ManagerObjectType.VIEW);
-		//viewManager.createView(ManagerObjectType.VIEW_PATHWAY);
-		//viewManager.createView(ManagerObjectType.VIEW_TEST_TABLE);
-		viewManager.createView(ManagerObjectType.VIEW_SWT_DATA_EXPLORER);
-		//viewManager.createView(ManagerObjectType.VIEW_SWT_SET_TABLE);
-		//viewManager.createView(ManagerObjectType.VIEW_SWT_STORAGE_TABLE);
-		//viewManager.createView(ManagerObjectType.VIEW_SWT_GEARS);
-		//viewManager.createView(ManagerObjectType.VIEW_SWT_HEATMAP2D);
-		
-		swtGuiManager.runApplication();
 	}
 	
+	protected void createViews()
+	{
+		// refViewManager.createView(ManagerObjectType.VIEW_PATHWAY);
+		// refViewManager.createView(ManagerObjectType.VIEW_TEST_TABLE);
+		refViewManager.createView(ManagerObjectType.VIEW_SWT_DATA_EXPLORER);
+		// refViewManager.createView(ManagerObjectType.VIEW_SWT_SET_TABLE);
+		// refViewManager.createView(ManagerObjectType.VIEW_SWT_STORAGE_TABLE);
+		// refViewManager.createView(ManagerObjectType.VIEW_SWT_GEARS);
+		// refViewManager.createView(ManagerObjectType.VIEW_SWT_HEATMAP2D);
+	}
+
 	/**
 	 * Opens a file and returns an input stream to that file.
 	 * 
-	 * @param sXMLFileName name abd path of the file
+	 * @param sXMLFileName
+	 *            name abd path of the file
 	 * @return input stream of the file, or null
 	 */
-	protected static InputSource openInputStreamFromFile( final String sXMLFileName ) {
-		
-		try {
-			File inputFile = new File( sXMLFileName );		
-			FileReader inReader = new FileReader( inputFile );
-			
-			InputSource inStream = new InputSource( inReader );
-			
+	protected InputSource openInputStreamFromFile(final String sXMLFileName)
+	{
+		try
+		{
+			File inputFile = new File(sXMLFileName);
+			FileReader inReader = new FileReader(inputFile);
+
+			InputSource inStream = new InputSource(inReader);
+
 			return inStream;
-		}
-		catch ( FileNotFoundException fnfe) {
-			System.out.println("File not found " + fnfe.toString() );
+		} catch (FileNotFoundException fnfe)
+		{
+			System.out.println("File not found " + fnfe.toString());
 		}
 		return null;
 	}
-	
-	public static boolean parseOnce( InputSource inStream, 
-			CerberusDefaultSaxHandler handler) {
-		
-		try {
-			XMLReader reader = XMLReaderFactory.createXMLReader();			
-			reader.setContentHandler( handler );
-			
-			try {						
-				reader.parse( inStream );
-			} 
-			catch ( IOException e) {
-				System.out.println(" EX " + e.toString() );
+
+	public boolean parseOnce(InputSource inStream,
+			CerberusDefaultSaxHandler handler)
+	{
+		try
+		{
+			XMLReader reader = XMLReaderFactory.createXMLReader();
+			reader.setContentHandler(handler);
+
+			try
+			{
+				reader.parse(inStream);
+			} catch (IOException e)
+			{
+				System.out.println(" EX " + e.toString());
 			}
-			
+
 			/**
 			 * Use data from parser to restore state...
-			 */ 
-			
-			System.out.println("PARSE DONE\n  INFO:" +
-					handler.getErrorMessage() );
-			
+			 */
+
+			System.out.println("PARSE DONE\n  INFO:"
+					+ handler.getErrorMessage());
+
 			/**
 			 * Restore state...
 			 */
-		
+
 			inStream.getCharacterStream().close();
-		}
-		catch (SAXException se) {
-			System.out.println("ERROR SE " + se.toString() );
+		} catch (SAXException se)
+		{
+			System.out.println("ERROR SE " + se.toString());
 		} // end try-catch SAXException
-		catch (IOException ioe) {
-			System.out.println("ERROR IO " + ioe.toString() );
+		catch (IOException ioe)
+		{
+			System.out.println("ERROR IO " + ioe.toString());
 		} // end try-catch SAXException, IOException
-		
-		
+
 		return true;
 	}
 }
