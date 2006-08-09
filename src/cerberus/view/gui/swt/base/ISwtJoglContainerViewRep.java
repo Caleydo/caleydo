23f896189fd4ddebf7b397cf8c7bcc68bd486553
kalkusch <kalkusch>
2006-08-09 07:42:31 +0000

    Created Interface for SWT-JOGL view-rep's
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@171 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ src/cerberus/view/gui/swt/base/ISwtJoglContainerViewRep.java
@@ -0,0 +1,55 @@
+package cerberus.view.gui.swt.base;
+
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLEventListener;
+
+import com.sun.opengl.util.Animator;
+
+public interface ISwtJoglContainerViewRep
+{
+
+	/**
+	 * Get the JOGL animator.
+	 * 
+	 * @see com.sun.opengl.util.Animator
+	 *  
+	 * @return animator current Animator
+	 */
+	public abstract Animator getAnimator();
+
+	/**
+	 * ISet the JOGL animator.
+	 * 
+	 * @see com.sun.opengl.util.Animator
+	 *  
+	 * @param setAnimator set current Animator
+	 * 
+	 * @return TRUE if Animator was scuessfully set.
+	 */
+	public abstract boolean setAnimator(final Animator setAnimator);
+
+	/**
+	 * Add a GLEventListener to the GLCanvas.
+	 * 
+	 * @see javax.media.opengl.GLCanvas#addGLEventListener(GLEventListener)
+	 * 
+	 * @param listener to be added
+	 */
+	public abstract void addGLEventListener(
+			final GLEventListener setGLEventListener);
+
+	/**
+	 * Removes a GLEventListener from the GLCanvas.
+	 *  
+	 * @see javax.media.opengl.GLCanvas#removeGLEventListener(GLEventListener)
+	 * 
+	 * @param setGLEventListener listener to be removed
+	 */
+	public abstract void removeGLEventListener(
+			final GLEventListener setGLEventListener);
+
+	public abstract boolean setGLCanvas(final GLCanvas setGLCanvas);
+
+	public abstract GLCanvas getGLCanvas();
+
+}
