9907267fc7dc87db1d45f8d0570b7b750fcd779e
kalkusch <kalkusch>
2006-08-17 21:59:45 +0000

    Added test applications fro new XML parser concept.
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@199 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ src/cerberus/application/core/CerberusBootloaderTest.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package cerberus.application.core;
+
+import org.xml.sax.InputSource;
+
+import cerberus.application.prototype.CerberusPrototype;
+import cerberus.manager.IGeneralManager;
+import cerberus.manager.singelton.OneForAllManager;
+import cerberus.util.system.CerberusInputStream;
+import cerberus.xml.parser.handler.IXmlParserHandler;
+import cerberus.xml.parser.handler.command.CommandSaxHandler;
+import cerberus.xml.parser.manager.XmlParserManager;
+import cerberus.xml.parser.manager.IXmlParserManager;
+
+
+/**
+ * @author kalkusch
+ *
+ */
+public class CerberusBootloaderTest
+{
+
+	protected XmlParserManager parserManager;
+	
+	protected final IGeneralManager refGeneralManager;
+	
+	
+	public static void main(String[] args) 
+	{
+		CerberusBootloaderTest prototype = new CerberusBootloaderTest();	
+	}
+	
+	/**
+	 * 
+	 */
+	public CerberusBootloaderTest()
+	{
+		refGeneralManager = new OneForAllManager( null );
+		
+		((OneForAllManager) refGeneralManager).initAll();
+		
+		
+		parserManager = new XmlParserManager( refGeneralManager, false );
+		
+		CommandSaxHandler cmdHandler = 
+			new CommandSaxHandler( refGeneralManager,
+					parserManager );
+		
+		
+		parserManager.registerSaxHandler( cmdHandler, true );
+		
+		
+		String filename = "data/XML/bootstrap/cerberus_bootstrap_sample.xml";
+		
+		InputSource inSource = 
+			CerberusInputStream.openInputStreamFromFile( filename );
+					
+		CerberusInputStream.parseOnce( inSource , parserManager );
+	}
+
+}
