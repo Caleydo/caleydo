767571b9df652950425d4d066631f03923bfaaa1
kalkusch <kalkusch>
2006-11-14 09:43:09 +0000

    Add midi in support for fader and pedals (Behringer)
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@360 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ src/cerberus/util/midi/device/IMidiCallback.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package cerberus.util.midi.device;
+
+/**
+ * @author kalkusch
+ *
+ */
+public interface IMidiCallback {
+
+	public void callbackSetValue( String value );
+}
---
+++ src/cerberus/util/midi/device/data/MidiData.java
@@ -0,0 +1,83 @@
+package cerberus.util.midi.device.data;
+
+import javax.sound.midi.ShortMessage;
+
+import cerberus.util.midi.device.data.MidiShortMessageType;
+
+/**
+ * Container to store data from MIDI event.
+ * 
+ * @author kalkusch
+ *
+ */
+public class MidiData {
+
+	protected MidiShortMessageType msgType = MidiShortMessageType.NO_MSG;
+	
+	protected int iChannel = -1;
+	
+	protected int iControlId = -1;
+	
+	protected int iValue = -1;
+	
+	
+	public MidiData() {
+		
+	}
+
+	public MidiShortMessageType getMidiType() {
+		return this.msgType;
+	}
+	
+	public void setMidiType( MidiShortMessageType type) {
+		this.msgType = type;
+	}
+	
+	public void setChannel( final int channel ) {
+		this.iChannel = channel;
+	}
+	
+	public int getChannel() {
+		return this.iChannel;
+	}
+	
+	public String toString() {
+		String resultMsg = "[" + 
+			iChannel + "# " +
+			iControlId + " -> " +
+			iValue + " " +
+			msgType + "]";
+		
+		return resultMsg;
+	}
+
+	
+	public int getControlId() {
+	
+		return iControlId;
+	}
+
+	
+	public void setControlId(int controlId) {
+	
+		iControlId = controlId;
+	}
+
+	
+	public int getValue() {
+	
+		return iValue;
+	}
+
+	
+	public void setValue(int value) {
+	
+		iValue = value;
+	}
+	
+	public void setControlIdAndValue( final ShortMessage message ) {
+		iControlId = message.getData1();
+		iValue = message.getData2();
+	}
+	
+}
