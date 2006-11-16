cab13f2a555e76c49a9faa5ceb3c970fb961fe92
kalkusch <kalkusch>
2006-11-16 16:49:57 +0000

    Listener for Swing MidiConnectorSwingSliders.java
    CoreMidiConnector.java as debug tool to trace midi calls from a midi device
    
    
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@368 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ src/cerberus/util/midi/device/PresetActionListener.java
@@ -0,0 +1,26 @@
+package cerberus.util.midi.device;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class PresetActionListener implements ActionListener {
+
+	private MidiConnectorSwingSliders parent;
+	
+	private int iPresetId;
+	
+	public PresetActionListener( MidiConnectorSwingSliders parent, int iPresetId ) {
+		super();
+		
+		this.iPresetId = iPresetId;		
+		this.parent = parent;
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+
+		parent.writePresetToMidiDevice( iPresetId );
+
+	}
+
+}
