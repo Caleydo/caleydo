fd065606bd9a31037ee9f0b494da930b93f34ad7
kalkusch <kalkusch>
2006-11-27 15:57:10 +0000

    Interface for Selection used by several ViewRep's
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@401 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ src/cerberus/view/gui/IGroupedSelection.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package cerberus.view.gui;
+
+/**
+ * @author kalkusch
+ *
+ * @ cerberus.view.gui.swt.pathway.APathwayGraphViewRep
+ */
+public interface IGroupedSelection {
+
+	public void createSelectionSet(int[] arSelectedId,
+			int[] arSelectionGroup,
+			int[] arOptionalData );
+	
+}
