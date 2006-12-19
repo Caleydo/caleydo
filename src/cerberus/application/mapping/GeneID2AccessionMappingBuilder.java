07ab11db2e6da2ffc02ac68151124c3010dbdb49
Marc Streit <marc.streit@jku.at>
2006-12-19 15:58:57 +0000

    Added classes for generating the mapping files. (GeneID <-> Accession , GeneID <-> Enzyme)
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@464 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ src/cerberus/application/mapping/GeneID2AccessionMappingBuilder.java
@@ -0,0 +1,12 @@
+package cerberus.application.mapping;
+
+/**
+ * Class generated a file that contains a mapping 
+ * from GeneIDs to Accession Code.
+ * 
+ * @author Marc Streit
+ *
+ */
+public class GeneID2AccessionMappingBuilder {
+
+}
