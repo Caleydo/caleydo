3deff32dc83ae0c0dd94c3f0f2e40569f6fc9d80
Alexander Lex <alex@seas.harvard.edu>
2007-08-31 14:00:17 +0000

    Implemented initial version of Coordinate System
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@854 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- cerberus_core/src/cerberus/view/opengl/canvas/parcoords/GLCanvasParCoords.java
+++ cerberus_core/src/cerberus/view/opengl/canvas/parcoords/GLCanvasParCoords.java
@@ -15,6 +15,7 @@ import cerberus.view.opengl.canvas.AGLCanvasUser;
 public class GLCanvasParCoords extends AGLCanvasUser {
 
 	private IGeneralManager refGeneralManager;
+	private float axisSpacing;
 	
 	public GLCanvasParCoords(IGeneralManager refGeneralManager,
 			int viewId,
@@ -25,24 +26,101 @@ public class GLCanvasParCoords extends AGLCanvasUser {
 
 		this.refViewCamera.setCaller(this);
 		this.refGeneralManager = refGeneralManager;
+		this.axisSpacing = 2;
 		
 	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see cerberus.view.opengl.canvas.AGLCanvasUser#initGLCanvas(javax.media.opengl.GL)
+	 */
+	public void initGLCanvas(GL gl) {
+	
+		super.initGLCanvas(gl);
+		gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+		
+	}
+	
 
 	/*
 	 * (non-Javadoc)
 	 * @see cerberus.view.opengl.canvas.AGLCanvasUser#renderPart(javax.media.opengl.GL)
 	 */
-	public void renderPart(GL gl) {
+	public void renderPart(GL gl) 
+	{
 		
-		gl.glBegin(GL.GL_TRIANGLES); // Drawing using triangles
-		gl.glColor3f(2.0f, 0.0f, 0.0f); // Set the color to red
-		gl.glVertex3f(0.0f, -2.0f, 0.0f); // Top
-		gl.glColor3f(0.0f, 2.0f, -1.0f); // Set the color to green
-		gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom left
-		gl.glColor3f(0.0f, 0.0f, 1.0f); // Set the color to blue
-		gl.glVertex3f(1.0f, -1.0f, 0.0f); // Bottom right
+		renderCoordinateSystem(gl, 4, 3);
+	/*
+		//int count = 0;
+		//while(count < 100000)
+		//{
+			//gl.glPushMatrix();
+			//gl.glRotatef(0.1f, 0.0f, 0.0f, 1.0f);
+			gl.glPointSize(2.0f);
+			gl.glColor3f(0.0f, 0.0f, 0.0f); // Set the color to red
+			
+			gl.glBegin(GL.GL_TRIANGLES); // Drawing using triangles			
+			gl.glVertex3f(0.1f, 0.0f, 0.0f); // Top
+			gl.glVertex3f(2.0f, 0.0f, 0.0f);
+			
+			//gl.glVertex3f(-0.1f, 0.0f, 0.0f);
+			gl.glColor3f(1.0f, 0.0f, 0.0f);
+			//gl.glVertex3f(-4.0f, 0.0f, 0.0f);
+			
+			gl.glVertex3f(0.0f, 0.1f, 0.0f);
+			gl.glVertex3f(0.0f, 4.0f, 0.0f);
+			
+			gl.glVertex3f(0.0f, -0.1f, 0.0f);
+			gl.glVertex3f(0.0f, -4.0f, 0.0f);
+			gl.glEnd();
+			
+			//gl.glColor3f(0.0f, 2.0f, -1.0f); // Set the color to green
+			//gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom left
+			//gl.glColor3f(0.0f, 0.0f, 1.0f); // Set the color to blue
+			//gl.glVertex3f(1.0f, -1.0f, 0.0f); // Bottom right
+			//gl.glColor3f(1.0f, 0.0f, 1.0f);
+			//gl.glVertex3f(-2.0f, -2.0f, 0.0f);
+			//gl.glPopMatrix();
+			//count++;
+		//}
+	
+	
 		gl.glEnd(); // Finish drawing the triangle
 
+		*/
+	}
+	
+	private void renderCoordinateSystem(GL gl, int numberParameters, float maxHeight)
+	{
+		// draw X-Axis
+		gl.glColor3f(0.0f, 0.0f, 0.0f);
+		gl.glLineWidth(3.0f);
+				
+		gl.glBegin(GL.GL_LINES);		
+	
+		gl.glVertex3f(-0.1f, 0.0f, 0.0f); 
+		gl.glVertex3f((numberParameters * axisSpacing)+0.1f, 0.0f, 0.0f);
+		
+		//gl.glVertex3f(0.0f, 0.0f, 0.0f);
+		//gl.glVertex3f(0.0f, maxHeight, 0.0f);			
+		
+		gl.glEnd();
+		
+		// draw all Y-Axis
+		gl.glColor3f(0.0f, 2.0f, 0.0f);
+		gl.glLineWidth(1.0f);
+		gl.glBegin(GL.GL_LINES);	
+		
+		int count = 0;
+		while (count <= numberParameters)
+		{
+			gl.glVertex3f(count*axisSpacing, 0.0f, 0.0f);
+			gl.glVertex3f(count*axisSpacing, maxHeight, 0.0f);
+			count++;
+		}
+		
+		gl.glEnd();
 	}
 
 }
