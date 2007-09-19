3feeceb041594a7495ae0471abefa56b4792dc83
kalkusch <kalkusch>
2007-09-19 14:07:38 +0000

    update batch file to create org.geneview.core_bin.jar
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@973 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.geneview.core/build/java1.5/win32/subroutine_build_jar.bat
+++ org.geneview.core/build/java1.5/win32/subroutine_build_jar.bat
@@ -1,5 +1,16 @@
 @echo off
 
+rem test if JAVAHOME environment variable ist set...
+IF NOT "%JAVAHOME%"=="" GOTO JAVAHOME_IS_SET
+
+:NO_JAVAHOMEE
+echo.
+echo.  Enviroment variable JAVAHOME need to be set to java.exe   (java 1.6 or higher)
+echo.
+GOTO END
+
+:JAVAHOME_IS_SET
+
 SETLOCAL
 
 rem you can switch between "src" and "bin"
@@ -8,30 +19,78 @@ SET version=%1
 SET destination=%2
 SET destination2=%3
 
+SET path_org_geneview_core=..\..\..\
+
+SET path_org_geneview_lib_jogl_jar=%path_org_geneview_core%..\org.geneview.lib\lib\jogl\*.jar
+SET path_org_geneview_core_jogl_jar=%path_org_geneview_core%lib\jogl\
+SET file_org_geneview_core_jogl_jar=%path_org_geneview_core%lib\jogl\jogl.jar
+
+SET path_org_geneview_lib_swt_jar=%path_org_geneview_core%..\org.geneview.lib\lib\swt\*.jar
+SET path_org_geneview_core_swt_jar=%path_org_geneview_core%lib\swt\
+SET file_org_geneview_core_swt_jar=%path_org_geneview_core%lib\swt\swt.jar
+
+SET path_org_geneview_rcp_lib=%path_org_geneview_core%..\org.geneview.rcp\lib\
+
 rem path and file name for jar.exe
-SET file_jar=C:\Compiler\java\jre1.6\bin\jar.exe
+SET file_jar=%JAVAHOME%\jar.exe
 
 rem path to bin folder were *.class files are stored
-SET class_files=..\..\..\%destination%%destination2%
+SET class_files=%path_org_geneview_core%%destination%%destination2%
 
 
 rem path and filename of resulting jar file
-SET jar_file_name=..\..\..\lib\org.geneview.core_%destination%_r%version%.jar
+SET jar_file_name=%path_org_geneview_core%lib\org.geneview.core_%destination%_r%version%.jar
+
+rem test if jogl.jar and swt.jar is in place.
+IF NOT EXIST %file_org_geneview_core_jogl_jar% GOTO COPY_PLATFORM_DEPANDENT_JAR
+IF NOT EXIST %file_org_geneview_core_swt_jar% GOTO COPY_PLATFORM_DEPANDENT_JAR
+
+GOTO MAKE_JAR
+
+:COPY_PLATFORM_DEPANDENT_JAR
+
+echo.
+echo. copy %path_org_geneview_lib_jogl_jar% %path_org_geneview_core_jogl_jar%
+echo. copy %path_org_geneview_lib_swt_jar% %path_org_geneview_core_swt_jar%
+echo.
+
+copy %path_org_geneview_lib_jogl_jar% %path_org_geneview_core_jogl_jar%
+copy %path_org_geneview_lib_swt_jar% %path_org_geneview_core_swt_jar%
+
+
+echo.
+echo.   make jar next..
+pause
+
+:MAKE_JAR
 
 echo info= %class_files%   %jar_file_name%
-
 echo.
 echo create jar in folder %jar_file_name%
 echo.
+
+pause
 
 IF NOT EXIST %file_jar% GOTO NO_JAR_EXE
 
 %file_jar% cvfM %jar_file_name% -C  %class_files% .
 
-
+echo.
+echo info= %class_files%   %jar_file_name%
 echo.
 echo jar in folder %jar_file_name% .. [done]
 echo.
+
+echo. copy to rcp:
+echo. copy %jar_file_name%
+echo.      %path_org_geneview_rcp_lib%
+
+copy %jar_file_name% %path_org_geneview_rcp_lib%
+
+echo.
+echo. copy to %path_org_geneview_rcp_lib%   ..[done]
+echo.
+
 GOTO END
 
 :NO_JAR_EXE
---
+++ org.geneview.core/lib/jogl/Readme_jogl.jar_details.txt
@@ -0,0 +1,11 @@
+use package "org.geneview.lib" 
+
+ org.geneview.lib/lib/jogl/*.jar  ==> /lib/swt/
+
+  inside "org.geneview.lib" 
+  =========================
+  Copy jogl.jar and gluegen-rt.jar from either win32, linux or mac folder to this folder /lib/jogl/
+  Eclipse project files target at this path.
+
+  download versions via:
+  https://jogl.dev.java.net/
