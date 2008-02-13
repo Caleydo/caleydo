8e65946aea427c8a10f1c37be0bd6625ecf9752b
Alexander Lex <alex@seas.harvard.edu>
2008-02-13 14:55:44 +0000

    svn bug...
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@1121 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ org.geneview.core/src/org/geneview/core/manager/view/ESelectionMode.java
@@ -0,0 +1,20 @@
+package org.geneview.core.manager.view;
+
+/**
+ * 
+ * Enum to specify the picking mode
+ * 
+ * @author Alexander Lex
+ *
+ */
+
+public enum ESelectionMode
+{
+
+	// Flag for adding the pick to the current selection
+	AddPick,
+	// Flag for replacing the pick with all stored picks per view and type
+	ReplacePick,
+	// Flag for removing the picked object from the pick list
+	RemovePick
+}
