bf87da712182054169b34be2d3607e0ac5f2b707
Marc Streit <marc.streit@jku.at>
2009-03-13 14:37:20 +0000

    Fixed clincal data setup with Asslaber data (requested by Stefan).
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@1812 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.core/src/org/caleydo/core/data/mapping/EIDType.java
+++ org.caleydo.core/src/org/caleydo/core/data/mapping/EIDType.java
@@ -3,28 +3,34 @@ package org.caleydo.core.data.mapping;
 import org.caleydo.core.data.collection.EStorageType;
 
 /**
- * Enum that defines all genome data types that could possibly be loaded to the system.
+ * Enum that defines all genome data types that could possibly be loaded to the
+ * system.
  * 
  * @author Michael Kalkusch
  * @author Marc Streit
  * @author Alexander Lex
  */
-public enum EIDType {
+public enum EIDType
+{
 
-	GENE_NAME(EStorageType.STRING, "gene-name", "gene pseudo name"), GENE_SYMBOL(EStorageType.STRING,
-		"gene-symbol", "gene pseudo symbol"), BIOCARTA_GENE_ID(EStorageType.STRING, "biocarta geneid",
-		"biocarta geneid"), DAVID(EStorageType.INT, "david", "david-id"), REFSEQ_MRNA(EStorageType.STRING,
-		"refseq-mrna", "refseq-mrna-id"), REFSEQ_MRNA_INT(EStorageType.INT, "refseq-mrna int",
-		"refseq-mrna-id int for internal usage in event system"), ENTREZ_GENE_ID(EStorageType.INT,
-		"entrez-gene-id", "entrez-gene-id"), EXPRESSION_INDEX(EStorageType.INT, "expression-index",
-		"The index of a gene expression value"),
+	GENE_NAME(EStorageType.STRING, "gene-name", "gene pseudo name"),
+	GENE_SYMBOL(EStorageType.STRING, "gene-symbol", "gene pseudo symbol"),
+	BIOCARTA_GENE_ID(EStorageType.STRING, "biocarta geneid", "biocarta geneid"),
+	DAVID(EStorageType.INT, "david", "david-id"),
+	REFSEQ_MRNA(EStorageType.STRING, "refseq-mrna", "refseq-mrna-id"),
+	REFSEQ_MRNA_INT(
+		EStorageType.INT,
+		"refseq-mrna int",
+		"refseq-mrna-id int for internal usage in event system"),
+	ENTREZ_GENE_ID(EStorageType.INT, "entrez-gene-id", "entrez-gene-id"),
+	EXPRESSION_INDEX(EStorageType.INT, "expression-index", "The index of a gene expression value"),
 	// EXPRESSION_EXPERIMENT(EStorageType.INT, "expression-experiment",
 	// "A expression experiment containing gene expression values"),
-	PATHWAY_VERTEX(EStorageType.INT, "pathway vertex id", "The id of the pathway vertex"), PATHWAY(
-		EStorageType.INT, "pathway id", "The ID of a pathway"), EXPERIMENT(EStorageType.STRING, "experiment id",
-		"The ID that connects clinical with microarray data"), EXPERIMENT_INDEX(EStorageType.INT,
-		"experiment index", "The experiment index"), CELL_COMPONENT(EStorageType.STRING, "GO cell component",
-		"The GO cell component"),
+	PATHWAY_VERTEX(EStorageType.INT, "pathway vertex id", "The id of the pathway vertex"),
+	PATHWAY(EStorageType.INT, "pathway id", "The ID of a pathway"),
+	EXPERIMENT(EStorageType.STRING, "experiment id", "The ID that connects clinical with microarray data"),
+	EXPERIMENT_INDEX(EStorageType.INT, "experiment index", "The experiment index"),
+	CELL_COMPONENT(EStorageType.STRING, "GO cell component", "The GO cell component"),
 
 	// FIXME: Make this general! Needed for Asslaber data
 	OLIGO(EStorageType.STRING, "oligo", "oligo-id");
@@ -39,11 +45,12 @@ public enum EIDType {
 	 * Constructor
 	 * 
 	 * @param storageType
-	 *          the type of the storage
+	 *            the type of the storage
 	 * @param sName
 	 * @param sDesciption
 	 */
-	private EIDType(final EStorageType storageType, final String sName, final String sDesciption) {
+	private EIDType(final EStorageType storageType, final String sName, final String sDesciption)
+	{
 
 		this.sName = sName;
 		this.sDescription = sDesciption;
@@ -53,7 +60,8 @@ public enum EIDType {
 	/**
 	 * @return the entities description.
 	 */
-	public String getDescription() {
+	public String getDescription()
+	{
 
 		return sDescription;
 	}
@@ -63,7 +71,8 @@ public enum EIDType {
 	 * 
 	 * @return type of storage needed
 	 */
-	public EStorageType getStorageType() {
+	public EStorageType getStorageType()
+	{
 
 		return storageType;
 	}
@@ -71,7 +80,8 @@ public enum EIDType {
 	/**
 	 * @return the entities name
 	 */
-	public String getName() {
+	public String getName()
+	{
 
 		return sName;
 	}
