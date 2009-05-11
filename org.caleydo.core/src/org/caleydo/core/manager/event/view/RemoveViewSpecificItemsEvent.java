f33c4d6828d2ad3a2de2afba4308e3aad90b8bd2
puff <puff>
2009-05-11 14:17:47 +0000

    fixed javadocs
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@2020 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.core/src/org/caleydo/core/manager/event/view/RemoveViewSpecificItemsEvent.java
+++ org.caleydo.core/src/org/caleydo/core/manager/event/view/RemoveViewSpecificItemsEvent.java
@@ -3,8 +3,7 @@ package org.caleydo.core.manager.event.view;
 import org.caleydo.core.manager.event.AEvent;
 
 /**
- * Event that signals that a detached view has been activated.
- * the command holds a list of view-ids as payload.  
+ * Event to signal that all view specific toolbar-items should be removed.
  * @author Werner Puff
  */
 public class RemoveViewSpecificItemsEvent
