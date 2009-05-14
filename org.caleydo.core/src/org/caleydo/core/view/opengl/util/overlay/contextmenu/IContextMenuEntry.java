bbd65533e260d216ba314b0c605d76cfec0c2a14
Alexander Lex <alex@seas.harvard.edu>
2009-05-14 13:36:29 +0000

    Forgot to add files
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@2046 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ org.caleydo.core/src/org/caleydo/core/view/opengl/util/overlay/contextmenu/IContextMenuEntry.java
@@ -0,0 +1,10 @@
+package org.caleydo.core.view.opengl.util.overlay.contextmenu;
+
+/**
+ * Base interface for entries in context menus.
+ * @author Alexander Lex
+ *
+ */
+public interface IContextMenuEntry {
+
+}
---
+++ org.caleydo.core/src/org/caleydo/core/view/opengl/util/overlay/contextmenu/Separator.java
@@ -0,0 +1,11 @@
+package org.caleydo.core.view.opengl.util.overlay.contextmenu;
+
+/**
+ * Implementation of IContextMenuEntry for a separator.
+ * 
+ * @author Alexander Lex
+ */
+public class Separator
+	implements IContextMenuEntry {
+
+}
