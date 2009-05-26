9cacc8879274d2617f2ce5d52d06124e5597b308
Alexander Lex <alex@seas.harvard.edu>
2009-05-26 15:41:06 +0000

    added website maintain script changed dates in html files minor changes in website
    git-svn-id: svn://localhost:8080/caleydo_main/webpage@2119 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ update_website.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+# Copyright (C) 2009 Alexander Lex - alexander.lex@icg.tugraz.at
+#
+# this script expects to be run from the root website svn repository directory
+
+
+
+mount_point="/mnt/webdav"
+htdocs=$mount_point"/htdocs"
+
+mount_website()
+{
+  already_mounted=false
+  for i in `cat /proc/mounts | cut -d' ' -f2`; do
+    if [ "$mount_point" = "$i" ]; then
+      already_mounted=true
+      echo "$mount_point is already a mount point"    
+    fi
+  done
+
+  if [ "$already_mounted" = "false" ]; then
+    mkdir -p $mount_points
+    sudo mount -t davfs https://caleydo.icg.tugraz.at/dav/ $mount_point
+  fi
+} 
+
+update_website()
+{
+  current_directory=$PWD
+  echo $current_directory
+  cd $htdocs
+  ls | grep -v download | xargs sudo rm -R
+  sudo rm downloads.html
+  cd $mount_point
+  sudo rm images -R
+  cd $current_directory
+  sudo cp htdocs/* $htdocs -Rv
+  sudo cp images $mount_point -Rv
+}
+
+purge_svn_folders()
+{
+  sudo find $mount_point -type d -name .svn -exec rm -rv {} \;
+  #sudo find $mount_point -type d -name .svn
+  #sudo rm -rfv `find $mount_point -type d -name .svn` 
+}
+
+update_dates()
+{
+  directory="htdocs"
+  todays_date=$(date +%Y-%m-%d)
+
+  find $directory -type f -name *.html -exec grep -l '20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]' {} \; | while read file
+  do
+      sed "s/20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]/$todays_date/ig" "$file" > tmp
+      mv tmp "$file"
+  done
+}
+
+
+print_help()
+{
+  echo "changes the date stamp in the files and copies them to the website"
+  echo "Usage: update_website.sh [ option ]"
+  echo ""
+  echo "Options:"
+  echo "-d update the dates of the html pages"
+  echo "-c copy website"
+  echo "-p purge svn folders"
+  echo ""
+  echo "If none of the options is present first the dates are updated and then the website is copied"
+}
+
+
+if [ $# -gt 0 ]
+then
+  count=0
+  while [ "$count" -lt $# ]
+  do
+    case $1 in
+      # update dates
+      -d) update_dates;;
+      # copy website
+      -c) mount_website
+	  update_website
+	  purge_svn_folders;;
+      -p) mount_website
+	  purge_svn_folders;;
+      # command not recognized
+      *) print_help;;
+    esac
+    shift
+    count=$(($count + 1))
+  done
+else
+  update_dates
+  mount_website
+  update_website
+  purge_svn_folders
+fi
