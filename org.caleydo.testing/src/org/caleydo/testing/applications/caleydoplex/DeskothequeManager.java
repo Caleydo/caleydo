c633423a1d92b22747303b733f645a3a71d1ecd2
waldner <waldner>
2009-08-20 08:53:46 +0000

    Testing Caleydoplex: tested multiple instances of "Caleydo" to connect to Deskotheque. The following features are working:  - Port numbers are dynamically chosen / rejected in a try-catch block.  - The connection to Deskotheque needs to be manually triggered as we need to send the window extents --> move the window to the desired display and then press "connect to Deskotheque"  - Deskotheque returns the correct target list and home / public instance. The public instance calculation takes the first connected Caleydo client from a public display up to now. In the future, this should return the "most suitable" public target (i.e. largest display and best visible).  - The client is unregistered from Deskotheque before disconnecting.
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@2356 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.testing/src/org/caleydo/testing/applications/caleydoplex/DeskothequeManager.java
+++ org.caleydo.testing/src/org/caleydo/testing/applications/caleydoplex/DeskothequeManager.java
@@ -10,77 +10,119 @@ import DKT.ServerApplicationIPrxHelper;
 import Ice.Communicator;
 
 public class DeskothequeManager {
-	
-	private GroupwareClientAppIPrx groupwareClientPrx; 
-	
-	private MasterApplicationIPrx masterPrx; 
-	
+
+	private GroupwareClientAppIPrx groupwareClientPrx;
+
+	private MasterApplicationIPrx masterPrx;
+
 	private Communicator communicator;
 
-	
-	public void establishConnection() {
-		GroupwareClient groupwareClient = new GroupwareClient();
-		
-		communicator = Ice.Util.initialize();
-		Ice.ObjectAdapter adapter = communicator
-				.createObjectAdapterWithEndpoints("GroupwareClient",
-						"default -p 8050");
-		Ice.ObjectPrx objPrx = adapter.add(groupwareClient, communicator
-				.stringToIdentity("GroupwareClientAppI"));
-		groupwareClientPrx = GroupwareClientAppIPrxHelper.checkedCast(objPrx);
-		adapter.activate();
-		
-		String serverName = "ServerAppI-fcggpc203-1"; 
-		String serverEndPoint = "tcp -h fcggpc203 -p 8011"; 
-		
-		Ice.ObjectPrx proxy = communicator.stringToProxy(serverName + ":" + serverEndPoint); 
-		ServerApplicationIPrx serverPrx = ServerApplicationIPrxHelper.checkedCast(proxy); 
-		
-		masterPrx = serverPrx.getMasterProxy(); 
-		
-		// testing communication 
-		
-		// registration at master proxy 
-		// FIXME: real window extents required! 
-		GroupwareInformation info = masterPrx.registerGroupwareClient(groupwareClientPrx, "Caleydo", serverPrx, 0, 0, 100, 100);
-		
-		System.out.println("Groupware information: displayID: " + info.displayID
-				+ ", is private: " + info.isPrivate + ", deskoXID: " + info.deskoXID);
-		
-		// obtaining resource manager proxy 
-		ResourceManagerIPrx resourceManagerPrx = masterPrx.getResourceManagerProxy(); 
-		
-		// getting available target locations 
-		String[] targetClients = resourceManagerPrx.getAvailableGroupwareClients(info.deskoXID); 
-		if(targetClients.length == 0){
-			System.out.println("No target clients found"); 
+	String deskoID;
+
+	public void establishConnection(int x, int y, int w, int h) {
+
+		// check if there is already a connection 
+		if (this.masterPrx == null) {
+
+			GroupwareClient groupwareClient = new GroupwareClient();
+
+			communicator = Ice.Util.initialize();
+			// this port number is pre-defined in Deskotheque and usually
+			// needs to be retrieved from the Deskotheque application
+			// - as this is not possible from Java, we need to hardcode the port
+			int port = 8050;
+			Ice.ObjectAdapter adapter = null;
+			boolean loop = true;
+
+			// we need this loop as the port has to be unique
+			// --> if more than one Caleydo instance is running,
+			// the port number needs to be incremented!
+			while(loop) {
+				try {
+					System.out.println("Using port " + port);
+					adapter = communicator.createObjectAdapterWithEndpoints(
+							"GroupwareClient", "default -p " + port);
+					loop = false;
+				} catch (Exception e) {
+					System.out.println("Port " + port + " already in use");
+					// e.printStackTrace();
+				}
+				port ++;
+			}
+			Ice.ObjectPrx objPrx = adapter.add(groupwareClient, communicator
+					.stringToIdentity("GroupwareClientAppI"));
+			groupwareClientPrx = GroupwareClientAppIPrxHelper
+					.checkedCast(objPrx);
+			adapter.activate();
+
+			// FIXME: hostname is still hardcoded
+			String serverName = "ServerAppI-fcggpc203-1";
+			String serverEndPoint = "tcp -h fcggpc203 -p 8011";
+
+			Ice.ObjectPrx proxy = communicator.stringToProxy(serverName + ":"
+					+ serverEndPoint);
+			ServerApplicationIPrx serverPrx = ServerApplicationIPrxHelper
+					.checkedCast(proxy);
+
+			masterPrx = serverPrx.getMasterProxy();
+
+			// testing communication
+
+			// registration at master proxy
+			GroupwareInformation info = masterPrx.registerGroupwareClient(
+					groupwareClientPrx, "Caleydo", serverPrx, x, y, w, h);
+
+			System.out.println("Groupware information: displayID: "
+					+ info.displayID + ", is private: " + info.isPrivate
+					+ ", deskoXID: " + info.deskoXID);
+
+			// info.deskoXID is the unique identifier for Deskotheque
+			this.deskoID = info.deskoXID;
+
+			// obtaining resource manager proxy
+			ResourceManagerIPrx resourceManagerPrx = masterPrx
+					.getResourceManagerProxy();
+
+			// getting available target locations
+			String[] targetClients = resourceManagerPrx
+					.getAvailableGroupwareClients(info.deskoXID);
+			if (targetClients.length == 0) {
+				System.out.println("No target clients found");
+			}
+			for (int i = 0; i < targetClients.length; i++) {
+				System.out.println("Target client [" + i + "]: "
+						+ targetClients[i]);
+			}
+
+			// get home of groupware client
+			String homeClient = resourceManagerPrx
+					.getHomeGroupwareClient(info.deskoXID);
+			System.out.println("Home client: " + homeClient);
+
+			// get public groupware client
+			String publicClient = resourceManagerPrx
+					.getPublicGroupwareClient(info.deskoXID);
+			System.out.println("Public client: " + publicClient);
+		} else {
+			System.out.println("Already established connection with ID "
+					+ this.deskoID);
 		}
-		for(int i = 0; i < targetClients.length; i++){
-			System.out.println("Target client [" + i + "]: " + targetClients); 
-		}
-		
-		// get home of groupware client 
-		String homeClient = resourceManagerPrx.getHomeGroupwareClient(info.deskoXID); 
-		System.out.println("Home client: " + homeClient);
-		
-		// get public groupware client 
-		String publicClient = resourceManagerPrx.getPublicGroupwareClient(info.deskoXID); 
-		System.out.println("Public client: " + publicClient); 
-		
-		// unregister groupware client 
-		resourceManagerPrx.unregisterGroupwareClient(info.deskoXID); 
-		
-		//System.out.println("Master hostname: " + masterPrx.getLocalHostName()); 
 	}
-	
+
 	public void destroy() {
+		if (this.masterPrx != null) {
+			// unregister at resource manager
+			ResourceManagerIPrx resourceManagerPrx = masterPrx
+					.getResourceManagerProxy();
+			resourceManagerPrx.unregisterGroupwareClient(this.deskoID);
+		}
 		if (communicator != null) {
-            try {
-            	communicator.destroy();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+			try {
+				communicator.destroy();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
--- org.caleydo.testing/src/org/caleydo/testing/applications/caleydoplex/Main.java
+++ org.caleydo.testing/src/org/caleydo/testing/applications/caleydoplex/Main.java
@@ -1,7 +1,5 @@
 package org.caleydo.testing.applications.caleydoplex;
 
-
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -12,30 +10,35 @@ import org.eclipse.swt.widgets.Shell;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Display display = new Display ();
+
+		Display display = new Display();
 		Shell shell = new Shell(display);
-		shell.setLayout (new GridLayout());
-		
+		shell.setLayout(new GridLayout());
+
+		Button connectButton = new Button(shell, SWT.PUSH);
+		connectButton.setText("connect to Deskotheque");
+
 		Button publishButton = new Button(shell, SWT.PUSH);
 		publishButton.setText("publish");
 		publishButton.addSelectionListener(new PublishListener());
-		
+
 		Button obtainButton = new Button(shell, SWT.PUSH);
 		obtainButton.setText("obtain");
 		obtainButton.addSelectionListener(new ObtainListener());
 
 		Label label = new Label(shell, SWT.NULL);
 		label.setText("<no message>");
-		
+
 		shell.setSize(300, 300);
-		shell.open ();
+		shell.open();
 
 		DeskothequeManager deskothequeManager = new DeskothequeManager();
-		deskothequeManager.establishConnection();
+		connectButton.addSelectionListener(new ConnectListener(shell,
+				deskothequeManager));
 
-		while (!shell.isDisposed ()) {
-			if (!display.readAndDispatch ()) display.sleep ();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
 		}
 		deskothequeManager.destroy();
 
