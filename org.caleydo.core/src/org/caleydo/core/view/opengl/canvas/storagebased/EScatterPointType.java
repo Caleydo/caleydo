451a5a2f20ddc0760247cc5210e560e03c8b9bdb
pillhof <pillhof>
2009-11-12 16:24:20 +0000

    git-svn-id: svn://localhost:8080/caleydo_main/trunk@2579 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ org.caleydo.core/src/org/caleydo/core/view/opengl/canvas/storagebased/EScatterPointType.java
@@ -0,0 +1,13 @@
+package org.caleydo.core.view.opengl.canvas.storagebased;
+
+/**
+ * Renderstyle of Scatterplot-Points
+ * 
+ * @author Jürgen Pillhofer
+ */
+public enum EScatterPointType {
+	BOX,
+	CIRCLE,
+	CROSS,
+	POINT
+}
