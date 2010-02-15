f433c2fc0fdcafa51fc3ccb8f7891215a1befbbe
Marc Streit <marc.streit@jku.at>
2010-02-15 23:23:49 +0000

    Implemented proof of concept for t-test of two int arrays using R. The result is read back to the java test application. This should be everything we need for a full integration of R to Caleydo.
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@2903 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.core/src/org/caleydo/core/application/core/RTest.java
+++ org.caleydo.core/src/org/caleydo/core/application/core/RTest.java
@@ -80,56 +80,67 @@ public class RTest {
 		/* High-level API - do not use RNI methods unless there is no other way
 			to accomplish what you want */
 		try {
-			REXP x;
-			re.eval("data(iris)",false);
-			System.out.println(x=re.eval("iris"));
-			// generic vectors are RVector to accomodate names
-			RVector v = x.asVector();
-			if (v.getNames()!=null) {
-				System.out.println("has names:");
-				for (Enumeration e = v.getNames().elements() ; e.hasMoreElements() ;) {
-					System.out.println(e.nextElement());
-				}
-			}
-			// for compatibility with Rserve we allow casting of vectors to lists
-			RList vl = x.asList();
-			String[] k = vl.keys();
-			if (k!=null) {
-				System.out.println("and once again from the list:");
-				int i=0; while (i<k.length) System.out.println(k[i++]);
-			}			
-
-			// get boolean array
-			System.out.println(x=re.eval("iris[[1]]>mean(iris[[1]])"));
-			// R knows about TRUE/FALSE/NA, so we cannot use boolean[] this way
-			// instead, we use int[] which is more convenient (and what R uses internally anyway)
-			int[] bi = x.asIntArray();
-			{
-			    int i = 0; while (i<bi.length) { System.out.print(bi[i]==0?"F ":(bi[i]==1?"T ":"NA ")); i++; }
-			    System.out.println("");
-			}
+			REXP test;
+			int[] array = new int[]{1,2,4};
+			int[] array_2 = new int[]{5,7,8};
+			re.assign("my_array", array);
+			re.assign("my_array_2", array_2);
 			
-			// push a boolean array
-			boolean by[] = { true, false, false };
-			re.assign("bool", by);
-			System.out.println(x=re.eval("bool"));
-			// asBool returns the first element of the array as RBool
-			// (mostly useful for boolean arrays of the length 1). is should return true
-			System.out.println("isTRUE? "+x.asBool().isTRUE());
-
-			// now for a real dotted-pair list:
-			System.out.println(x=re.eval("pairlist(a=1,b='foo',c=1:5)"));
-			RList l = x.asList();
-			if (l!=null) {
-				int i=0;
-				String [] a = l.keys();
-				System.out.println("Keys:");
-				while (i<a.length) System.out.println(a[i++]);
-				System.out.println("Contents:");
-				i=0;
-				while (i<a.length) System.out.println(l.at(i++));
-			}
-			System.out.println(re.eval("sqrt(36)"));
+			System.out.println("Array: " +re.eval("my_array"));
+			System.out.println("Array 2: " +re.eval("my_array_2"));
+			System.out.println("T-Test result: "+re.eval("t.test(my_array,my_array_2)"));
+			
+//			REXP x;
+//			re.eval("data(iris)",false);
+//			System.out.println(x=re.eval("iris"));
+//		
+//			// generic vectors are RVector to accomodate names
+//			RVector v = x.asVector();
+//			if (v.getNames()!=null) {
+//				System.out.println("has names:");
+//				for (Enumeration e = v.getNames().elements() ; e.hasMoreElements() ;) {
+//					System.out.println(e.nextElement());
+//				}
+//			}
+//			// for compatibility with Rserve we allow casting of vectors to lists
+//			RList vl = x.asList();
+//			String[] k = vl.keys();
+//			if (k!=null) {
+//				System.out.println("and once again from the list:");
+//				int i=0; while (i<k.length) System.out.println(k[i++]);
+//			}			
+//
+//			// get boolean array
+//			System.out.println(x=re.eval("iris[[1]]>mean(iris[[1]])"));
+//			// R knows about TRUE/FALSE/NA, so we cannot use boolean[] this way
+//			// instead, we use int[] which is more convenient (and what R uses internally anyway)
+//			int[] bi = x.asIntArray();
+//			{
+//			    int i = 0; while (i<bi.length) { System.out.print(bi[i]==0?"F ":(bi[i]==1?"T ":"NA ")); i++; }
+//			    System.out.println("");
+//			}
+//			
+//			// push a boolean array
+//			boolean by[] = { true, false, false };
+//			re.assign("bool", by);
+//			System.out.println(x=re.eval("bool"));
+//			// asBool returns the first element of the array as RBool
+//			// (mostly useful for boolean arrays of the length 1). is should return true
+//			System.out.println("isTRUE? "+x.asBool().isTRUE());
+//
+//			// now for a real dotted-pair list:
+//			System.out.println(x=re.eval("pairlist(a=1,b='foo',c=1:5)"));
+//			RList l = x.asList();
+//			if (l!=null) {
+//				int i=0;
+//				String [] a = l.keys();
+//				System.out.println("Keys:");
+//				while (i<a.length) System.out.println(a[i++]);
+//				System.out.println("Contents:");
+//				i=0;
+//				while (i<a.length) System.out.println(l.at(i++));
+//			}
+//			System.out.println(re.eval("sqrt(36)"));
 		} catch (Exception e) {
 			System.out.println("EX:"+e);
 			e.printStackTrace();
