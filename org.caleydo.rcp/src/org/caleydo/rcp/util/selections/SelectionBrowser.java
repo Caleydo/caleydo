bc51a1ef327235d9803b2163bf0bffd15ebc7f2c
pillhof <pillhof>
2010-03-17 17:12:28 +0000

    git-svn-id: svn://localhost:8080/caleydo_main/trunk@3098 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.rcp/src/org/caleydo/rcp/util/selections/SelectionBrowser.java
+++ org.caleydo.rcp/src/org/caleydo/rcp/util/selections/SelectionBrowser.java
@@ -1,12 +1,23 @@
 package org.caleydo.rcp.util.selections;
 
+import java.util.ArrayList;
+
+import org.caleydo.core.data.collection.ISet;
 import org.caleydo.core.data.mapping.EIDCategory;
+import org.caleydo.core.data.selection.ContentSelectionManager;
 import org.caleydo.core.data.selection.ContentVAType;
+
+import org.caleydo.core.data.selection.ContentVirtualArray;
+import org.caleydo.core.data.selection.ESelectionCommandType;
 import org.caleydo.core.data.selection.SelectionCommand;
+import org.caleydo.core.data.selection.SelectionType;
+import org.caleydo.core.data.selection.StorageSelectionManager;
 import org.caleydo.core.data.selection.delta.ContentVADelta;
 import org.caleydo.core.data.selection.delta.ISelectionDelta;
+import org.caleydo.core.data.selection.delta.SelectionDeltaItem;
 import org.caleydo.core.manager.IEventPublisher;
 import org.caleydo.core.manager.IGeneralManager;
+import org.caleydo.core.manager.IUseCase;
 import org.caleydo.core.manager.event.AEvent;
 import org.caleydo.core.manager.event.AEventListener;
 import org.caleydo.core.manager.event.IListenerOwner;
@@ -16,6 +27,7 @@ import org.caleydo.core.manager.event.view.storagebased.RedrawViewEvent;
 import org.caleydo.core.manager.event.view.storagebased.SelectionUpdateEvent;
 import org.caleydo.core.manager.event.view.storagebased.VirtualArrayUpdateEvent;
 import org.caleydo.core.manager.general.GeneralManager;
+import org.caleydo.core.manager.usecase.EDataDomain;
 import org.caleydo.core.view.opengl.canvas.listener.ClearSelectionsListener;
 import org.caleydo.core.view.opengl.canvas.listener.ContentVAUpdateListener;
 import org.caleydo.core.view.opengl.canvas.listener.IContentVAUpdateHandler;
@@ -26,8 +38,17 @@ import org.caleydo.core.view.opengl.canvas.listener.RedrawViewListener;
 import org.caleydo.core.view.opengl.canvas.listener.SelectionCommandListener;
 import org.caleydo.core.view.opengl.canvas.listener.SelectionUpdateListener;
 import org.caleydo.rcp.util.info.listener.InfoAreaUpdateListener;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.ui.PlatformUI;
 
 /**
@@ -42,6 +63,18 @@ public class SelectionBrowser
 
 	IGeneralManager generalManager = null;
 	IEventPublisher eventPublisher = null;
+	
+	ContentSelectionManager contentSelectionManager;
+	StorageSelectionManager storageSelectionManager;
+	
+	private Tree selectionTree;
+	private TreeItem contentTree;
+
+	
+	private Label lblTest;
+	private Button btnAdd;
+	private Button btnSub;;
+	private Composite parentComposite;
 
 	protected SelectionUpdateListener selectionUpdateListener;
 	protected ContentVAUpdateListener virtualArrayUpdateListener;
@@ -57,23 +90,200 @@ public class SelectionBrowser
 	public SelectionBrowser() {
 		generalManager = GeneralManager.get();
 		eventPublisher = generalManager.getEventPublisher();
-
+		
+		ContentVAType contentVAType = ContentVAType.CONTENT;				
+		IUseCase useCase = generalManager.getUseCase(EDataDomain.GENETIC_DATA);
+		
+		ContentVirtualArray contentVA= useCase.getContentVA(contentVAType);
+		contentSelectionManager = useCase.getContentSelectionManager();
+		contentSelectionManager.setVA(contentVA);
+		
+					
+			
 		registerEventListeners();
 	}
 
 	public Control createControl(final Composite parent) {
 
+		parentComposite = parent;
+		selectionTree = new Tree(parent, SWT.NULL  | SWT.MULTI );
+		
+		btnAdd = new Button(parent, SWT.WRAP);
+		btnAdd.setText("Add");
+		
+		btnSub = new Button(parent, SWT.WRAP);
+		btnSub.setText("Del");
+		
+		btnAdd.addSelectionListener(new SelectionListener() {
+
+		      public void widgetSelected(SelectionEvent event) {
+		    	  lblTest.setText("Add Clicked!");
+		      }
+
+		      public void widgetDefaultSelected(SelectionEvent event) {
+		    	  lblTest.setText("Add Default Clicked!");
+		      }
+		    });
+									
+		btnSub.addSelectionListener(new SelectionListener() {
+
+			 public void widgetSelected(SelectionEvent event) {
+		    	  lblTest.setText("Sub Clicked!");
+		      }
+
+		      public void widgetDefaultSelected(SelectionEvent event) {
+		    	  lblTest.setText("Sub Default Clicked!");
+		      }
+		    });
+		
+		
+		
+		lblTest = new Label(parent, SWT.WRAP);
+		lblTest.setAlignment(SWT.CENTER);		
+		lblTest.setText("This is a Test");
+	
+		
+		GridData gridData = new GridData(GridData.FILL_BOTH);
+
+	
+		gridData.minimumWidth = 145;
+		gridData.widthHint = 145;
+		gridData.minimumHeight = 82;
+		gridData.heightHint = 82;		
+
+		lblTest.setLayoutData(gridData);
+		
+		
+		
+		
+		gridData = new GridData(GridData.FILL_BOTH);
+		gridData.minimumHeight = 62;
+		gridData.heightHint = 156;
+		
+		if (System.getProperty("os.name").contains("Win")) {
+			// In windows the list needs more space because of no multi line
+			// support
+			gridData.widthHint = 145;
+			gridData.minimumWidth = 145;
+		}
+
+		
+
+		selectionTree.setLayoutData(gridData);
+		
+		contentTree = new TreeItem(selectionTree, SWT.NONE);
+		contentTree.setExpanded(true);
+		contentTree.setData(-1);
+		contentTree.setText("Content Selections");
+		
+		
+updateContentTree();
+		
 		return parent;
 	}
 
+	private void updateContentTree(){
+		ArrayList<SelectionType> sTypes = contentSelectionManager.getSelectionTypes();
+		Color color=null;
+		contentTree.removeAll(); 
+		for (SelectionType tmpSelectionType : sTypes) 
+		{
+								
+		if (SelectionType.isDefaultType(tmpSelectionType) || tmpSelectionType == SelectionType.DESELECTED)
+				continue;
+		
+		TreeItem item = new TreeItem(contentTree, SWT.NONE);
+		
+		float[] fArColor = tmpSelectionType.getColor();
+		
+		color =
+			new Color(parentComposite.getDisplay(), (int) (fArColor[0] * 255),
+				(int) (fArColor[1] * 255), (int) (fArColor[2] * 255));
+
+		item.setText(tmpSelectionType.toString());
+		item.setBackground(color);		
+		item.setData(tmpSelectionType.hashCode());
+		item.setData("selection_type", tmpSelectionType);
+
+		contentTree.setExpanded(true);
+		
+		
+		}
+		
+		
+		
+	}
+	
+	
 	@Override
 	public void handleSelectionUpdate(final ISelectionDelta selectionDelta, final boolean scrollToSelection,
 		final String info) {
+		
+		
+		parentComposite.getDisplay().asyncExec(new Runnable() {
+			public void run() {
 
+				updateContentTree();
+//				String tmpString="";
+//				if (info != null) {
+//					
+//					Color color=null;
+//					for (SelectionDeltaItem selectionItem : selectionDelta) {
+//						
+//						SelectionType currentSelection =selectionItem.getSelectionType();
+//						
+//						int i=contentSelectionManager.getNumberOfElements(currentSelection);
+//						
+//						tmpString=currentSelection.toString()+"("+i+")";
+//						
+//						float[] fArColor = currentSelection.getColor();
+//						
+//						color =
+//							new Color(parentComposite.getDisplay(), (int) (fArColor[0] * 255),
+//								(int) (fArColor[1] * 255), (int) (fArColor[2] * 255));
+//
+//						
+//
+//					}
+//					lblTest.setText(tmpString);
+//					lblTest.setBackground(color);
+//				}
+//			
+		}
+	
+		});
 	}
 
 	@Override
 	public void handleSelectionCommand(EIDCategory category, final SelectionCommand selectionCommand) {
+		
+		if (parentComposite.isDisposed())
+			return;
+
+		parentComposite.getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				ESelectionCommandType cmdType;
+				
+
+				
+		
+				
+				lblTest.setText("Reseted Selection");
+
+				cmdType = selectionCommand.getSelectionCommandType();
+				
+				
+				
+				if (cmdType == ESelectionCommandType.RESET || cmdType == ESelectionCommandType.CLEAR_ALL) {
+					lblTest.setText("Reseted Selection");
+				}
+//				else if (cmdType == ESelectionCommandType.CLEAR) {
+//					lblViewInfoContent.setText("Cleared current Selection");
+//					}
+
+				
+			}
+		});
 
 	}
 
@@ -99,6 +309,12 @@ public class SelectionBrowser
 	 *            short-info of the sender to display
 	 */
 	public void handleInfoAreaUpdate(final String info) {
+		parentComposite.getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				lblTest.setText("joschi");
+			}
+		});
+		
 
 	}
 
