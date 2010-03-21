e91efefeddbc540d15763f80ebd5f9c13b9ce32f
Alexander Lex <alex@seas.harvard.edu>
2010-03-21 11:53:10 +0000

    Moved tesselation initialization to init of renderer. Saves a lot of processing power.
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@3143 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.view.compare/src/org/caleydo/view/compare/renderer/CompareConnectionBandRenderer.java
+++ org.caleydo.view.compare/src/org/caleydo/view/compare/renderer/CompareConnectionBandRenderer.java
@@ -15,46 +15,24 @@ public class CompareConnectionBandRenderer
 			ICompareConnectionRenderer {
 
 	private GLU glu;
-	private int displayListID;
+	//private int displayListID;
+	
+	private GLUtessellator tobj;
 
 	public void init(GL gl) {
 		glu = new GLU();
 
-//		TesselationCallback tessCallback = new TesselationCallback(gl, glu);
-//
-//		double star[][] = new double[][]{// [5][6]; 6x5 in java
-//		{2.50, 5.00, 0.0, 1.0, 0.0, 1.0}, {3.250, 2.000, 0.0, 1.0, 1.0, 0.0},
-//				{4.000, 5.00, 0.0, 0.0, 1.0, 1.0},
-//				{2.500, 1.500, 0.0, 1.0, 0.0, 0.0},
-//				{4.000, 1.500, 0.0, 0.0, 1.0, 0.0}};
-//
-//		displayListID = gl.glGenLists(2);
-//
-//		GLUtessellator tobj = glu.gluNewTess();
-//
-//		glu.gluTessCallback(tobj, GLU.GLU_TESS_VERTEX, tessCallback);// vertexCallback);
-//		glu.gluTessCallback(tobj, GLU.GLU_TESS_BEGIN, tessCallback);// beginCallback);
-//		glu.gluTessCallback(tobj, GLU.GLU_TESS_END, tessCallback);// endCallback);
-//		glu.gluTessCallback(tobj, GLU.GLU_TESS_ERROR, tessCallback);// errorCallback);
-//		glu.gluTessCallback(tobj, GLU.GLU_TESS_COMBINE, tessCallback);// combineCallback);
-//
-//		/* smooth shaded, self-intersecting star */
-//		gl.glNewList(displayListID, GL.GL_COMPILE);
-//		gl.glShadeModel(GL.GL_SMOOTH);
-//		glu.gluTessProperty(tobj, //
-//				GLU.GLU_TESS_WINDING_RULE, //
-//				GLU.GLU_TESS_WINDING_POSITIVE);
-//		glu.gluTessBeginPolygon(tobj, null);
-//		glu.gluTessBeginContour(tobj);
-//		glu.gluTessVertex(tobj, star[0], 0, star[0]);
-//		glu.gluTessVertex(tobj, star[1], 0, star[1]);
-//		glu.gluTessVertex(tobj, star[2], 0, star[2]);
-//		glu.gluTessVertex(tobj, star[3], 0, star[3]);
-//		glu.gluTessVertex(tobj, star[4], 0, star[4]);
-//		glu.gluTessEndContour(tobj);
-//		glu.gluTessEndPolygon(tobj);
-//		gl.glEndList();
-//		glu.gluDeleteTess(tobj);
+		TesselationCallback tessCallback = new TesselationCallback(gl, glu);
+		
+		tobj = glu.gluNewTess();
+
+		glu.gluTessCallback(tobj, GLU.GLU_TESS_VERTEX, tessCallback);// vertexCallback);
+		glu.gluTessCallback(tobj, GLU.GLU_TESS_BEGIN, tessCallback);// beginCallback);
+		glu.gluTessCallback(tobj, GLU.GLU_TESS_END, tessCallback);// endCallback);
+		glu.gluTessCallback(tobj, GLU.GLU_TESS_ERROR, tessCallback);// errorCallback);
+		glu.gluTessCallback(tobj, GLU.GLU_TESS_COMBINE, tessCallback);// combineCallback);
+
+		gl.glShadeModel(GL.GL_SMOOTH);
 	}
 
 	public void display(GL gl) {
@@ -96,8 +74,6 @@ public class CompareConnectionBandRenderer
 	}
 	
 	public void render(GL gl, ArrayList<Vec3f> points) {
-		
-		TesselationCallback tessCallback = new TesselationCallback(gl, glu);
 
 		double inputPoints[][] = new double[points.size()][3];
 
@@ -106,19 +82,10 @@ public class CompareConnectionBandRenderer
 			inputPoints[pointIndex][1] = points.get(pointIndex).y();
 			inputPoints[pointIndex][2] = points.get(pointIndex).z();
 		}
-		
-		GLUtessellator tobj = glu.gluNewTess();
 
-		glu.gluTessCallback(tobj, GLU.GLU_TESS_VERTEX, tessCallback);// vertexCallback);
-		glu.gluTessCallback(tobj, GLU.GLU_TESS_BEGIN, tessCallback);// beginCallback);
-		glu.gluTessCallback(tobj, GLU.GLU_TESS_END, tessCallback);// endCallback);
-		glu.gluTessCallback(tobj, GLU.GLU_TESS_ERROR, tessCallback);// errorCallback);
-		glu.gluTessCallback(tobj, GLU.GLU_TESS_COMBINE, tessCallback);// combineCallback);
-
-		gl.glShadeModel(GL.GL_SMOOTH);
-//		glu.gluTessProperty(tobj, //
-//				GLU.GLU_TESS_WINDING_RULE, //
-//				GLU.GLU_TESS_WINDING_POSITIVE);
+		glu.gluTessProperty(tobj, //
+				GLU.GLU_TESS_WINDING_RULE, //
+				GLU.GLU_TESS_WINDING_POSITIVE);
 		glu.gluTessBeginPolygon(tobj, null);
 		glu.gluTessBeginContour(tobj);
 		
