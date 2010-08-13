85629128964dba0aac7e4bab129e7f0a5de240ce
Alexander Lex <alex@seas.harvard.edu>
2010-08-13 14:57:10 +0000

    Added RCPViewCreator
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@3523 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ org.caleydo.rcp/src/org/caleydo/rcp/view/creator/ARCPViewCreator.java
@@ -0,0 +1,17 @@
+package org.caleydo.rcp.view.creator;
+
+import org.caleydo.core.manager.view.creator.AViewCreator;
+import org.caleydo.rcp.view.rcp.CaleydoRCPViewPart;
+
+public abstract class ARCPViewCreator
+	extends AViewCreator {
+
+	public ARCPViewCreator(String viewType) {
+		super(viewType);		
+	}
+	
+	public abstract CaleydoRCPViewPart createView(int parentContainerID, String label);
+
+
+
+}
