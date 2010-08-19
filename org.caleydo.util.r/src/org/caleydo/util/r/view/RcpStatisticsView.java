ef2814d85fd47846e3cacaaa2f73845a0a32e23e
Alexander Lex <alex@seas.harvard.edu>
2010-08-19 14:25:25 +0000

    forgot changes in r package
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@3567 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.util.r/src/org/caleydo/util/r/view/RcpStatisticsView.java
+++ org.caleydo.util.r/src/org/caleydo/util/r/view/RcpStatisticsView.java
@@ -1,8 +1,8 @@
 package org.caleydo.util.r.view;
 
 import org.caleydo.core.manager.GeneralManager;
+import org.caleydo.core.manager.datadomain.ASetBasedDataDomain;
 import org.caleydo.core.manager.datadomain.DataDomainManager;
-import org.caleydo.core.manager.datadomain.ISetBasedDataDomain;
 import org.caleydo.rcp.view.rcp.CaleydoRCPViewPart;
 import org.eclipse.swt.widgets.Composite;
 
@@ -15,8 +15,9 @@ public class RcpStatisticsView extends CaleydoRCPViewPart {
 		statisticsView = (StatisticsView) GeneralManager.get().getViewGLCanvasManager()
 				.createView(StatisticsView.VIEW_ID, -1, "Statistics View");
 
-		ISetBasedDataDomain dataDomain = (ISetBasedDataDomain) DataDomainManager
-				.getInstance().getDataDomain("org.caleydo.datadomain.genetic");
+		ASetBasedDataDomain dataDomain = (ASetBasedDataDomain) DataDomainManager
+				.getInstance().getDataDomain(
+						determineDataDomain(new SerializedStatisticsView()));
 		statisticsView.initViewRCP(parent);
 		statisticsView.drawView();
 
@@ -35,8 +36,8 @@ public class RcpStatisticsView extends CaleydoRCPViewPart {
 	public void dispose() {
 		super.dispose();
 		statisticsView.unregisterEventListeners();
-		GeneralManager.get().getViewGLCanvasManager().unregisterItem(
-				statisticsView.getID());
+		GeneralManager.get().getViewGLCanvasManager()
+				.unregisterItem(statisticsView.getID());
 	}
 
 	public StatisticsView getTabularDataView() {
