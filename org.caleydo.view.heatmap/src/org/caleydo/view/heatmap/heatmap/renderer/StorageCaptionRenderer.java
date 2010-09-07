bbb10abaa508b190f26fa1dd99f09137da5100b7
Alexander Lex <alex@seas.harvard.edu>
2010-09-07 13:16:18 +0000

    Correct access to storage labels (dataDomains instead of using sets directly) plus some cleanup
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@3612 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.view.heatmap/src/org/caleydo/view/heatmap/heatmap/renderer/StorageCaptionRenderer.java
+++ org.caleydo.view.heatmap/src/org/caleydo/view/heatmap/heatmap/renderer/StorageCaptionRenderer.java
@@ -6,11 +6,16 @@ import javax.media.opengl.GL;
 
 import org.caleydo.core.data.selection.StorageVirtualArray;
 import org.caleydo.core.view.opengl.renderstyle.GeneralRenderStyle;
-import org.caleydo.core.view.opengl.util.GLHelperFunctions;
 import org.caleydo.core.view.opengl.util.text.CaleydoTextRenderer;
 import org.caleydo.view.heatmap.HeatMapRenderStyle;
 import org.caleydo.view.heatmap.heatmap.GLHeatMap;
 
+/**
+ * Rendering the storage captions for the GLHeatmap
+ * 
+ * @author Alexander Lex
+ * 
+ */
 public class StorageCaptionRenderer extends AContentRenderer {
 
 	private CaleydoTextRenderer textRenderer;
@@ -29,6 +34,7 @@ public class StorageCaptionRenderer extends AContentRenderer {
 	public void render(GL gl) {
 
 		StorageVirtualArray storageVA = heatMap.getStorageVA();
+		textRenderer.setColor(0, 0, 0, 1);
 
 		float xPosition = 0;
 		float fieldWidth = contentSpacing.getFieldWidth();
@@ -41,7 +47,7 @@ public class StorageCaptionRenderer extends AContentRenderer {
 		if (fieldWidth > height)
 			xOffset = (fieldWidth - height) / 2;
 		for (Integer storageID : storageVA) {
-			String label = heatMap.getSet().get(storageID).getLabel();
+			String label = heatMap.getDataDomain().getStorageLabel(storageID);
 
 			float fRotation = -90;
 			float yOffset = 0.57f;
@@ -56,66 +62,6 @@ public class StorageCaptionRenderer extends AContentRenderer {
 
 		}
 
-		textRenderer.setColor(0, 0, 0, 1);
-
 	}
-	// {
-	// float fFontScaling = 0;
-	//
-	// float fColumnDegrees = 0;
-	// float fLineDegrees = 0;
-	//
-	// fColumnDegrees = 90;// 60;
-	// fLineDegrees = 0;
-	// // render column captions
-	// if (heatMap.getDetailLevel() == EDetailLevel.HIGH
-	// && heatMap.getStorageVA().size() < 60) {
-	// if (iCount == heatMap.getContentVA().size()) {
-	// fXPosition = 0;
-	//
-	// if (heatMap.bClusterVisualizationExperimentsActive)
-	// gl.glTranslatef(+renderStyle
-	// .getWidthClusterVisualization(), 0, 0);
-	//
-	// for (Integer iStorageIndex : heatMap.getStorageVA()) {
-	// textRenderer.setColor(0, 0, 0, 1);
-	// renderCaption(gl, heatMap.getSet().get(iStorageIndex)
-	// .getLabel(), fXPosition + fFieldWidth / 2,
-	// fYPosition + 0.05f - 0.5f, 0, fColumnDegrees,
-	// renderStyle.getSmallFontScalingFactor());
-	// fXPosition += fFieldWidth;
-	// }
-	//
-	// if (heatMap.bClusterVisualizationExperimentsActive)
-	// gl.glTranslatef(-renderStyle
-	// .getWidthClusterVisualization(), 0, 0);
-	// }
-	// }
-	// }
-	//
-	// public void renderCaption(GL gl, String sLabel, float fXOrigin,
-	// float fYOrigin, float fZOrigin, float fRotation, float fFontScaling) {
-	// if (heatMap.isRenderedRemote()
-	// && heatMap.getRemoteRenderingGLCanvas().getViewType().equals(
-	// "org.caleydo.view.bucket"))
-	// fFontScaling *= 1.5;
-	// if (sLabel.length() > GeneralRenderStyle.NUM_CHAR_LIMIT + 1) {
-	// sLabel = sLabel.substring(0, GeneralRenderStyle.NUM_CHAR_LIMIT - 2);
-	// sLabel = sLabel + "..";
-	// }
-	//
-	// // textRenderer.setColor(0, 0, 0, 1);
-	// gl.glPushAttrib(GL.GL_CURRENT_BIT | GL.GL_LINE_BIT);
-	// gl.glTranslatef(fXOrigin, fYOrigin, fZOrigin);
-	// gl.glRotatef(fRotation, 0, 0, 1);
-	// textRenderer.begin3DRendering();
-	// textRenderer.draw3D(gl, sLabel, 0, 0, 0, fFontScaling,
-	// HeatMapRenderStyle.LABEL_TEXT_MIN_SIZE);
-	// textRenderer.end3DRendering();
-	// gl.glRotatef(-fRotation, 0, 0, 1);
-	// gl.glTranslatef(-fXOrigin, -fYOrigin, -fZOrigin);
-	// // textRenderer.begin3DRendering();
-	// gl.glPopAttrib();
-	// }
 
 }
