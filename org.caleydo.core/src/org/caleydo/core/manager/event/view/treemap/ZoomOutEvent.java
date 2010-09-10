f4658716a7e67e1a51601654eedf9c0035ceb4ef
lafer <lafer>
2010-09-10 15:00:40 +0000

    ZoomOutEvent for treemap created
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@3638 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ org.caleydo.core/src/org/caleydo/core/manager/event/view/treemap/ZoomOutEvent.java
@@ -0,0 +1,18 @@
+package org.caleydo.core.manager.event.view.treemap;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.caleydo.core.manager.event.AEvent;
+
+@XmlRootElement
+@XmlType
+public class ZoomOutEvent
+	extends AEvent {
+
+	@Override
+	public boolean checkIntegrity() {
+		return true;
+	}
+
+}
