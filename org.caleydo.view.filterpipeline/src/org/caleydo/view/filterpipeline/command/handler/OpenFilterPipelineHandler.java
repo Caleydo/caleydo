9e057db5b894076c64f6144b646830bd573bf63c
Marc Streit <marc.streit@jku.at>
2010-12-06 13:10:29 +0000

    Filter pipeline view supports multiple instances.
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@3760 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.view.filterpipeline/src/org/caleydo/view/filterpipeline/command/handler/OpenFilterPipelineHandler.java
+++ org.caleydo.view.filterpipeline/src/org/caleydo/view/filterpipeline/command/handler/OpenFilterPipelineHandler.java
@@ -5,16 +5,26 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 public class OpenFilterPipelineHandler extends AbstractHandler implements IHandler {
 
-	@Override
+	/**
+	 * Counter variable for determination of the secondary view ID.
+	 * Needed for multiple instances of the same view type.
+	 */
+	private static int SECONDARY_ID = 0;
+	
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		try {
+
 			HandlerUtil.getActiveWorkbenchWindow(event).getActivePage()
-					.showView(GLFilterPipeline.VIEW_ID);
+					.showView(GLFilterPipeline.VIEW_ID, Integer.toString(SECONDARY_ID), IWorkbenchPage.VIEW_ACTIVATE);
+			
+			SECONDARY_ID++;
+			
 		} catch (PartInitException e) {
 			e.printStackTrace();
 		}
