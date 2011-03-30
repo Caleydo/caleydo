81af05f4aeead858a194d125e23a80e68265f3d2
Marc Streit <marc.streit@jku.at>
2011-03-30 08:07:03 +0000

    fixed loading of GO CC mappings
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@4020 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.core/src/org/caleydo/core/data/collection/set/statistics/IDBasedBinning.java
+++ org.caleydo.core/src/org/caleydo/core/data/collection/set/statistics/IDBasedBinning.java
@@ -23,23 +23,28 @@ public class IDBasedBinning {
 		if (bin.isEmpty()) {
 			ArrayList<Integer> nonMappingIDs = new ArrayList<Integer>();
 			for (Integer contentID : contenVA) {
-				Set<String> mappedIDs = mappingManager.getIDAsSet(sourceType, targetType, contentID);
-				if (mappedIDs == null) {
-					nonMappingIDs.add(contentID);
-					bin.put("Other", nonMappingIDs);
-					continue;
-				}
-				for (String id : mappedIDs) {
-					if (!bin.containsKey(id)) {
-						ArrayList<Integer> ids = new ArrayList<Integer>();
-						ids.add(contentID);
-						bin.put(id, ids);
+				
+				Set<String> refSeqIDs = mappingManager.getIDAsSet(sourceType, IDType.getIDType("REFSEQ_MRNA"), contentID);
+				
+				for (String refSeq : refSeqIDs) {
+					Set<String> mappedIDs = mappingManager.getIDAsSet(IDType.getIDType("REFSEQ_MRNA"), targetType, refSeq);
+					if (mappedIDs == null) {
+						nonMappingIDs.add(contentID);
+						bin.put("Other", nonMappingIDs);
+						continue;
 					}
-					else {
-						ArrayList<Integer> ids = bin.get(id);
-						ids.add(contentID);
-						bin.put(id, ids);
-					}
+					for (String id : mappedIDs) {
+						if (!bin.containsKey(id)) {
+							ArrayList<Integer> ids = new ArrayList<Integer>();
+							ids.add(contentID);
+							bin.put(id, ids);
+						}
+						else {
+							ArrayList<Integer> ids = bin.get(id);
+							ids.add(contentID);
+							bin.put(id, ids);
+						}
+					}					
 				}
 			}
 		}
