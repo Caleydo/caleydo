0b9452f6161184969fad60f1c08281ecae6d0345
Christian Partl <partl@icg.tugraz.at>
2011-08-12 15:00:36 +0000

    continued working on edge routing in data graph
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@4243 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.view.datagraph/src/org/caleydo/view/datagraph/GLDataGraph.java
+++ org.caleydo.view.datagraph/src/org/caleydo/view/datagraph/GLDataGraph.java
@@ -439,12 +439,12 @@ public class GLDataGraph extends AGLView implements IViewCommandHandler {
 		}
 		gl.glEnd();
 
-		gl.glPointSize(5);
-		gl.glColor3f(0, 0, 1);
-		gl.glBegin(GL2.GL_POINTS);
-		gl.glVertex2d(ctrlx1, ctrly1);
-		gl.glVertex2d(ctrlx2, ctrly2);
-		gl.glEnd();
+		// gl.glPointSize(5);
+		// gl.glColor3f(0, 0, 1);
+		// gl.glBegin(GL2.GL_POINTS);
+		// gl.glVertex2d(ctrlx1, ctrly1);
+		// gl.glVertex2d(ctrlx2, ctrly2);
+		// gl.glEnd();
 		// curve.setCurve(v1.getX(), v1.getY(), ctrlx1, ctrly1, ctrlx2, ctrly2,
 		// v2.getX(), v2.getY());
 	}
@@ -541,6 +541,14 @@ public class GLDataGraph extends AGLView implements IViewCommandHandler {
 		edgePoints.add(node2BundlingPoint);
 
 		createEdge(gl, edgePoints);
+
+		edgePoints.add(edgePoints.get(edgePoints.size() - 1));
+		edgePoints.add(0, edgePoints.get(0));
+
+		for (int i = 0; i < edgePoints.size() - 3; i++) {
+			computeOneSpline(edgePoints.get(i), edgePoints.get(i + 1),
+					edgePoints.get(i + 2), edgePoints.get(i + 3), gl);
+		}
 
 		// Set<ADimensionGroupData> dimensionGroups1 =
 		// node1.getDimensionGroups();
@@ -664,6 +672,8 @@ public class GLDataGraph extends AGLView implements IViewCommandHandler {
 		if (edgePoints == null || edgePoints.size() < 2)
 			return;
 
+		Map<Point2D, IDataGraphNode> pointsOnBoundingBoxes = new HashMap<Point2D, IDataGraphNode>();
+
 		for (int i = 1; i < edgePoints.size(); i++) {
 			Point2D point1 = edgePoints.get(i - 1);
 			Point2D point2 = edgePoints.get(i);
@@ -673,35 +683,57 @@ public class GLDataGraph extends AGLView implements IViewCommandHandler {
 				int code1 = box.outcode(point1);
 				int code2 = box.outcode(point2);
 
-				if (((code1 & code2) != 0) || (code1 == 0) || (code2 == 0)) {
+				boolean isPoint1OnBoundingBox = (pointsOnBoundingBoxes
+						.get(point1) == node);
+				boolean isPoint2OnBoundingBox = (pointsOnBoundingBoxes
+						.get(point2) == node);
+
+				if (((code1 & code2) != 0)
+						|| (code1 == 0 && !isPoint1OnBoundingBox)
+						|| (code2 == 0 && !isPoint2OnBoundingBox)
+						|| (isPoint1OnBoundingBox && isPoint2OnBoundingBox)) {
 					continue;
 				}
 
-				Point2D intersection1 = calcIntersectionPoint(point1, point2,
-						box, code1);
-				Point2D intersection2 = calcIntersectionPoint(point2, point1,
-						box, code2);
+				Point2D intersection1 = (isPoint1OnBoundingBox) ? (point1)
+						: (calcIntersectionPoint(point1, point2, box, code1));
+				Point2D intersection2 = (isPoint2OnBoundingBox) ? (point2)
+						: (calcIntersectionPoint(point2, point1, box, code2));
 
 				if (intersection1 != null && intersection2 != null) {
 
+					if (intersection1.getX() == intersection2.getX()
+							&& intersection1.getY() == intersection2.getY()) {
+						continue;
+					}
+
 					Point2D[] corners = new Point2D[4];
-					corners[0] = new Point2D.Double(box.getMinX() - 0.001,
-							box.getMinY() - 0.001);
-					corners[1] = new Point2D.Double(box.getMaxX() + 0.001,
-							box.getMinY() - 0.001);
-					corners[2] = new Point2D.Double(box.getMaxX() + 0.001,
-							box.getMaxY() + 0.001);
-					corners[3] = new Point2D.Double(box.getMinX() - 0.001,
-							box.getMaxY() + 0.001);
+					// corners[0] = new Point2D.Double(box.getMinX() - 0.001,
+					// box.getMinY() - 0.001);
+					// corners[1] = new Point2D.Double(box.getMaxX() + 0.001,
+					// box.getMinY() - 0.001);
+					// corners[2] = new Point2D.Double(box.getMaxX() + 0.001,
+					// box.getMaxY() + 0.001);
+					// corners[3] = new Point2D.Double(box.getMinX() - 0.001,
+					// box.getMaxY() + 0.001);
+					corners[0] = new Point2D.Double(box.getMinX(),
+							box.getMinY());
+					corners[1] = new Point2D.Double(box.getMaxX(),
+							box.getMinY());
+					corners[2] = new Point2D.Double(box.getMaxX(),
+							box.getMaxY());
+					corners[3] = new Point2D.Double(box.getMinX(),
+							box.getMaxY());
 
 					double minDistance = Double.MAX_VALUE;
 					Point2D bendPoint = null;
 
 					for (int j = 0; j < 4; j++) {
-						if ((corners[j].getX() == point1.getX() && corners[j]
-								.getY() == point1.getY())
+						if ((corners[j].getX() == point1.getX()
+								&& corners[j].getY() == point1.getY() && isPoint1OnBoundingBox)
 								|| (corners[j].getX() == point2.getX() && corners[j]
-										.getY() == point2.getY())) {
+										.getY() == point2.getY())
+								&& isPoint2OnBoundingBox) {
 							continue;
 						}
 						double currentSummedDistance = intersection1
@@ -717,7 +749,22 @@ public class GLDataGraph extends AGLView implements IViewCommandHandler {
 						System.out.println("null");
 					}
 
+					boolean isPointAlreadyAdded = false;
+
+					for (Point2D point : edgePoints) {
+						if (point.getX() == bendPoint.getX()
+								&& point.getY() == bendPoint.getY()) {
+							isPointAlreadyAdded = true;
+							break;
+						}
+					}
+
+					if (isPointAlreadyAdded) {
+						continue;
+					}
+
 					edgePoints.add(i, bendPoint);
+					pointsOnBoundingBoxes.put(bendPoint, node);
 					i--;
 
 					gl.glPointSize(5);
@@ -727,6 +774,7 @@ public class GLDataGraph extends AGLView implements IViewCommandHandler {
 					gl.glVertex2d(intersection2.getX(), intersection2.getY());
 					gl.glEnd();
 
+					gl.glLineWidth(1);
 					gl.glColor3f(0, 0, 1);
 					gl.glBegin(GL2.GL_LINE_LOOP);
 					gl.glVertex2d(box.getMinX(), box.getMinY());
@@ -740,12 +788,12 @@ public class GLDataGraph extends AGLView implements IViewCommandHandler {
 			}
 		}
 
-		gl.glColor3f(1, 0, 1);
-		gl.glBegin(GL2.GL_LINE_STRIP);
-		for (Point2D point : edgePoints) {
-			gl.glVertex2d(point.getX(), point.getY());
-		}
-		gl.glEnd();
+		// gl.glColor3f(1, 0, 1);
+		// gl.glBegin(GL2.GL_LINE_STRIP);
+		// for (Point2D point : edgePoints) {
+		// gl.glVertex2d(point.getX(), point.getY());
+		// }
+		// gl.glEnd();
 	}
 
 	private Point2D calcIntersectionPoint(Point2D point1, Point2D point2,
