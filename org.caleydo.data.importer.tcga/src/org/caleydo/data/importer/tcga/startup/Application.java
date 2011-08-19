a781c58feda14a6fcc51949421ac627847f99f11
Alexander Lex <alex@seas.harvard.edu>
2011-08-19 18:04:37 +0000

    first steps for tcga importer
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@4257 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ org.caleydo.data.importer.tcga/src/org/caleydo/data/importer/tcga/startup/Application.java
@@ -0,0 +1,11 @@
+package org.caleydo.data.importer.tcga.startup;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+		LoadDataParameters parameters;
+	}
+
+}
