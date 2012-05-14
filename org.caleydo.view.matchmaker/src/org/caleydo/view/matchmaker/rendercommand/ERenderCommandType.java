fcf5053781f36336550e1391390dcc1280d4bc6a
Marc Streit <marc.streit@jku.at>
2012-05-14 09:22:15 +0000

    Deleted matchmaker view plugin.
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@4833 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.view.matchmaker/src/org/caleydo/view/matchmaker/rendercommand/ERenderCommandType.java
+++
@@ -1,5 +0,0 @@
-package org.caleydo.view.matchmaker.rendercommand;
-
-public enum ERenderCommandType {
-	OVERVIEW_GROUP_BAR, OVERVIEW_HEATMAP, OVERVIEW_SLIDER, DETAIL_HEATMAPS, CAPTION_LABEL, DENDROGRAM_BUTTON, DENDROGRAM
-}
--- org.caleydo.view.matchmaker/src/org/caleydo/view/matchmaker/state/ECompareViewStateType.java
+++
@@ -1,5 +0,0 @@
-package org.caleydo.view.matchmaker.state;
-
-public enum ECompareViewStateType {
-	DETAIL_VIEW, OVERVIEW, OVERVIEW_TO_DETAIL_TRANSITION, DETAIL_TO_OVERVIEW_TRANSITION
-}
