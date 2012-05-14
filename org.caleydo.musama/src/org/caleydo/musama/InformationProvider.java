066239578d8832f4db07b7e9ef3b64af164de270
Marc Streit <marc.streit@jku.at>
2012-05-14 06:35:16 +0000

    Removed Caleydo-Musama bridge.
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@4827 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.musama/src/org/caleydo/musama/InformationProvider.java
+++
@@ -1,9 +0,0 @@
-package org.caleydo.musama;
-
-import de.mmis.core.base.event.Observable;
-
-public interface InformationProvider extends
-		Observable<InformationProviderEvent> {
-	public String getRole(String id);
-
-}
