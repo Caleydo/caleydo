9200812dc70d68d25615534f9f1e23a1dbbe22c6
Alexander Lex <alex@seas.harvard.edu>
2012-05-22 15:18:16 +0000

    Updated help link
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@4912 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.core/src/org/caleydo/core/gui/menu/HelpContentsHandler.java
+++ org.caleydo.core/src/org/caleydo/core/gui/menu/HelpContentsHandler.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
  * Caleydo - visualization for molecular biology - http://caleydo.org
- *  
+ * 
  * Copyright(C) 2005, 2012 Graz University of Technology, Marc Streit, Alexander
  * Lex, Christian Partl, Johannes Kepler University Linz </p>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- *  
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ * 
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>
  *******************************************************************************/
@@ -28,19 +28,17 @@ import org.eclipse.core.commands.IHandler;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-public class HelpContentsHandler
-	extends AbstractHandler
-	implements IHandler {
+public class HelpContentsHandler extends AbstractHandler implements IHandler {
 
-	private final static String URL_HELP_CONTENTS = "http://www.caleydo.org/help/help.html";
+	private final static String URL_HELP_CONTENTS = "http://www.icg.tugraz.at/project/caleydo/help/caleydo-2.0/caleydo-help";
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
 		try {
-			HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().showView("org.caleydo.view.browser");
-		}
-		catch (PartInitException e) {
+			HandlerUtil.getActiveWorkbenchWindow(event).getActivePage()
+					.showView("org.caleydo.view.browser");
+		} catch (PartInitException e) {
 			e.printStackTrace();
 		}
 
