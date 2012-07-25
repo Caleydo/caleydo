8c23dc18f2a6eb71291087441c0865c8fe9b59c2
Alexander Lex <alex@seas.harvard.edu>
2012-07-25 09:46:28 +0000

    Branched 2.01
    git-svn-id: svn://localhost:8080/caleydo_main/branches/stable_2.01@5069 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ org.caleydo.view.stratomex/src/org/caleydo/view/stratomex/brick/contextmenu/SplitBrickItem.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Caleydo - visualization for molecular biology - http://caleydo.org
+ *  
+ * Copyright(C) 2005, 2012 Graz University of Technology, Marc Streit, Alexander
+ * Lex, Christian Partl, Johannes Kepler University Linz </p>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *  
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>
+ *******************************************************************************/
+/**
+ * 
+ */
+package org.caleydo.view.stratomex.brick.contextmenu;
+
+import org.caleydo.core.view.contextmenu.AContextMenuItem;
+import org.caleydo.view.stratomex.event.SplitBrickEvent;
+
+/**
+ * @author alexsb
+ * 
+ */
+public class SplitBrickItem extends AContextMenuItem {
+
+	public SplitBrickItem(Integer connectionBandID, Boolean splitLeftBrick) {
+		if (splitLeftBrick)
+
+			setLabel("Split Left Brick");
+		else
+			setLabel("Split Right Brick");
+
+		SplitBrickEvent event = new SplitBrickEvent(connectionBandID, splitLeftBrick);
+		event.setSender(this);
+		registerEvent(event);
+	}
+}
