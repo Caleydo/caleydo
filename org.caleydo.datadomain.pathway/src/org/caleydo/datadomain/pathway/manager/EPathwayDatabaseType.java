d2188970b998dc1caa64d34745b320c171f3b0bb
Alexander Lex <alex@seas.harvard.edu>
2012-07-25 10:36:04 +0000

    Re-added branch
    git-svn-id: svn://localhost:8080/caleydo_main/branches/stable_2.01@5079 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ org.caleydo.datadomain.pathway/src/org/caleydo/datadomain/pathway/manager/EPathwayDatabaseType.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Caleydo - visualization for molecular biology - http://caleydo.org
+ *  
+ * Copyright(C) 2005, 2012 Graz University of Technology, Marc Streit, Alexander
+ * Lex, Christian Partl, Johannes Kepler University Linz </p>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *  
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>
+ *******************************************************************************/
+package org.caleydo.datadomain.pathway.manager;
+
+/**
+ * Class holds all supported pathway database types.
+ * 
+ * @author Marc Streit
+ */
+public enum EPathwayDatabaseType {
+
+	KEGG("KEGG", "www.genome.jp/kegg"), BIOCARTA("BioCarta", "www.biocarta.com");
+
+	private String name;
+
+	private String url;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param name
+	 * @param url
+	 */
+	private EPathwayDatabaseType(String name, String url) {
+
+		this.name = name;
+		this.url = url;
+	}
+
+	public String getName() {
+
+		return name;
+	}
+
+	public String getURL() {
+
+		return url;
+	}
+}
