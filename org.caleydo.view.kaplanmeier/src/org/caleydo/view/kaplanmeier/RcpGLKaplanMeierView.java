e24dc8b1912af6da153e5664d57bac7cf30bb780
Alexander Lex <alex@seas.harvard.edu>
2012-07-25 09:46:17 +0000

    Branched 2.01
    git-svn-id: svn://localhost:8080/caleydo_main/branches/stable_2.01@5065 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ org.caleydo.view.kaplanmeier/src/org/caleydo/view/kaplanmeier/RcpGLKaplanMeierView.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Caleydo - visualization for molecular biology - http://caleydo.org
+ *  
+ * Copyright(C) 2005, 2012 Graz University of Technology, Marc Streit, Alexander
+ * Lex, Christian Partl, Johannes Kepler University Linz </p>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *  
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>
+ *******************************************************************************/
+package org.caleydo.view.kaplanmeier;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import org.caleydo.core.view.ARcpGLViewPart;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * RCP Kaplan Meier view.
+ * 
+ * @author Marc Streit
+ */
+public class RcpGLKaplanMeierView extends ARcpGLViewPart {
+
+	/**
+	 * Constructor.
+	 */
+	public RcpGLKaplanMeierView() {
+		super();
+
+		try {
+			viewContext = JAXBContext.newInstance(SerializedKaplanMeierView.class);
+		} catch (JAXBException ex) {
+			throw new RuntimeException("Could not create JAXBContext", ex);
+		}
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
+
+		createGLCanvas();
+		view = new GLKaplanMeier(glCanvas, parentComposite,
+				serializedView.getViewFrustum());
+		initializeView();
+		createPartControlGL();
+	}
+
+	@Override
+	public void createDefaultSerializedView() {
+		serializedView = new SerializedKaplanMeierView();
+		determineDataConfiguration(serializedView);
+	}
+
+	@Override
+	public String getViewGUIID() {
+		return GLKaplanMeier.VIEW_TYPE;
+	}
+
+}
