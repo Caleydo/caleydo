6327a47f6f729ea61d2ac6d25d76f20c3db82137
Alexander Lex <alex@seas.harvard.edu>
2012-07-25 09:45:55 +0000

    Branched 2.01
    git-svn-id: svn://localhost:8080/caleydo_main/branches/stable_2.01@5058 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ org.caleydo.util.r/lib/windows/readme.txt
@@ -0,0 +1,3 @@
+the JRI dll files for windows are taken from the binary file:
+http://www.rforge.net/rJava/files/
+Currently commited version: 0.8.5
---
+++ org.caleydo.util.r/src/org/caleydo/util/r/RConsole.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * Caleydo - visualization for molecular biology - http://caleydo.org
+ *  
+ * Copyright(C) 2005, 2012 Graz University of Technology, Marc Streit, Alexander
+ * Lex, Christian Partl, Johannes Kepler University Linz </p>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *  
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>
+ *******************************************************************************/
+package org.caleydo.util.r;
+
+import java.awt.FileDialog;
+import java.awt.Frame;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import org.rosuda.JRI.RMainLoopCallbacks;
+import org.rosuda.JRI.Rengine;
+
+public class RConsole implements RMainLoopCallbacks {
+	@Override
+	public void rWriteConsole(Rengine re, String text, int oType) {
+		System.out.print(text);
+	}
+
+	@Override
+	public void rBusy(Rengine re, int which) {
+		System.out.println("rBusy(" + which + ")");
+	}
+
+	@Override
+	public String rReadConsole(Rengine re, String prompt, int addToHistory) {
+		System.out.print(prompt);
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String s = br.readLine();
+			return (s == null || s.length() == 0) ? s : s + "\n";
+		} catch (Exception e) {
+			System.out.println("jriReadConsole exception: " + e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public void rShowMessage(Rengine re, String message) {
+		System.out.println("rShowMessage \"" + message + "\"");
+	}
+
+	@Override
+	public String rChooseFile(Rengine re, int newFile) {
+		FileDialog fd = new FileDialog(new Frame(), (newFile == 0) ? "Select a file"
+				: "Select a new file", (newFile == 0) ? FileDialog.LOAD : FileDialog.SAVE);
+		fd.show();
+		String res = null;
+		if (fd.getDirectory() != null)
+			res = fd.getDirectory();
+		if (fd.getFile() != null)
+			res = (res == null) ? fd.getFile() : (res + fd.getFile());
+		return res;
+	}
+
+	@Override
+	public void rFlushConsole(Rengine re) {
+	}
+
+	@Override
+	public void rLoadHistory(Rengine re, String filename) {
+	}
+
+	@Override
+	public void rSaveHistory(Rengine re, String filename) {
+	}
+}
