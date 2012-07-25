0a51cc63ab211dc94c91943a767bb5ab7dd9bf37
Alexander Lex <alex@seas.harvard.edu>
2012-07-25 10:36:48 +0000

    Re-added branch
    git-svn-id: svn://localhost:8080/caleydo_main/branches/stable_2.01@5097 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

---
+++ org.caleydo.view.stratomex/src/org/caleydo/view/stratomex/brick/viewcreation/IRemoteViewCreator.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Caleydo - visualization for molecular biology - http://caleydo.org
+ *  
+ * Copyright(C) 2005, 2012 Graz University of Technology, Marc Streit, Alexander
+ * Lex, Christian Partl, Johannes Kepler University Linz </p>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *  
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>
+ *******************************************************************************/
+package org.caleydo.view.stratomex.brick.viewcreation;
+
+import javax.media.opengl.GL2;
+import org.caleydo.core.view.opengl.canvas.AGLView;
+import org.caleydo.core.view.opengl.mouse.GLMouseListener;
+import org.caleydo.view.stratomex.brick.GLBrick;
+
+/**
+ * Base class whose subclasses are intended to create remote views for a brick.
+ * 
+ * @author Christian Partl
+ * 
+ */
+public interface IRemoteViewCreator {
+
+	/**
+	 * Creates a remote view specified by the concrete subclass of
+	 * {@link IRemoteViewCreator}.
+	 * 
+	 * @param remoteRenderingView
+	 *            View that remote renders the view created by this method.
+	 * @param gl
+	 * @param glMouseListener
+	 * @return remote view.
+	 */
+	public AGLView createRemoteView(GLBrick remoteRenderingView, GL2 gl,
+			GLMouseListener glMouseListener);
+
+}
