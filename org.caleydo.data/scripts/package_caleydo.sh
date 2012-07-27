7d3b54f39089ed276862c22ba2a26ca19a1d130e
Alexander Lex <alex@seas.harvard.edu>
2012-07-27 12:33:02 +0000

    revised script cleaned up resources
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@5138 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.data/scripts/package_caleydo.sh
+++ org.caleydo.data/scripts/package_caleydo.sh
@@ -18,25 +18,40 @@
 # this program. If not, see <http://www.gnu.org/licenses/>
 #*******************************************************************************
 #!/bin/bash
-# Copyright (C) 2009 Alexander Lex - alexander.lex@icg.tugraz.at
+# Copyright (C) 2009, 2012 Alexander Lex - alexander.lex@icg.tugraz.at
 #
 
 
 # Requires alien to be installed. Run  sudo apt-get install alien
 
 mount_point="/mnt/webdav"
+# the folder on the webserver where the packages are placed
 download_folder=$mount_point"/download"
-
-
+# the folder where the binaries are after the eclipse export
 export_root=$HOME"/caleydo_export"
 
+# the folder where the debian package specific stuff is stored, which is relative to the location of this script
 debian_folder=$(pwd)"/debian"
-linux_folder=""
-linux_source_folder=""
-arch=""
 
-echo $debian_folder
+#======= The platform specific binary folders ========
 
+linux32_folder=$export_root"/linux.gtk.x86"
+linux64_folder=$export_root"/linux.gtk.x86_64"
+
+win32_folder=$export_root"/win32.win32.x86"
+win64_folder=$export_root"/win32.win32.x86_64"
+
+mac32_folder=$export_root"/macosx.cocoa.x86"
+mac32_folder=$export_root"/macosx.cocoa.x86_64"
+
+#======================================================
+
+# the name of the product, corresponding also to the directory inside the binary folders. 
+# eg $linux32_folder"/"$product should be the folder to the binaries
+product="caleydo"
+
+
+# mounts the web server locally
 mount_webdav()
 {
   already_mounted=false
@@ -52,11 +67,13 @@ mount_webdav()
   fi
 }
 
-copy_to_web()
+# copies the standalone packages to the web server
+copy_standalone()
 {
   sudo cp $export_path $download_folder -R -v
 }
 
+# copies the webstart parts to the webserver
 copy_webstart()
 {
   webstart_export_path=$export_root"/webstart_"$version_number
@@ -65,59 +82,107 @@ copy_webstart()
   sudo cp $webstart_export_path"/plugins/org.caleydo."* $webstart_plugins_folder -v
 }
 
-make_archive()
-{  
-  #echo $export_path
-  mkdir -p $export_path #{"win_vista","win_xp","linux"}
-
-  #echo $export_path/caleydo_$version_number"_linux_x86-32.tar.gz"
-  #echo $export_root/linux.gtk.x86/caleydo/*
-  tar -czvf $export_path/caleydo_$version_number"_linux_x86-32.tar.gz" -C $export_root/linux.gtk.x86/ caleydo
-  tar -czvf $export_path/caleydo_data_importer_$version_number"_linux_x86-32.tar.gz" -C $export_root/linux.gtk.x86/ caleydo_data_importer
+# moves the windows installer if it exists
+move_windows_installer()
+{
+ setup_file=$win32_folder"/"$product"/Setup.exe"
+ target_setup_file=$export_path"/"$product"_"$version_number"_installer_win_x86-32.exe"
+ move_file
  
-  tar -czvf $export_path/caleydo_$version_number"_linux_x86-64.tar.gz" -C $export_root/linux.gtk.x86_64/ caleydo
-  tar -czvf $export_path/caleydo_data_importer_$version_number"_linux_x86-64.tar.gz" -C $export_root/linux.gtk.x86_64/ caleydo_data_importer  
+ setup_file=$win64_folder"/"$product"/Setup.exe"
+ target_setup_file=$export_path"/"$product"_"$version_number"_installer_win_x86-64.exe"
+ move_file
+}
+# helper for moving a file if it exists
+move_file()
+{
+  if [ -f $setup_file ]
+  then
+      mv $setup_file $target_setup_file
+  else
+      echo "Error: Windows setup file does not exist: "$setup_file
+  fi
+}
+
+# create zip packages of the binaries
+create_zip_packages()
+{  
+  echo -n "Creating linux_x86-32 tar"
+  tar -czf $export_path/caleydo_$version_number"_linux_x86-32.tar.gz" -C $linux32_folder  caleydo
+  echo ".... [x] done"
+  
+  echo -n "Creating linux_x86-64 tar"
+  tar -czf $export_path/caleydo_$version_number"_linux_x86-64.tar.gz" -C $linux64_folder caleydo
+  echo ".... [x] done"
+ 
+  echo -n "Creating win32.x86 zip"
+  cd $win32_folder
+  zip -q -r $export_path/caleydo_$version_number"_win_x86-32.zip" caleydo
+  echo ".... [x] done"
+  
+  echo -n "Creating win32.x86-64 zip"
+  cd $win64_folder
+  zip -q -r $export_path/caleydo_$version_number"_win_x86-64.zip" caleydo 
+  echo ".... [x] done"
+  
+  echo -n "Creating macosx.cocoa.x86 zip"
+  cd $mac32_folder
+  zip -q -r $export_path/caleydo_$version_number"_macosx_cocoa_x86-32.zip" caleydo
+  echo ".... [x] done"
+  
+  echo -n "Creating macosx.cocoa.x86-64 zip"
+  cd $mac64_folder
+  zip -q -r $export_path/caleydo_$version_number"_macosx_cocoa_x86-64.zip" caleydo 
+  echo ".... [x] done"
+  
+}
+
+# create zip packages of the importer bianries
+# TODO fix this! It just does the same as create_zip_packages with different names
+package_importer()
+{
+
+  tar -czf $export_path/caleydo_data_importer_$version_number"_linux_x86-32.tar.gz" -C $export_root/linux.gtk.x86/ caleydo_data_importer
+ 
+  tar -czf $export_path/caleydo_data_importer_$version_number"_linux_x86-64.tar.gz" -C $export_root/linux.gtk.x86_64/ caleydo_data_importer  
   
   cd $export_root/win32.win32.x86/
-  zip -r $export_path/caleydo_$version_number"_win_x86-32.zip" caleydo
-  zip -r $export_path/caleydo_data_importer_$version_number"_win_x86-32.zip" caleydo_data_importer
+  zip -q -r $export_path/caleydo_data_importer_$version_number"_win_x86-32.zip" caleydo_data_importer
   
   cd $export_root/win32.win32.x86_64/
-  zip -r $export_path/caleydo_$version_number"_win_x86-64.zip" caleydo 
-  zip -r $export_path/caleydo_data_importer_$version_number"_win_x86-64.zip" caleydo_data_importer 
+  zip -q -r $export_path/caleydo_data_importer_$version_number"_win_x86-64.zip" caleydo_data_importer 
   
   cd $export_root/macosx.cocoa.x86/
-  zip -r $export_path/caleydo_$version_number"_macosx_cocoa_x86-32.zip" caleydo
-  zip -r $export_path/caleydo_data_importer_$version_number"_macosx_cocoa_x86-32.zip" caleydo_data_importer
+  zip -q -r $export_path/caleydo_data_importer_$version_number"_macosx_cocoa_x86-32.zip" caleydo_data_importer
   
   cd $export_root/macosx.cocoa.x86_64/
-  zip -r $export_path/caleydo_$version_number"_macosx_cocoa_x86-64.zip" caleydo 
-  zip -r $export_path/caleydo_data_importer_$version_number"_macosx_cocoa_x86-64.zip" caleydo_data_importer
+  zip -q -r $export_path/caleydo_data_importer_$version_number"_macosx_cocoa_x86-64.zip" caleydo_data_importer
 }
 
 # Trigger creation of debian packages for architectures x86-32 and x86-64
-make_debian_packages()
+create_linux_packages()
 {
-  mkdir -p $export_path
-  linux_source_folder=$export_root/linux.gtk.x86/
+  linux_source_folder=$linux32_folder
   linux_folder="caleydo_"$version_number"_linux_x86-32"
   arch="i386"
   do_deb
   
-  linux_source_folder=$export_root/linux.gtk.x86_64/
+  linux_source_folder=$linux64_folder
   linux_folder="caleydo_"$version_number"_linux_x86-64"
   arch="amd64"
-  do_deb
-  
+  do_deb  
 }
 
-# create deb and rpm for specific platform
+# helper to create deb and rpm for specific platform
 do_deb()
 {
   echo -n "Creating Linux packages for "$arch 
   cd $export_root
   cp $debian_folder  $linux_folder -R
-  cp $linux_source_folder $linux_32_folder"/opt/caleydo"
+  # remove .svn files from debian folder
+  rm -rf `find $linux_folder -type d -name .svn`
+  
+  cp $linux_source_folder $linux_folder"/opt/caleydo" -R
   #cp eclipse/ $linux_folder"/opt/caleydo" -R
   sed -i 's/ARCH_STRING/'$arch'/g' $linux_folder"/DEBIAN/control"
   sed -i 's/VERSION_NUMBER/'$version_number'/g' $linux_folder"/DEBIAN/control"
@@ -143,18 +208,17 @@ print_help()
   echo "If none of the options is present all are run: first the archive is made then the result is copied to the web, then the webstart is copied"
 }
 
+# reads the version number and creates the export path
 ask_for_version()
 {
   echo "Caleydo packaging"
   echo "Enter the version number for the export"
   read version_number
   export_path=$export_root/$version_number
+  mkdir -p $export_path
 }
-#echo $*
 
-#$# number of params
-#$1 first param
-
+# checking for input parameters
 
 if [ $# -gt 0 ]
 then
@@ -162,25 +226,31 @@ then
   while [ "$count" -lt $# ]
   do
     case $1 in
+      # create only linux packages
       -d) ask_for_version
-          make_debian_packages;;
-      # package standalone
+          create_linux_packages;;
+      # package everything standalone
       -a) ask_for_version
-	  make_archive
-	  make_debian_packages;;
+	  move_windows_installer
+	  create_zip_packages
+	  create_linux_packages;;
       # copy standalone
       -c) ask_for_version
 	  mount_webdav
-	  copy_to_web;;
+	  copy_standalone;;
       # package and copy standalone
       -s) ask_for_version
-	  make_archive
-	  make_debian_packages
+	  move_windows_installer
+	  create_zip_packages
+	  create_linux_packages
 	  mount_webdav
-	  copy_to_web;;
+	  copy_standalone;;
       # copy webstart
       -w) mount_webdav
 	  copy_webstart;;
+      # move and rename the windows installer
+      -i) ask_for_version
+	  move_windows_installer;;
       # command not recognized
       *) print_help;;
     esac
@@ -188,19 +258,22 @@ then
     count=$(($count + 1))
   done
 else
+
+  #interactive mode
   ask_for_version
   echo "Do you want to package the standalone release? (y/n)"
   read copy
   if [ $copy = "y" ];
     then
-    make_archive
-    make_debian_packages
+    move_windows_installer
+    create_zip_packages
+    create_linux_packages
     echo "Do you want to copy the release to the website? (y/n)"
     read copy
     if [ $copy = "y" ];
       then
       mount_webdav
-      copy_to_web
+      copy_standalone
     fi
   fi
 
