f364fb8352e01280aacc00e8ab9ed634d5b1e390
Marc Streit <marc.streit@jku.at>
2012-08-03 18:43:02 +0000

    Fixed wrong order of method arguments that caused Copy Number data to fail.
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@5194 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.data.importer/src/org/caleydo/data/importer/tcga/TCGADataXMLGenerator.java
+++ org.caleydo.data.importer/src/org/caleydo/data/importer/tcga/TCGADataXMLGenerator.java
@@ -244,15 +244,15 @@ public class TCGADataXMLGenerator
 		rowIDSpecification = new IDSpecification();
 		rowIDSpecification.setIdType("microRNA");
 		rowIDSpecification.setIdCategory("microRNA");
-		
-		 try {
-		 dataSetDescriptionCollection.add(setUpClusteredMatrixData("miR_Clustering_CNMF",
-		 "miR_Clustering_Consensus", "cnmf.normalized.gct", "microRNA",
-		 rowIDSpecification, sampleIDSpecification, false));
-		 }
-		 catch (Exception e) {
-		 System.err.println(e.getMessage());
-		 }
+
+		try {
+			dataSetDescriptionCollection.add(setUpClusteredMatrixData("miR_Clustering_CNMF",
+					"miR_Clustering_Consensus", "cnmf.normalized.gct", "microRNA",
+					rowIDSpecification, sampleIDSpecification, false));
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 
 		// TCGA SAMPLE IDs look different for seq data (an "-01" is attached)
 
@@ -266,15 +266,15 @@ public class TCGADataXMLGenerator
 
 		// ====== mRNAseq ======
 
-		 try {
-		 dataSetDescriptionCollection.add(setUpClusteredMatrixData(
-		 "mRNAseq_Clustering_CNMF", "mRNAseq_Clustering_Consensus",
-		 "outputprefix.expclu.gct", "mRNA-seq", rowIDSpecification,
-		 seqSampleIDSpecification, true));
-		 }
-		 catch (Exception e) {
-		 System.err.println(e.getMessage());
-		 }
+		try {
+			dataSetDescriptionCollection.add(setUpClusteredMatrixData(
+					"mRNAseq_Clustering_CNMF", "mRNAseq_Clustering_Consensus",
+					"outputprefix.expclu.gct", "mRNA-seq", rowIDSpecification,
+					seqSampleIDSpecification, true));
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 
 		// ====== microRNAseq ======
 
@@ -292,60 +292,60 @@ public class TCGADataXMLGenerator
 
 		rowIDSpecification = null; // uses genes
 
-		 try {
-		 dataSetDescriptionCollection.add(setUpClusteredMatrixData(
-		 "Methylation_Clustering_CNMF", "Methylation_Clustering_Consensus",
-		 "cnmf.normalized.gct", "methylation", rowIDSpecification,
-		 sampleIDSpecification, true));
-		 }
-		 catch (Exception e) {
-		 System.err.println(e.getMessage());
-		 }
-		
-		 // ====== reverse-phase protein arrays ======
-		
-		 rowIDSpecification = new IDSpecification();
-		 rowIDSpecification.setIdType("protein");
-		 rowIDSpecification.setIdCategory("protein");
-		
-		 try {
-		 dataSetDescriptionCollection.add(setUpClusteredMatrixData("RPPA_Clustering_CNMF",
-		 "RPPA_Clustering_Consensus", "cnmf.normalized.gct", "RPPA",
-		 rowIDSpecification, sampleIDSpecification, false));
-		 }
-		 catch (Exception e) {
-		 System.err.println(e.getMessage());
-		 }
-		
-		 // ====== copy number ======
-		
-		 try {
-		 dataSetDescriptionCollection.add(setUpCopyNumberData("CopyNumber_Gistic2",
-		 "Copy Number"));
-		 }
-		 catch (Exception e) {
-		 System.err.println(e.getMessage());
-		 }
-		
-		 // ====== mutation ======
-		
-		 try {
-		 dataSetDescriptionCollection.add(setUpMutationData("Mutation_Significance",
-		 "Mutations"));
-		 }
-		 catch (Exception e) {
-		 System.err.println(e.getMessage());
-		 }
-		
-		 // ====== clinical ======
-		
-		 try {
-		 dataSetDescriptionCollection.add(setUpClinicalData("Clinical_Pick_Tier1",
-		 "Clinical"));
-		 }
-		 catch (Exception e) {
-		 System.err.println(e.getMessage());
-		 }
+		try {
+			dataSetDescriptionCollection.add(setUpClusteredMatrixData(
+					"Methylation_Clustering_CNMF", "Methylation_Clustering_Consensus",
+					"cnmf.normalized.gct", "methylation", rowIDSpecification,
+					sampleIDSpecification, true));
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+
+		// ====== reverse-phase protein arrays ======
+
+		rowIDSpecification = new IDSpecification();
+		rowIDSpecification.setIdType("protein");
+		rowIDSpecification.setIdCategory("protein");
+
+		try {
+			dataSetDescriptionCollection.add(setUpClusteredMatrixData("RPPA_Clustering_CNMF",
+					"RPPA_Clustering_Consensus", "cnmf.normalized.gct", "RPPA",
+					rowIDSpecification, sampleIDSpecification, false));
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+
+		// ====== copy number ======
+
+		try {
+			dataSetDescriptionCollection.add(setUpCopyNumberData("CopyNumber_Gistic2",
+					"Copy Number"));
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+
+		// ====== mutation ======
+
+		try {
+			dataSetDescriptionCollection.add(setUpMutationData("Mutation_Significance",
+					"Mutations"));
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+
+		// ====== clinical ======
+
+		try {
+			dataSetDescriptionCollection.add(setUpClinicalData("Clinical_Pick_Tier1",
+					"Clinical"));
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 	}
 
 	private DataSetDescription setUpClusteredMatrixData(String cnmfArchiveName,
@@ -461,8 +461,8 @@ public class TCGADataXMLGenerator
 	}
 
 	private DataSetDescription setUpCopyNumberData(String archiveName, String dataType) {
-		String copyNumberFile = this.extractFile("all_thresholded.by_genes.txt",
-				analysisRunIdentifierWithoutUnderscore, archiveName,
+		String copyNumberFile = this.extractFile("all_thresholded.by_genes.txt", archiveName,
+				analysisRunIdentifierWithoutUnderscore,
 				remoteAnalysisRunArchiveDirectory, 4);
 
 		DataSetDescription copyNumberData = new DataSetDescription();
