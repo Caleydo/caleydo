849e7e02d8e3005b5b2c1cbd47a1a4ec3ee2972f
Marc Streit <marc.streit@jku.at>
2012-08-03 16:56:59 +0000

    Fixed problem with min/max calculation when all values are NaN.
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@5192 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.core/src/org/caleydo/core/data/collection/ccontainer/FloatCContainer.java
+++ org.caleydo.core/src/org/caleydo/core/data/collection/ccontainer/FloatCContainer.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
  * Caleydo - visualization for molecular biology - http://caleydo.org
- *  
+ * 
  * Copyright(C) 2005, 2012 Graz University of Technology, Marc Streit, Alexander
  * Lex, Christian Partl, Johannes Kepler University Linz </p>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- *  
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ * 
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>
  *******************************************************************************/
@@ -28,20 +28,20 @@ import org.caleydo.core.util.conversion.ConversionTools;
  * 
  * @author Alexander Lex
  */
-public class FloatCContainer implements INumericalCContainer {
+public class FloatCContainer
+	implements INumericalCContainer {
 
 	private float[] container;
 
 	private float min = Float.NaN;
 
-	private float fMax = Float.NaN;
+	private float max = Float.NaN;
 
 	/**
 	 * Constructor Pass a float array. The length of the array can not be
 	 * modified after initialization
 	 * 
-	 * @param container
-	 *            the float array
+	 * @param container the float array
 	 */
 	public FloatCContainer(final float[] container) {
 
@@ -56,10 +56,8 @@ public class FloatCContainer implements INumericalCContainer {
 	/**
 	 * Returns the value associated with the index
 	 * 
-	 * @throws IndexOutOfBoundsException
-	 *             if index out of range
-	 * @param index
-	 *            index of element to return
+	 * @throws IndexOutOfBoundsException if index out of range
+	 * @param index index of element to return
 	 * @return the element at the specified position in this list
 	 */
 	public float get(final int index) {
@@ -95,26 +93,26 @@ public class FloatCContainer implements INumericalCContainer {
 
 	@Override
 	public double getMax() {
-		if (Float.isNaN(fMax)) {
+		if (Float.isNaN(max)) {
 			calculateMinMax();
 		}
-		return fMax;
+		return max;
 	}
 
 	@Override
 	public FloatCContainer normalize() {
+
 		return new FloatCContainer(ConversionTools.normalize(container, (int) getMin(),
 				(int) getMax()));
 	}
 
 	@Override
-	public FloatCContainer normalizeWithExternalExtrema(final double dMin,
-			final double dMax) {
-		if (dMin >= dMax)
+	public FloatCContainer normalizeWithExternalExtrema(final double min, final double max) {
+		if (min >= max)
 			throw new IllegalArgumentException("Minimum was bigger or same as maximum");
 
-		return new FloatCContainer(ConversionTools.normalize(container, (float) dMin,
-				(float) dMax));
+		return new FloatCContainer(ConversionTools.normalize(container, (float) min,
+				(float) max));
 
 	}
 
@@ -137,20 +135,20 @@ public class FloatCContainer implements INumericalCContainer {
 
 	@Override
 	public FloatCContainer log(int base) {
-		float[] fArTarget = new float[container.length];
+		float[] target = new float[container.length];
 
-		float fTmp;
+		float tmp;
 		for (int index = 0; index < container.length; index++) {
-			fTmp = container[index];
+			tmp = container[index];
 
-			fArTarget[index] = (float) Math.log(fTmp) / (float) Math.log(base);
+			target[index] = (float) Math.log(tmp) / (float) Math.log(base);
 
-			if (fArTarget[index] == Float.NEGATIVE_INFINITY) {
-				fArTarget[index] = 0;
+			if (target[index] == Float.NEGATIVE_INFINITY) {
+				target[index] = 0;
 			}
 		}
 
-		return new FloatCContainer(fArTarget);
+		return new FloatCContainer(target);
 	}
 
 	/**
@@ -159,20 +157,27 @@ public class FloatCContainer implements INumericalCContainer {
 	 */
 	private void calculateMinMax() {
 		min = Float.MAX_VALUE;
-		fMax = Float.MIN_VALUE;
-		for (float fCurrent : container) {
-			if (Float.isNaN(fCurrent)) {
+		max = Float.MIN_VALUE;
+		for (float current : container) {
+			if (Float.isNaN(current)) {
 				continue;
 			}
 
-			if (fCurrent < min) {
-				min = fCurrent;
+			if (current < min) {
+				min = current;
 				continue;
 			}
-			if (fCurrent > fMax) {
-				fMax = fCurrent;
+			if (current > max) {
+				max = current;
 			}
 		}
+
+		// Needed if all values in array are NaN
+		if (min == Float.MAX_VALUE)
+			min = Float.NaN;
+		if (max == Float.MIN_VALUE)
+			max = Float.NaN;
+
 		return;
 	}
 
