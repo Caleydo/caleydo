d00cdef4fe1dc8b29de71cb6d9edf74eadb789c5
Alexander Lex <alex@seas.harvard.edu>
2012-08-09 10:10:25 +0000

    Fixed logging of exceptions
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@5219 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.core/src/org/caleydo/core/util/logging/LogListener.java
+++ org.caleydo.core/src/org/caleydo/core/util/logging/LogListener.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
  * Caleydo - visualization for molecular biology - http://caleydo.org
- *  
+ * 
  * Copyright(C) 2005, 2012 Graz University of Technology, Marc Streit, Alexander
  * Lex, Christian Partl, Johannes Kepler University Linz </p>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- *  
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ * 
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>
  *******************************************************************************/
@@ -24,6 +24,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -36,26 +39,26 @@ import org.eclipse.core.runtime.IStatus;
  * 
  * @author Alexander Lex
  */
-public class LogListener
-	implements ILogListener {
+public class LogListener implements ILogListener {
 
 	private File logFile;
 
 	public LogListener() {
 		// String path = System.getProperty("user.dir");
 
-		String directory = GeneralManager.CALEYDO_HOME_PATH + File.separator + "logs" + File.separator;
+		String directory = GeneralManager.CALEYDO_HOME_PATH + File.separator + "logs"
+				+ File.separator;
 		File dir = new File(directory);
 		if (!dir.exists())
 			dir.mkdir();
 
 		Date timeNow = new Date();
-		String timeStamp = (new SimpleDateFormat("yyyy.MM.dd_HH.mm.ss.SSS")).format(timeNow);
+		String timeStamp = (new SimpleDateFormat("yyyy.MM.dd_HH.mm.ss.SSS"))
+				.format(timeNow);
 		logFile = new File(directory + timeStamp + ".log");
 		try {
 			logFile.createNewFile();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -67,17 +70,13 @@ public class LogListener
 			StringBuffer str = new StringBuffer();
 			if (status.getSeverity() == IStatus.ERROR) {
 				str.append("Error: ");
-			}
-			else if (status.getSeverity() == IStatus.CANCEL) {
+			} else if (status.getSeverity() == IStatus.CANCEL) {
 				str.append("CANCEL: ");
-			}
-			else if (status.getSeverity() == IStatus.INFO) {
+			} else if (status.getSeverity() == IStatus.INFO) {
 				str.append("INFO: ");
-			}
-			else if (status.getSeverity() == IStatus.WARNING) {
+			} else if (status.getSeverity() == IStatus.WARNING) {
 				str.append("WARNING: ");
-			}
-			else if (status.getSeverity() == IStatus.OK) {
+			} else if (status.getSeverity() == IStatus.OK) {
 				str.append("OK: ");
 			}
 			str.append(status.getPlugin() + ": ");
@@ -85,19 +84,24 @@ public class LogListener
 			str.append(status.getMessage());
 			Throwable ex = status.getException();
 			if (ex != null) {
-				String stackTrace = status.getException().getStackTrace().toString();
-				str.append(stackTrace);
+
+				str.append(stackTraceAsTring(ex));
 			}
 			str.append(System.getProperty("line.separator"));
 			bos.write(str.toString());
 			bos.close();
-		}
-		catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
+	}
+
+	private static String stackTraceAsTring(Throwable throwable) {
+		final Writer result = new StringWriter();
+		final PrintWriter printWriter = new PrintWriter(result);
+		throwable.printStackTrace(printWriter);
+		return result.toString();
 	}
 
 }
