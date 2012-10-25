8bdddf85a44e827ec65083146f0e59c1956ccbc9
Marc Streit <marc.streit@jku.at>
2012-10-25 09:11:32 +0000

    Fixed problem with null state of workbench window that occurred when opening enRoute and the pathway view right after each other.
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@5405 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.view.enroute/src/org/caleydo/view/enroute/command/handler/OpenEnRouteHandler.java
+++ org.caleydo.view.enroute/src/org/caleydo/view/enroute/command/handler/OpenEnRouteHandler.java
@@ -25,6 +25,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -35,9 +36,10 @@ public class OpenEnRouteHandler
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		try {
-			HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().showView(GLPathway.VIEW_TYPE);
+			IWorkbenchWindow activeWindows = HandlerUtil.getActiveWorkbenchWindow(event);
 
-			HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().showView(GLEnRoutePathway.VIEW_TYPE);
+			activeWindows.getActivePage().showView(GLPathway.VIEW_TYPE);
+			activeWindows.getActivePage().showView(GLEnRoutePathway.VIEW_TYPE);
 
 		}
 		catch (PartInitException e) {
