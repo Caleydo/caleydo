f9eff8623f15f738376ce4fa69e2dc6505d8d881
Alexander Lex <alex@seas.harvard.edu>
2012-10-31 20:51:58 +0000

    Fixed typo in enroute message.
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@5439 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.view.enroute/src/org/caleydo/view/enroute/GLEnRoutePathway.java
+++ org.caleydo.view.enroute/src/org/caleydo/view/enroute/GLEnRoutePathway.java
@@ -101,8 +101,8 @@ public class GLEnRoutePathway
 	public static String VIEW_TYPE = "org.caleydo.view.enroute";
 	public static String VIEW_NAME = "enRoute";
 
-	protected final static String EMPTY_VIEW_TEXT_LINE_ONE = "Please select a path of nodes and assign data to enRoute using the Data View Integrater (DVI) View.";
-	protected final static String EMPTY_VIEW_TEXT_LINE_TWO = "in the pathway view.";
+	protected final static String EMPTY_VIEW_TEXT_LINE_ONE = "Please select a path of nodes using the pathway view ";
+	protected final static String EMPTY_VIEW_TEXT_LINE_TWO = "and assign data to enRoute using the Data-View Integrater View.";
 
 	protected final static int DEFAULT_DATA_ROW_HEIGHT_PIXELS = 60;
 	protected final static int BRANCH_COLUMN_WIDTH_PIXELS = 100;
