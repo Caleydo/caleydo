27a18f8000567bdacc44a876e407b5fb0e7a631b
Nils Gehlenborg <nils@gehlenborg.com>
2012-11-05 18:56:13 +0000

    Fixed typos in FAQ, changed how stratification as displayed in the table (moved from listing names to giving a count).
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@5483 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.data.importer/website/js/firehose-data.js
+++ org.caleydo.data.importer/website/js/firehose-data.js
@@ -148,7 +148,6 @@ function renderAnalysisSelectionOverview( runs, element ) {
 	
 	var analysisDates = [];
 	var analysisIndex = 0;
-	//console.log(runs);
 	
 	for ( var i = 0; i < runs.length; ++i ) {
 		if ( runs[i].id === analysis ) {
@@ -185,7 +184,6 @@ function renderAnalysisSelection( runs, element ) {
 	
 	var analysisDates = [];
 	var analysisIndex = 0;
-	//console.log(runs);
 	
 	for ( var i = 0; i < runs.length; ++i ) {
 		if ( runs[i].id === analysis ) {
@@ -500,14 +498,14 @@ function renderTable( data, tumorIndex, element ) {
 
 
 function _genomicHeaderRender() {
-		return "<thead><tr><th width=10%>" + "Data Type" + "</th><th></th><th>" + "#Patients" + "</th><th width=40%>" + "Patient Stratifications" + 
-										  "</td><th>" + "#Genes" + "</th><th width=40%>" + "Gene Stratifications" + "</th></tr></thead>";			
+		return "<thead><tr><th>" + "Data Type" + "</th><th></th><th>" + "#Patients" + "</th><th>" + "#Patient Stratifications" + 
+										  "</td><th>" + "#Genes" + "</th><th>" + "#Gene Stratifications" + "</th></tr></thead>";			
 	}
 
 function _genomicRowRender( dataset, datasetName ) {
 	if ( dataset ) {
-		return "<tr><th>" + datasetName + "</th><td><span class=\"badge\" style=\"background: " + mapDataTypeToColor( datasetName ) +";\"></span></td><td>" + dataset.sample.count + "</td><td>" + dataset.sample.groupings.join( "; ") + 
-										  "</td><td>" + dataset.gene.count + "</td><td>" + dataset.gene.groupings.join( "; ") + "</td></tr>";			
+		return "<tr><th>" + datasetName + "</th><td><span class=\"badge\" style=\"background: " + mapDataTypeToColor( datasetName ) +";\"></span></td><td>" + dataset.sample.count + "</td><td>" + (dataset.sample.groupings.length-1)/*join( "; ")*/ + 
+										  "</td><td>" + dataset.gene.count + "</td><td>" + (dataset.gene.groupings.length-1)/*join( "; ")*/ + "</td></tr>";			
 	}
 	/*
 	else {
@@ -518,7 +516,7 @@ function _genomicRowRender( dataset, datasetName ) {
 
 
 function _nonGenomicHeaderRender() {
-		return "<thead><tr><th width=10%>" + "Data Type" + "</th><th>" + "#Patients" + "</th><th>" + "Parameters" + 
+		return "<thead><tr><th>" + "Data Type" + "</th><th>" + "#Patients" + "</th><th>" + "Parameters" + 
 										  "</th></tr></thead>";			
 	}
 
@@ -610,7 +608,7 @@ function renderOverviewChart() {
 		}
 	}
 
-	console.log( newData );
+	console.log( input );
 			
 	var n = newData.length; // number of layers
 	var m = newData[0].length; // number of samples per layer
@@ -798,40 +796,6 @@ function transitionStack() {
         .attr("x", 0)
         .attr("width", x({x: .9}));
   }
-}
-
-/* Inspired by Lee Byron's test data generator. */
-function stream_layers(n, m, o) {
-  if (arguments.length < 3) o = 0;
-  function bump(a) {
-    var x = 1 / (.1 + Math.random()),
-        y = 2 * Math.random() - .5,
-        z = 10 / (.1 + Math.random());
-    for (var i = 0; i < m; i++) {
-      var w = (i / m - y) * z;
-      a[i] += x * Math.exp(-w * w);
-    }
-  }
-  return d3.range(n).map(function() {
-      var a = [], i;
-      for (i = 0; i < m; i++) a[i] = o + o * Math.random();
-      for (i = 0; i < 5; i++) bump(a);
-      return a.map(stream_index);
-    });
-}
-
-/* Another layer generator using gamma distributions. */
-function stream_waves(n, m) {
-  return d3.range(n).map(function(i) {
-    return d3.range(m).map(function(j) {
-        var x = 20 * j / m - i / 3;
-        return 2 * x * Math.exp(-.5 * x);
-      }).map(stream_index);
-    });
-}
-
-function stream_index(d, i) {
-  return {x: i, y: Math.max(0, d)};
 }
 
 
