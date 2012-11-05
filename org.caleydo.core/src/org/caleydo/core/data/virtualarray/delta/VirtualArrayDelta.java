2c3024d7d28018b7d465577e659bd7cae67d6da9
Samuel Gratzl <samuel_gratzl@gmx.at>
2012-11-05 10:53:28 +0000

    switch to Java-7, disable project specific formatters -> use workspace mechanic, add bin directories to svn:ignore
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@5473 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.core/src/org/caleydo/core/data/virtualarray/delta/VirtualArrayDelta.java
+++ org.caleydo.core/src/org/caleydo/core/data/virtualarray/delta/VirtualArrayDelta.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Caleydo - visualization for molecular biology - http://caleydo.org
- *  
+ *
  * Copyright(C) 2005, 2012 Graz University of Technology, Marc Streit, Alexander
  * Lex, Christian Partl, Johannes Kepler University Linz </p>
  *
@@ -8,12 +8,12 @@
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>
  *******************************************************************************/
@@ -21,15 +21,17 @@ package org.caleydo.core.data.virtualarray.delta;
 
 import java.util.Collection;
 import java.util.Iterator;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
+
 import org.caleydo.core.data.selection.delta.IDelta;
 import org.caleydo.core.id.IDType;
 import org.caleydo.core.util.collection.UniqueList;
 
 /**
  * Implementation of {@link IVirtualArrayDelta}
- * 
+ *
  * @author Alexander Lex
  */
 @XmlType
@@ -59,7 +61,7 @@ public abstract class VirtualArrayDelta<ConcreteType extends VirtualArrayDelta<C
 
 	/**
 	 * Returns the type of the virtual array as specified in {@link VAType}
-	 * 
+	 *
 	 * @return
 	 */
 	public String getVAType() {
@@ -68,7 +70,7 @@ public abstract class VirtualArrayDelta<ConcreteType extends VirtualArrayDelta<C
 
 	/**
 	 * Set the type of the VA
-	 * 
+	 *
 	 * @param vaType
 	 */
 	public void setVAType(String vaType) {
@@ -106,7 +108,7 @@ public abstract class VirtualArrayDelta<ConcreteType extends VirtualArrayDelta<C
 	}
 
 	public void append(ConcreteType delta) {
-		deltaItems.addAll(delta.deltaItems);
+		deltaItems.addAll(delta.getAllItems());
 	}
 
 	@Override
