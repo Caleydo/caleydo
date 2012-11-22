5ee264aaeac14b4ded627c64b84896976587a183
Samuel Gratzl <samuel_gratzl@gmx.at>
2012-11-22 09:39:46 +0000

    tcga: convert -g to a string option to allow setting to false (boolean flags are just for enabling, not disabling things)
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@5563 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.data.importer/src/org/caleydo/data/importer/tcga/regular/TCGASettings.java
+++ org.caleydo.data.importer/src/org/caleydo/data/importer/tcga/regular/TCGASettings.java
@@ -8,14 +8,14 @@ public class TCGASettings extends Settings {
 	@Option(name = "-s", aliases = { "--server" }, usage = "TCGA Server URL that hosts TCGA Caleydo project files default: \"http://compbio.med.harvard.edu/tcga/stratomex/data/\"")
 	private String tcgaServerURL = "http://compbio.med.harvard.edu/tcga/stratomex/data/";
 
-	@Option(name = "-g", aliases = { "--sampleGenes" }, usage = "whether to use the sampled genes or not default: \"true\"")
-	private boolean sampleGenes = true;
+	@Option(name = "-g", aliases = { "--sampledGenes" }, usage = "whether to use the sampled genes or not default: \"true\"")
+	private String sampleGenes = "true";
 
 	public String getTcgaServerURL() {
 		return tcgaServerURL;
 	}
 
 	public boolean isSampleGenes() {
-		return sampleGenes;
+		return Boolean.parseBoolean(sampleGenes);
 	}
 }
