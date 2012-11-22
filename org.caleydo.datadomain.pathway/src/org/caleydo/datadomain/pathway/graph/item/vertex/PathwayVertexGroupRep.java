737bac74350144bb0ea90efd941392c9b9ebd024
Marc Streit <marc.streit@jku.at>
2012-11-22 11:40:29 +0000

    Manually merged r5569 to stable branch.
    git-svn-id: svn://localhost:8080/caleydo_main/branches/stable_2.1@5569 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.datadomain.pathway/src/org/caleydo/datadomain/pathway/graph/item/vertex/PathwayVertexGroupRep.java
+++ org.caleydo.datadomain.pathway/src/org/caleydo/datadomain/pathway/graph/item/vertex/PathwayVertexGroupRep.java
@@ -5,6 +5,7 @@ package org.caleydo.datadomain.pathway.graph.item.vertex;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.caleydo.core.util.collection.Pair;
 
 /**
  * A pathway vertex representation that groups together multiple {@link PathwayVertexRep} objects.
@@ -30,29 +31,39 @@ public class PathwayVertexGroupRep
 	public void addVertexRep(PathwayVertexRep vertexRep) {
 		groupedVertexReps.add(vertexRep);
 
-		if (vertexRep.getCoords().get(0).getFirst() < coords.get(0).getFirst())
-			coords.get(0).setFirst(vertexRep.getCoords().get(0).getFirst());
+		if (groupedVertexReps.size() == 1) {
+			coords = new ArrayList<Pair<Short, Short>>(4);
 
-		if (vertexRep.getCoords().get(0).getSecond() < coords.get(0).getSecond())
-			coords.get(0).setSecond(vertexRep.getCoords().get(0).getSecond());
+			ArrayList<Pair<Short, Short>> vertexRepCoords = vertexRep.getCoords();
+			coords.add(new Pair<Short, Short>(vertexRepCoords.get(0).getFirst(), vertexRepCoords.get(0).getSecond()));
+			coords.add(new Pair<Short, Short>(vertexRepCoords.get(1).getFirst(), vertexRepCoords.get(1).getSecond()));
+			coords.add(new Pair<Short, Short>(vertexRepCoords.get(2).getFirst(), vertexRepCoords.get(2).getSecond()));
+			coords.add(new Pair<Short, Short>(vertexRepCoords.get(3).getFirst(), vertexRepCoords.get(3).getSecond()));
+		} else {
+			if (vertexRep.getCoords().get(0).getFirst() < coords.get(0).getFirst())
+				coords.get(0).setFirst(vertexRep.getCoords().get(0).getFirst());
 
-		if (vertexRep.getCoords().get(1).getFirst() > coords.get(1).getFirst())
-			coords.get(1).setFirst(vertexRep.getCoords().get(1).getFirst());
+			if (vertexRep.getCoords().get(0).getSecond() < coords.get(0).getSecond())
+				coords.get(0).setSecond(vertexRep.getCoords().get(0).getSecond());
 
-		if (vertexRep.getCoords().get(1).getSecond() < coords.get(1).getSecond())
-			coords.get(1).setSecond(vertexRep.getCoords().get(1).getSecond());
+			if (vertexRep.getCoords().get(1).getFirst() > coords.get(1).getFirst())
+				coords.get(1).setFirst(vertexRep.getCoords().get(1).getFirst());
 
-		if (vertexRep.getCoords().get(2).getFirst() > coords.get(2).getFirst())
-			coords.get(2).setFirst(vertexRep.getCoords().get(2).getFirst());
+			if (vertexRep.getCoords().get(1).getSecond() < coords.get(1).getSecond())
+				coords.get(1).setSecond(vertexRep.getCoords().get(1).getSecond());
 
-		if (vertexRep.getCoords().get(2).getSecond() > coords.get(2).getSecond())
-			coords.get(2).setSecond(vertexRep.getCoords().get(2).getSecond());
+			if (vertexRep.getCoords().get(2).getFirst() > coords.get(2).getFirst())
+				coords.get(2).setFirst(vertexRep.getCoords().get(2).getFirst());
 
-		if (vertexRep.getCoords().get(3).getFirst() < coords.get(3).getFirst())
-			coords.get(3).setFirst(vertexRep.getCoords().get(3).getFirst());
+			if (vertexRep.getCoords().get(2).getSecond() > coords.get(2).getSecond())
+				coords.get(2).setSecond(vertexRep.getCoords().get(2).getSecond());
 
-		if (vertexRep.getCoords().get(3).getSecond() > coords.get(3).getSecond())
-			coords.get(3).setSecond(vertexRep.getCoords().get(3).getSecond());
+			if (vertexRep.getCoords().get(3).getFirst() < coords.get(3).getFirst())
+				coords.get(3).setFirst(vertexRep.getCoords().get(3).getFirst());
+
+			if (vertexRep.getCoords().get(3).getSecond() > coords.get(3).getSecond())
+				coords.get(3).setSecond(vertexRep.getCoords().get(3).getSecond());
+		}
 	}
 
 	/**
