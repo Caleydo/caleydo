207426e9eec5a158141eccc34d1a5d8e5614acdc
Samuel Gratzl <samuel_gratzl@gmx.at>
2012-11-26 12:55:05 +0000

    #958 standard input dialog for renaming
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@5590 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.core/src/org/caleydo/core/gui/util/ChangeNameDialog.java
+++ org.caleydo.core/src/org/caleydo/core/gui/util/ChangeNameDialog.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Caleydo - visualization for molecular biology - http://caleydo.org
- *  
+ *
  * Copyright(C) 2005, 2012 Graz University of Technology, Marc Streit, Alexander
  * Lex, Christian Partl, Johannes Kepler University Linz </p>
  *
@@ -8,91 +8,41 @@
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>
  *******************************************************************************/
 package org.caleydo.core.gui.util;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
+import org.eclipse.jface.dialogs.IInputValidator;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 
 /**
  * @author Alexander Lex
  */
 public class ChangeNameDialog {
-	private String resultingName;
-
-	public void run(Display display, final String originalName) {
-		resultingName = originalName;
-		// Shell shell = new Shell(display);
-		// shell.pack();
-		// shell.open();
-		final Shell dialog = new Shell(display, SWT.DIALOG_TRIM);
-		final Text text = new Text(dialog, SWT.SHADOW_IN);
-		text.setBounds(140, 40, 100, 25);
-		text.setText(originalName);
-		final Button okButton = new Button(dialog, SWT.PUSH);
-		okButton.setText("&OK");
-		Button cancelButton = new Button(dialog, SWT.PUSH);
-		cancelButton.setText("&Cancel");
-
-		FormLayout form = new FormLayout();
-		form.marginWidth = form.marginHeight = 8;
-		dialog.setLayout(form);
-		FormData okData = new FormData();
-		okData.top = new FormAttachment(text, 8);
-		okButton.setLayoutData(okData);
-		FormData cancelData = new FormData();
-		cancelData.left = new FormAttachment(okButton, 8);
-		cancelData.top = new FormAttachment(okButton, 0, SWT.TOP);
-		cancelButton.setLayoutData(cancelData);
-
-		dialog.setDefaultButton(okButton);
-		dialog.pack();
-		dialog.open();
-
-		Listener okListener = new Listener() {
-
+	public static String show(Shell shell, String title, String original) {
+		IInputValidator validator = new IInputValidator() {
 			@Override
-			public void handleEvent(Event event) {
-				if (event.widget == okButton) {
-					resultingName = text.getText();
-				}
-				dialog.dispose();
-
+			public String isValid(String newText) {
+				if (newText == null || newText.trim().length() == 0)
+					return "Please enter a name.";
+				else
+					return null;
 			}
 		};
 
-		okButton.addListener(SWT.Selection, okListener);
-
-		cancelButton.addListener(SWT.Selection, okListener);
-
-		while (!dialog.isDisposed()) {
-			if (!display.readAndDispatch())
-				display.sleep();
-		}
-
+		InputDialog dialog = new InputDialog(new Shell(), title, title, original, validator);
+		if (dialog.open() == Window.OK) {
+			return dialog.getValue();
+		} else
+			return null;
 	}
-
-	/**
-	 * @return the resultingName, see {@link #resultingName}
-	 */
-	public String getResultingName() {
-		return resultingName;
-	}
-
 }
