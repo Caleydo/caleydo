a9f14f93daeb2bd574a33298fdfe910bd36d0259
Marc Streit <marc.streit@jku.at>
2012-12-07 09:04:29 +0000

    #959
    git-svn-id: svn://localhost:8080/caleydo_main/trunk@5654 1f7349ae-fd9f-0d40-aeb8-9798e6c0fce3

--- org.caleydo.view.stratomex/src/org/caleydo/view/stratomex/toolbar/ConnectionsModeGUI.java
+++ org.caleydo.view.stratomex/src/org/caleydo/view/stratomex/toolbar/ConnectionsModeGUI.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Caleydo - visualization for molecular biology - http://caleydo.org
- *  
+ *
  * Copyright(C) 2005, 2012 Graz University of Technology, Marc Streit, Alexander
  * Lex, Christian Partl, Johannes Kepler University Linz </p>
  *
@@ -8,12 +8,12 @@
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>
  *******************************************************************************/
@@ -51,7 +51,7 @@ public class ConnectionsModeGUI extends ControlContribution implements IToolBarI
 		final Button[] radios = new Button[1];
 		radios[0] = new Button(composite, SWT.CHECK);
 		radios[0].setSelection(true);
-		radios[0].setText("Switch Connections ON/OFF");
+		radios[0].setText("Show Only Selected Connections");
 
 		final Button dynamicTrendHighlightButton = new Button(composite, SWT.CHECK);
 		dynamicTrendHighlightButton.setText("Dynamic Connection Highlight Focus");
