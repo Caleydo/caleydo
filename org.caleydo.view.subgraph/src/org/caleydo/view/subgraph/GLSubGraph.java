44a5624f819f90de85f650f98696c2090e8d90f3
Christian Partl <partl@icg.tugraz.at>
2013-02-14 10:52:53 +0000

    tried to use new layout in subgraph
    git-svn-id: svn://localhost:8080/entourage/trunk@26 fba478fa-5d3f-d649-a3c9-ad29d1569dfd

--- org.caleydo.view.subgraph/src/org/caleydo/view/subgraph/GLSubGraph.java
+++ org.caleydo.view.subgraph/src/org/caleydo/view/subgraph/GLSubGraph.java
@@ -20,23 +20,23 @@ import org.caleydo.core.view.listener.AddTablePerspectivesEvent;
 import org.caleydo.core.view.listener.AddTablePerspectivesListener;
 import org.caleydo.core.view.opengl.camera.ViewFrustum;
 import org.caleydo.core.view.opengl.canvas.AGLView;
-import org.caleydo.core.view.opengl.canvas.EDetailLevel;
 import org.caleydo.core.view.opengl.canvas.IGLCanvas;
 import org.caleydo.core.view.opengl.canvas.remote.IGLRemoteRenderingView;
-import org.caleydo.core.view.opengl.layout.Column;
-import org.caleydo.core.view.opengl.layout.ElementLayout;
 import org.caleydo.core.view.opengl.layout.LayoutManager;
-import org.caleydo.core.view.opengl.layout.Row;
 import org.caleydo.core.view.opengl.layout.util.multiform.MultiFormRenderer;
 import org.caleydo.core.view.opengl.layout.util.multiform.MultiFormViewSwitchingBar;
-import org.caleydo.core.view.opengl.mouse.GLMouseListener;
+import org.caleydo.core.view.opengl.layout2.AGLElementGLView;
+import org.caleydo.core.view.opengl.layout2.GLElement;
+import org.caleydo.core.view.opengl.layout2.GLElementAdapter;
+import org.caleydo.core.view.opengl.layout2.GLElementContainer;
+import org.caleydo.core.view.opengl.layout2.layout.GLLayouts;
 import org.caleydo.datadomain.pathway.PathwayDataDomain;
 import org.caleydo.datadomain.pathway.data.PathwayTablePerspective;
 import org.caleydo.datadomain.pathway.manager.EPathwayDatabaseType;
 import org.caleydo.datadomain.pathway.manager.PathwayManager;
 import org.eclipse.swt.widgets.Composite;
 
-public class GLSubGraph extends AGLView implements IMultiTablePerspectiveBasedView, IGLRemoteRenderingView {
+public class GLSubGraph extends AGLElementGLView implements IMultiTablePerspectiveBasedView, IGLRemoteRenderingView {
 
 	public static String VIEW_TYPE = "org.caleydo.view.subgraph";
 
@@ -44,21 +44,24 @@ public class GLSubGraph extends AGLView implements IMultiTablePerspectiveBasedVi
 
 	private LayoutManager layoutManager;
 
-	private MultiFormRenderer pathMultiformRenderer;
+	private MultiFormRenderer pathMultiformRenderer = new MultiFormRenderer(this, false);
 
 	private List<TablePerspective> tablePerspectives = new ArrayList<>();
 
 	private Set<String> remoteRenderedPathwayMultiformViewIDs;
 
-	private Row baseRow;
+	// private Row baseRow;
 
-	private Column pathwayColumn;
+	// private Column pathwayColumn;
 
 	private AddTablePerspectivesListener addTablePerspectivesListener;
 
-	private Column pathColumn;
+	// private Column pathColumn;
 
 	private String pathEventSpace;
+
+	private GLElementContainer baseContainer;
+	private GLElementContainer pathwayColumn;
 
 	/**
 	 * Constructor.
@@ -71,6 +74,9 @@ public class GLSubGraph extends AGLView implements IMultiTablePerspectiveBasedVi
 
 		super(glCanvas, parentComposite, viewFrustum, VIEW_TYPE, VIEW_NAME);
 		pathEventSpace = GeneralManager.get().getEventPublisher().createUniqueEventSpace();
+		baseContainer = new GLElementContainer(GLLayouts.flowHorizontal(10));
+		 pathwayColumn = new GLElementContainer(GLLayouts.flowVertical(10));
+		 baseContainer.add(pathwayColumn, 0.4f);
 	}
 
 	@Override
@@ -95,75 +101,76 @@ public class GLSubGraph extends AGLView implements IMultiTablePerspectiveBasedVi
 		// // TODO Auto-generated catch block
 		// e.printStackTrace();
 		// }
-		displayListIndex = gl.glGenLists(1);
-		detailLevel = EDetailLevel.HIGH;
-		pathMultiformRenderer = new MultiFormRenderer(this, false);
-
-		layoutManager = new LayoutManager(viewFrustum, pixelGLConverter);
-		baseRow = new Row();
-
-		pathwayColumn = new Column();
-		pathwayColumn.setRatioSizeX(0.3f);
-		pathwayColumn.setRatioSizeY(1);
-		pathwayColumn.setBottomUp(false);
-
-		pathColumn = new Column();
-		pathColumn.setRatioSizeX(0.7f);
-		pathColumn.setRatioSizeY(1);
-
-		baseRow.add(pathwayColumn);
-		baseRow.add(pathColumn);
-		layoutManager.setBaseElementLayout(baseRow);
-		layoutManager.setUseDisplayLists(true);
-
-	}
-
-	@Override
-	public void initLocal(GL2 gl) {
-		init(gl);
-	}
-
-	@Override
-	public void initRemote(final GL2 gl, final AGLView glParentView, final GLMouseListener glMouseListener) {
-
-		// Register keyboard listener to GL2 canvas
-		glParentView.getParentComposite().getDisplay().asyncExec(new Runnable() {
-			@Override
-			public void run() {
-				glParentView.getParentComposite().addKeyListener(glKeyListener);
-			}
-		});
-
-		this.glMouseListener = glMouseListener;
-
-		init(gl);
-	}
-
-	@Override
-	public void displayLocal(GL2 gl) {
-		pickingManager.handlePicking(this, gl);
-		display(gl);
-		if (busyState != EBusyState.OFF) {
-			renderBusyMode(gl);
-		}
+		super.init(gl);
+		// displayListIndex = gl.glGenLists(1);
+		// detailLevel = EDetailLevel.HIGH;
+		// pathMultiformRenderer = new MultiFormRenderer(this, false);
+		//
+		// layoutManager = new LayoutManager(viewFrustum, pixelGLConverter);
+		// baseRow = new Row();
+		//
+		// pathwayColumn = new Column();
+		// pathwayColumn.setRatioSizeX(0.3f);
+		// pathwayColumn.setRatioSizeY(1);
+		// pathwayColumn.setBottomUp(false);
+		//
+		// pathColumn = new Column();
+		// pathColumn.setRatioSizeX(0.7f);
+		// pathColumn.setRatioSizeY(1);
+		//
+		// baseRow.add(pathwayColumn);
+		// baseRow.add(pathColumn);
+		// layoutManager.setBaseElementLayout(baseRow);
+		// layoutManager.setUseDisplayLists(true);
 
 	}
 
-	@Override
-	public void displayRemote(GL2 gl) {
-		display(gl);
-	}
+	// @Override
+	// public void initLocal(GL2 gl) {
+	// init(gl);
+	// }
+	//
+	// @Override
+	// public void initRemote(final GL2 gl, final AGLView glParentView, final GLMouseListener glMouseListener) {
+	//
+	// // Register keyboard listener to GL2 canvas
+	// glParentView.getParentComposite().getDisplay().asyncExec(new Runnable() {
+	// @Override
+	// public void run() {
+	// glParentView.getParentComposite().addKeyListener(glKeyListener);
+	// }
+	// });
+	//
+	// this.glMouseListener = glMouseListener;
+	//
+	// init(gl);
+	// }
 
-	@Override
-	public void display(GL2 gl) {
-
-		if (isDisplayListDirty) {
-			layoutManager.updateLayout();
-			isDisplayListDirty = false;
-		}
-		layoutManager.render(gl);
-		checkForHits(gl);
-	}
+	// @Override
+	// public void displayLocal(GL2 gl) {
+	// pickingManager.handlePicking(this, gl);
+	// display(gl);
+	// if (busyState != EBusyState.OFF) {
+	// renderBusyMode(gl);
+	// }
+	//
+	// }
+	//
+	// @Override
+	// public void displayRemote(GL2 gl) {
+	// display(gl);
+	// }
+	//
+	// @Override
+	// public void display(GL2 gl) {
+	//
+	// if (isDisplayListDirty) {
+	// layoutManager.updateLayout();
+	// isDisplayListDirty = false;
+	// }
+	// layoutManager.render(gl);
+	// checkForHits(gl);
+	// }
 
 	@Override
 	public ASerializedView getSerializableRepresentation() {
@@ -257,14 +264,14 @@ public class GLSubGraph extends AGLView implements IMultiTablePerspectiveBasedVi
 					PathwayManager.get().getPathwayByTitle("Glioma", EPathwayDatabaseType.KEGG));
 			pathwayDataDomain.addTablePerspective(pathwayPathwayTablePerspective);
 
-			addPathwayMultiform(pathwayPathwayTablePerspective);
+			 addPathwayMultiform(pathwayPathwayTablePerspective);
 
 			pathwayPathwayTablePerspective = new PathwayTablePerspective(tablePerspective.getDataDomain(),
 					pathwayDataDomain, newRecordPerspective, newDimensionPerspective, PathwayManager.get()
 							.getPathwayByTitle("Glioma", EPathwayDatabaseType.KEGG));
 			pathwayDataDomain.addTablePerspective(pathwayPathwayTablePerspective);
 
-			addPathwayMultiform(pathwayPathwayTablePerspective);
+			 addPathwayMultiform(pathwayPathwayTablePerspective);
 
 			Set<String> pathViewIDs = ViewManager.get().getRemotePlugInViewIDs(VIEW_TYPE, EEmbeddingID.PATH.id());
 
@@ -273,13 +280,14 @@ public class GLSubGraph extends AGLView implements IMultiTablePerspectiveBasedVi
 						tablePerspectives, pathEventSpace);
 			}
 
-			MultiFormViewSwitchingBar viewSwitchingBar = new MultiFormViewSwitchingBar(pathMultiformRenderer, this);
-			pathColumn.add(viewSwitchingBar);
-			ElementLayout multiformRendererLayout = new ElementLayout();
-			multiformRendererLayout.setRenderer(pathMultiformRenderer);
-			pathColumn.add(multiformRendererLayout);
+			baseContainer.add(new GLElementAdapter(this, pathMultiformRenderer));
+			// MultiFormViewSwitchingBar viewSwitchingBar = new MultiFormViewSwitchingBar(pathMultiformRenderer, this);
+			// pathColumn.add(viewSwitchingBar);
+			// ElementLayout multiformRendererLayout = new ElementLayout();
+			// multiformRendererLayout.setRenderer(pathMultiformRenderer);
+			// pathColumn.add(multiformRendererLayout);
 
-			setDisplayListDirty();
+			// setDisplayListDirty();
 		}
 	}
 
@@ -292,23 +300,21 @@ public class GLSubGraph extends AGLView implements IMultiTablePerspectiveBasedVi
 		// Different renderers should receive path updates from the beginning on, therefore no lazy creation.
 		MultiFormRenderer renderer = new MultiFormRenderer(this, false);
 
-
 		for (String viewID : remoteRenderedPathwayMultiformViewIDs) {
 			renderer.addPluginVisualization(viewID, getViewType(), EEmbeddingID.PATHWAY_MULTIFORM.id(),
 					pathwayTablePerspectives, pathEventSpace);
 		}
-		Row row = new Row();
-		row.setRatioSizeY(0.5f);
-		Column column = new Column();
-		row.add(column);
+
+		GLElementAdapter multiFormRendererAdapter = new GLElementAdapter(this, renderer);
+		pathwayColumn.add(multiFormRendererAdapter);
 
 		MultiFormViewSwitchingBar viewSwitchingBar = new MultiFormViewSwitchingBar(renderer, this);
-		column.add(viewSwitchingBar);
-
-		ElementLayout multiformRendererLayout = new ElementLayout();
-		multiformRendererLayout.setRenderer(renderer);
-		column.add(multiformRendererLayout);
-		pathwayColumn.add(row);
+		// baseContainer.add(new GLElementAdapter(this, viewSwitchingBar));
+		//
+		// ElementLayout multiformRendererLayout = new ElementLayout();
+		// multiformRendererLayout.setRenderer(renderer);
+		// column.add(multiformRendererLayout);
+		// pathwayColumn.add(row);
 
 	}
 
@@ -331,6 +337,12 @@ public class GLSubGraph extends AGLView implements IMultiTablePerspectiveBasedVi
 	@Override
 	public boolean isDataView() {
 		return true;
+	}
+
+	@Override
+	protected GLElement createRoot() {
+
+		return baseContainer;
 	}
 
 }
