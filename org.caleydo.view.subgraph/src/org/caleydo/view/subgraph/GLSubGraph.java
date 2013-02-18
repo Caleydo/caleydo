ed427350011dd4cd3a8b95fb9cb939b34a04b676
Christian Partl <partl@icg.tugraz.at>
2013-02-18 15:55:01 +0000

    worked on configurable path size
    git-svn-id: svn://localhost:8080/entourage/trunk@30 fba478fa-5d3f-d649-a3c9-ad29d1569dfd

--- org.caleydo.view.subgraph/src/org/caleydo/view/subgraph/GLSubGraph.java
+++ org.caleydo.view.subgraph/src/org/caleydo/view/subgraph/GLSubGraph.java
@@ -44,19 +44,9 @@ public class GLSubGraph extends AGLElementGLView implements IMultiTablePerspecti
 
 	private LayoutManager layoutManager;
 
-	// private MultiFormRenderer pathMultiformRenderer = new MultiFormRenderer(this, false);
-
 	private List<TablePerspective> tablePerspectives = new ArrayList<>();
 
 	private Set<String> remoteRenderedPathwayMultiformViewIDs;
-
-	// private Row baseRow;
-
-	// private Column pathwayColumn;
-
-	// private AddTablePerspectivesListener addTablePerspectivesListener;
-
-	// private Column pathColumn;
 
 	private String pathEventSpace;
 
@@ -81,96 +71,9 @@ public class GLSubGraph extends AGLElementGLView implements IMultiTablePerspecti
 
 	@Override
 	public void init(GL2 gl) {
-		// PathwayImporter importer = new GpmlFormat();
-		// try {
-		// Pathway pathway = importer
-		// .doImport(new File(
-		// "D:\\Downloads\\wikipathways_Homo_sapiens_Curation-Tutorial_gpml\\Hs_BMP_signalling_and_regulation_WP1425_44981.gpml"));
-		// List<Xref> refs = pathway.getDataNodeXrefs();
-		// List<PathwayElement> elements = pathway.getDataObjects();
-		// for (PathwayElement element : elements) {
-		// if (element.getObjectType() == ObjectType.LINE) {
-		// System.out.println(pathway.getElementById(element.getStartGraphRef()).getTextLabel() + "->"
-		// + pathway.getElementById(element.getEndGraphRef()).getTextLabel());
-		// }
-		// }
-		// // for (Xref ref : refs) {
-		// // System.out.println("DB: " + ref.getDataSource().getFullName() + ", id: " + ref.getId());
-		// // }
-		// } catch (ConverterException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
 		super.init(gl);
-		// displayListIndex = gl.glGenLists(1);
-		// detailLevel = EDetailLevel.HIGH;
-		// pathMultiformRenderer = new MultiFormRenderer(this, false);
-		//
-		// layoutManager = new LayoutManager(viewFrustum, pixelGLConverter);
-		// baseRow = new Row();
-		//
-		// pathwayColumn = new Column();
-		// pathwayColumn.setRatioSizeX(0.3f);
-		// pathwayColumn.setRatioSizeY(1);
-		// pathwayColumn.setBottomUp(false);
-		//
-		// pathColumn = new Column();
-		// pathColumn.setRatioSizeX(0.7f);
-		// pathColumn.setRatioSizeY(1);
-		//
-		// baseRow.add(pathwayColumn);
-		// baseRow.add(pathColumn);
-		// layoutManager.setBaseElementLayout(baseRow);
-		// layoutManager.setUseDisplayLists(true);
 
 	}
-
-	// @Override
-	// public void initLocal(GL2 gl) {
-	// init(gl);
-	// }
-	//
-	// @Override
-	// public void initRemote(final GL2 gl, final AGLView glParentView, final GLMouseListener glMouseListener) {
-	//
-	// // Register keyboard listener to GL2 canvas
-	// glParentView.getParentComposite().getDisplay().asyncExec(new Runnable() {
-	// @Override
-	// public void run() {
-	// glParentView.getParentComposite().addKeyListener(glKeyListener);
-	// }
-	// });
-	//
-	// this.glMouseListener = glMouseListener;
-	//
-	// init(gl);
-	// }
-
-	// @Override
-	// public void displayLocal(GL2 gl) {
-	// pickingManager.handlePicking(this, gl);
-	// display(gl);
-	// if (busyState != EBusyState.OFF) {
-	// renderBusyMode(gl);
-	// }
-	//
-	// }
-	//
-	// @Override
-	// public void displayRemote(GL2 gl) {
-	// display(gl);
-	// }
-	//
-	// @Override
-	// public void display(GL2 gl) {
-	//
-	// if (isDisplayListDirty) {
-	// layoutManager.updateLayout();
-	// isDisplayListDirty = false;
-	// }
-	// layoutManager.render(gl);
-	// checkForHits(gl);
-	// }
 
 	@Override
 	public ASerializedView getSerializableRepresentation() {
@@ -256,7 +159,7 @@ public class GLSubGraph extends AGLElementGLView implements IMultiTablePerspecti
 			addMultiformRenderer(pathwayTablePerspectives, EEmbeddingID.PATHWAY_MULTIFORM.id(), pathwayColumn);
 
 			pathwayTablePerspective = new PathwayTablePerspective(tablePerspective.getDataDomain(), pathwayDataDomain,
-					newRecordPerspective, newDimensionPerspective, PathwayManager.get().getPathwayByTitle("Glioma",
+					newRecordPerspective, newDimensionPerspective, PathwayManager.get().getPathwayByTitle("Apoptosis",
 							EPathwayDatabaseType.KEGG));
 			pathwayDataDomain.addTablePerspective(pathwayTablePerspective);
 
@@ -288,13 +191,6 @@ public class GLSubGraph extends AGLElementGLView implements IMultiTablePerspecti
 		container.add(viewSwitchingBar);
 
 		parent.add(container);
-
-		// baseContainer.add(new GLElementAdapter(this, viewSwitchingBar));
-		//
-		// ElementLayout multiformRendererLayout = new ElementLayout();
-		// multiformRendererLayout.setRenderer(renderer);
-		// column.add(multiformRendererLayout);
-		// pathwayColumn.add(row);
 
 	}
 
