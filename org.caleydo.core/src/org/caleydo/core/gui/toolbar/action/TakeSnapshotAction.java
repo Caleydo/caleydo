0becac1e15eba1907ea14465e47ba72008fd68ca
Samuel Gratzl <samuel_gratzl@gmx.at>
2013-04-25 09:49:14 +0200

    cleanup take snapshot action for #779

--- org.caleydo.core/src/org/caleydo/core/gui/toolbar/action/TakeSnapshotAction.java
+++ org.caleydo.core/src/org/caleydo/core/gui/toolbar/action/TakeSnapshotAction.java
@@ -27,75 +27,46 @@ import org.caleydo.core.gui.SimpleAction;
 import org.caleydo.core.util.logging.Logger;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.ImageLoader;
 import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
-import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 
 public class TakeSnapshotAction extends SimpleAction {
-
-	public static final String LABEL = "Take snapshot";
-	public static final String ICON = "resources/icons/general/snapshot.png";
-
-	private Composite composite;
-
 	/**
 	 * Constructor without arguments. In this case a snapshot from the whole workbench is made.
 	 */
 	public TakeSnapshotAction() {
-		super(LABEL, ICON);
-	}
-
-	/**
-	 * Constructor that takes a composite which which the screenshot is taken.
-	 */
-	public TakeSnapshotAction(Composite composite) {
-
-		this();
-		this.composite = composite;
+		super("Take snapshot", "resources/icons/general/snapshot.png");
 	}
 
 	@Override
 	public void run() {
 		super.run();
 
-		String path = "screenshot_" + getDateTime() + ".png";
-
 		Display display = PlatformUI.getWorkbench().getDisplay();
 		Shell shell = display.getActiveShell();
-		Rectangle bounds = null;
-		GC gc = null;
-		if (composite == null) {
-			bounds = shell.getBounds();
-			gc = new GC(shell);
-		}
-		else {
-			bounds = composite.getBounds();
-			gc = new GC(composite);
-		}
+
+		String path = getFilePath(shell);
+		if (path == null)
+			return;
+
+		Rectangle bounds = shell.getClientArea();
+		GC gc = new GC(shell);
 
 		final Image image = new Image(display, bounds);
 		gc.copyArea(image, 0, 0);
 		gc.dispose();
 
-		FileDialog saveFileDialog = new FileDialog(shell, SWT.SAVE);
-		saveFileDialog.setFileName(path);
-		path = saveFileDialog.open();
-
 		ImageLoader loader = new ImageLoader();
 		loader.data = new ImageData[] { image.getImageData() };
-
-		// check if file dialog was canceled
-		if (path == null)
-			return;
 
 		loader.save(path, SWT.IMAGE_PNG);
 
@@ -103,14 +74,28 @@ public class TakeSnapshotAction extends SimpleAction {
 
 		Logger.log(new Status(IStatus.INFO, this.toString(), message));
 
-		MessageBox messageBox = new MessageBox(shell, SWT.OK);
-		messageBox.setText("Screenshot");
-		messageBox.setMessage(message);
-		messageBox.open();
+		MessageDialog.openInformation(shell, "Screenshot", message);
 	}
 
-	private String getDateTime() {
-		DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmm");
+	private static String getFilePath(Shell shell) {
+		String path = "screenshot_" + getDateTime() + ".png";
+
+		FileDialog saveFileDialog = new FileDialog(shell, SWT.SAVE);
+		saveFileDialog.setFilterExtensions(new String[] { "*.png" });
+		saveFileDialog.setFileName(path);
+		path = saveFileDialog.open();
+		// check if file dialog was canceled
+		if (path == null)
+			return null;
+
+		if (!path.endsWith(".png"))
+			path += ".png";
+
+		return path;
+	}
+
+	private static String getDateTime() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
 		Date date = new Date();
 		return dateFormat.format(date);
 	}
