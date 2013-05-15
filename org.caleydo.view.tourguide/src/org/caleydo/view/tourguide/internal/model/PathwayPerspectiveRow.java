af30d8942f2451acf1e3c68dadebc81d58b5d493
Samuel Gratzl <samuel_gratzl@gmx.at>
2013-05-15 16:41:21 +0200

    changed pathway perspective row and avoid PathwayRecordPerspective

--- org.caleydo.view.tourguide/src/org/caleydo/view/tourguide/internal/model/PathwayPerspectiveRow.java
+++ org.caleydo.view.tourguide/src/org/caleydo/view/tourguide/internal/model/PathwayPerspectiveRow.java
@@ -19,25 +19,43 @@
  *******************************************************************************/
 package org.caleydo.view.tourguide.internal.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.caleydo.core.data.datadomain.DataDomainManager;
 import org.caleydo.core.data.datadomain.IDataDomain;
 import org.caleydo.core.data.perspective.table.TablePerspective;
-import org.caleydo.core.data.virtualarray.VirtualArray;
 import org.caleydo.core.data.virtualarray.group.Group;
-import org.caleydo.datadomain.pathway.data.PathwayRecordPerspective;
+import org.caleydo.core.id.IDType;
+import org.caleydo.datadomain.pathway.PathwayDataDomain;
+import org.caleydo.datadomain.pathway.data.PathwayTablePerspective;
 import org.caleydo.datadomain.pathway.graph.PathwayGraph;
+import org.caleydo.datadomain.pathway.graph.item.vertex.PathwayVertexRep;
 import org.caleydo.datadomain.pathway.manager.EPathwayDatabaseType;
 
 /**
  * @author Samuel Gratzl
  *
  */
-public final class PathwayPerspectiveRow extends AVirtualArrayScoreRow {
+public final class PathwayPerspectiveRow extends AScoreRow {
 	private final PathwayGraph pathway;
-	private final PathwayRecordPerspective stratification;
+	private final Group group;
 
-	public PathwayPerspectiveRow(PathwayRecordPerspective pathway) {
-		this.pathway = pathway.getPathway();
-		this.stratification = pathway;
+	public PathwayPerspectiveRow(PathwayGraph pathway) {
+		this.pathway = pathway;
+		List<Integer> idsInPathway = new ArrayList<Integer>();
+		for (PathwayVertexRep vertexRep : pathway.vertexSet()) {
+			idsInPathway.add(vertexRep.getID());
+		}
+		this.group = new Group(pathway.vertexSet().size(), 0);
+		this.group.setLabel(pathway.getName(), false);
+		this.group.setStartIndex(0);
+		this.group.setGroupIndex(0);
 	}
 
 	@Override
@@ -67,28 +85,48 @@ public final class PathwayPerspectiveRow extends AVirtualArrayScoreRow {
 	}
 
 	@Override
-	public VirtualArray getVirtualArray() {
-		return stratification.getVirtualArray();
-	}
-
-	@Override
 	public IDataDomain getDataDomain() {
-		return stratification.getDataDomain();
-	}
-	@Override
-	protected boolean isFiltered() {
-		return false;
-	}
-
-	@Override
-	protected boolean filter(Group g) {
-		return true;
+		return DataDomainManager.get().getDataDomainByType(PathwayDataDomain.DATA_DOMAIN_TYPE);
 	}
 
 	@Override
 	public boolean is(TablePerspective tablePerspective) {
-		// TODO Auto-generated method stub
-		return false;
+		return tablePerspective instanceof PathwayTablePerspective
+				&& ((PathwayTablePerspective) tablePerspective).getPathway().equals(pathway);
+	}
+
+	@Override
+	public IDType getIdType() {
+		return PathwayVertexRep.getIdType();
+	}
+
+	@Override
+	public Collection<Group> getGroups() {
+		return Collections.singleton(group);
+	}
+
+	@Override
+	public int getGroupSize() {
+		return 1;
+	}
+
+	@Override
+	public Collection<Integer> of(Group group) {
+		Set<Integer> idsInPathway = new TreeSet<Integer>();
+		for (PathwayVertexRep vertexRep : pathway.vertexSet()) {
+			idsInPathway.add(vertexRep.getID());
+		}
+		return idsInPathway;
+	}
+
+	@Override
+	public int size() {
+		return group.getSize();
+	}
+
+	@Override
+	public Iterator<Integer> iterator() {
+		return of(null).iterator();
 	}
 }
 
