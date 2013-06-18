e66489d2c5050c76c44e4c381e1eed9e42ef7aef
Samuel Gratzl <samuel_gratzl@gmx.at>
2013-06-18 15:10:15 +0200

    fix #1224 selection refactoring

--- org.caleydo.view.info.selection/src/org/caleydo/view/info/selection/RcpSelectionInfoView.java
+++ org.caleydo.view.info.selection/src/org/caleydo/view/info/selection/RcpSelectionInfoView.java
@@ -16,39 +16,43 @@
  *******************************************************************************/
 package org.caleydo.view.info.selection;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.Collections;
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 
 import org.caleydo.core.data.selection.EventBasedSelectionManager;
 import org.caleydo.core.data.selection.IEventBasedSelectionManagerUser;
-import org.caleydo.core.data.selection.SelectionManager;
 import org.caleydo.core.data.selection.SelectionType;
 import org.caleydo.core.id.IDCategory;
-import org.caleydo.core.id.IDMappingManager;
-import org.caleydo.core.id.IDMappingManagerRegistry;
-import org.caleydo.core.id.IDType;
-import org.caleydo.core.util.format.Formatter;
 import org.caleydo.core.view.CaleydoRCPViewPart;
 import org.caleydo.view.info.selection.external.OpenExternalAction;
 import org.caleydo.view.info.selection.external.OpenExternalWrappingAction;
+import org.caleydo.view.info.selection.model.CategoryItem;
+import org.caleydo.view.info.selection.model.ElementItem;
+import org.caleydo.view.info.selection.model.SelectionTypeItem;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IMenuListener;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.StyledCellLabelProvider;
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerCell;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.Tree;
-import org.eclipse.swt.widgets.TreeItem;
 
 /**
  * Search view contains gene and pathway search.
@@ -59,9 +63,8 @@ import org.eclipse.swt.widgets.TreeItem;
 public class RcpSelectionInfoView extends CaleydoRCPViewPart implements IEventBasedSelectionManagerUser {
 	public static final String VIEW_TYPE = "org.caleydo.view.info.selection";
 
-	private final Map<SelectionManager, TreeItem> selectionManagerToSubTree = new HashMap<SelectionManager, TreeItem>();
-
-	private Tree selectionTree;
+	private final NavigableSet<CategoryItem> categories = new TreeSet<>();
+	private TreeViewer selectionTree;
 
 	private OpenExternalWrappingAction openExternalWrapper;
 
@@ -76,18 +79,11 @@ public class RcpSelectionInfoView extends CaleydoRCPViewPart implements IEventBa
 		} catch (JAXBException ex) {
 			throw new RuntimeException("Could not create JAXBContext", ex);
 		}
-	}
-
-	private void createSelectionManagersForIDCategories() {
 
 		for (IDCategory idCategory : IDCategory.getAllRegisteredIDCategories()) {
-
-			if (idCategory.getPrimaryMappingType() == null)
+			if (idCategory.isInternaltCategory() || idCategory.getPrimaryMappingType() == null)
 				continue;
-
-			EventBasedSelectionManager selectionManager = new EventBasedSelectionManager(this,
-					idCategory.getPrimaryMappingType());
-			selectionManager.registerEventListeners();
+			categories.add(new CategoryItem(idCategory, this));
 		}
 	}
 
@@ -97,28 +93,145 @@ public class RcpSelectionInfoView extends CaleydoRCPViewPart implements IEventBa
 		parentComposite = new Composite(parent, SWT.NULL);
 		parentComposite.setLayout(new FillLayout());
 
-		selectionTree = new Tree(parentComposite, SWT.NULL);
+		selectionTree = new TreeViewer(parentComposite, SWT.NONE);
 
 		GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
 		gridData.grabExcessVerticalSpace = true;
 		gridData.minimumHeight = 150;
-		selectionTree.setLayoutData(gridData);
+		selectionTree.getTree().setLayoutData(gridData);
 
-		// lblViewInfoContent = new Label(parent, SWT.WRAP);
-		// lblViewInfoContent.setText("");
-		// gridData = new GridData(GridData.FILL_BOTH);
-		//
-		// gridData.grabExcessVerticalSpace = true;
-		// gridData.minimumHeight = 100;
-		// lblViewInfoContent.setLayoutData(gridData);
-
-		createSelectionManagersForIDCategories();
+		selectionTree.setContentProvider(new SelectionContentProvider());
+		selectionTree.setLabelProvider(new SelectionLabelProvider());
+		selectionTree.setAutoExpandLevel(2);
+		selectionTree.addFilter(new ViewerFilter() {
+			@Override
+			public boolean select(Viewer viewer, Object parentElement, Object element) {
+				if (element instanceof CategoryItem)
+					return ((CategoryItem) element).getTotal() > 0;
+				if (element instanceof SelectionTypeItem)
+					return ((SelectionTypeItem) element).size() > 0;
+				return true;
+			}
+		});
 
 		this.injectExternalFeature();
 
-		parentComposite.layout();
+		selectionTree.setInput(this.categories);
 
 		addToolBarContent();
+	}
+
+	@Override
+	public void registerEventListeners() {
+		for (CategoryItem item : categories)
+			item.getManager().registerEventListeners();
+		super.registerEventListeners();
+	}
+
+	@Override
+	public void unregisterEventListeners() {
+		for (CategoryItem item : categories)
+			item.getManager().unregisterEventListeners();
+		super.unregisterEventListeners();
+	}
+
+	private static class SelectionContentProvider implements ITreeContentProvider {
+
+		@Override
+		public void dispose() {
+
+		}
+
+		@Override
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+
+		}
+
+		@Override
+		public Object[] getElements(Object inputElement) {
+			@SuppressWarnings("unchecked")
+			final NavigableSet<CategoryItem> items = (NavigableSet<CategoryItem>) inputElement;
+			return items.toArray();
+		}
+
+		@Override
+		public Object[] getChildren(Object parentElement) {
+			if (parentElement instanceof CategoryItem)
+				return ((CategoryItem) parentElement).getChildren().toArray();
+			if (parentElement instanceof SelectionTypeItem)
+				return ((SelectionTypeItem) parentElement).getChildren().toArray();
+			if (parentElement instanceof ElementItem)
+				return Collections.emptyList().toArray();
+			return null;
+		}
+
+		@Override
+		public Object getParent(Object element) {
+			if (element instanceof CategoryItem)
+				return null;
+			if (element instanceof SelectionTypeItem)
+				return ((SelectionTypeItem) element).getParent();
+			if (element instanceof ElementItem)
+				return ((ElementItem) element).getParent();
+			return null;
+		}
+
+		@Override
+		public boolean hasChildren(Object element) {
+			if (element instanceof CategoryItem)
+				return !((CategoryItem) element).getChildren().isEmpty();
+			if (element instanceof SelectionTypeItem)
+				return !((SelectionTypeItem) element).isEmpty();
+			if (element instanceof ElementItem)
+				return false;
+			return false;
+		}
+
+	}
+
+	public class SelectionLabelProvider extends StyledCellLabelProvider {
+		@Override
+		protected void measure(Event event, Object element) {
+			super.measure(event, element);
+			// if (element instanceof SelectionTypeItem) {
+			// Point size = event.gc.textExtent("a\nb");
+			// event.height = Math.max(event.height, size.y);
+			// }
+		}
+
+		@Override
+		public void update(ViewerCell cell) {
+			Object element = cell.getElement();
+			StyledString text = new StyledString();
+
+			if (element instanceof CategoryItem) {
+				CategoryItem item = (CategoryItem) element;
+				text.append(item.getLabel());
+				if (item.size() > 0)
+					text.append(String.format(" (%d)", item.size()), StyledString.COUNTER_STYLER);
+				cell.setText(text.toString());
+				cell.setStyleRanges(text.getStyleRanges());
+			} else if (element instanceof SelectionTypeItem) {
+				SelectionTypeItem item = (SelectionTypeItem) element;
+				cell.setBackground(item.getColor());
+				text.append(item.getLabel());
+				int base = item.getParent().size();
+				if (base > 0) {
+					text.append(
+							String.format(" %d (%.1f%%) ", item.size(), item.size() * 100.f / item.getParent().size()),
+							StyledString.COUNTER_STYLER);
+				} else {
+					text.append(String.format(" %d ", item.size()), StyledString.COUNTER_STYLER);
+				}
+				text.append(item.getPreview(), StyledString.QUALIFIER_STYLER);
+				cell.setText(text.toString());
+				cell.setStyleRanges(text.getStyleRanges());
+			} else if (element instanceof ElementItem) {
+				ElementItem item = (ElementItem) element;
+				cell.setText(item.getLabel());
+			}
+			super.update(cell);
+		}
 	}
 
 	private void injectExternalFeature() {
@@ -128,33 +241,39 @@ public class RcpSelectionInfoView extends CaleydoRCPViewPart implements IEventBa
 		menuMgr.addMenuListener(new IMenuListener() {
 			@Override
 			public void menuAboutToShow(IMenuManager manager) {
-				IAction action = openExternalWrapper.getWrappee();
-				if (action == null)
+				Object element = ((StructuredSelection) selectionTree.getSelection()).getFirstElement();
+				if (element == null)
 					return;
-				manager.add(action);
+				if (element instanceof CategoryItem) {
+					// CategoryItem item = (CategoryItem)element;
+				} else if (element instanceof SelectionTypeItem) {
+					SelectionTypeItem item = (SelectionTypeItem) element;
+					if (!SelectionType.isDefaultType(item.getSelectionType())) {
+						manager.add(new RemoveSelectionTypeAction(item.getSelectionType()));
+					}
+				} else if (element instanceof ElementItem) {
+					IAction action = openExternalWrapper.getWrappee();
+					if (action == null)
+						return;
+					manager.add(action);
+				}
+
 			}
 		});
-		Menu menu = menuMgr.createContextMenu(selectionTree);
-		selectionTree.setMenu(menu);
+		Menu menu = menuMgr.createContextMenu(selectionTree.getTree());
+		selectionTree.getTree().setMenu(menu);
 
-		selectionTree.addSelectionListener(new SelectionAdapter() {
+		selectionTree.addSelectionChangedListener(new ISelectionChangedListener() {
 			@Override
-			public void widgetSelected(SelectionEvent e) {
-				IAction action = createOpenExternalFor(selectionTree.getSelection());
-				openExternalWrapper.setWrappee(action);
+			public void selectionChanged(SelectionChangedEvent event) {
+				Object element = ((StructuredSelection) event.getSelection()).getFirstElement();
+				if (element instanceof ElementItem) {
+					ElementItem item = (ElementItem) element;
+					IAction action = OpenExternalAction.create(item.getIDType(), item.getId());
+					openExternalWrapper.setWrappee(action);
+				}
 			}
 		});
-	}
-
-	private IAction createOpenExternalFor(TreeItem[] treeItems) {
-		if (treeItems.length == 0)
-			return null;
-		TreeItem treeItem = treeItems[0];
-
-		IDType type = (IDType) treeItem.getData("idType");
-		if (type == null)
-			return null;
-		return OpenExternalAction.create(type, treeItem.getData());
 	}
 
 	@Override
@@ -170,86 +289,22 @@ public class RcpSelectionInfoView extends CaleydoRCPViewPart implements IEventBa
 		determineDataConfiguration(serializedView, false);
 	}
 
-	private void updateSubTree(final SelectionManager selectionManager) {
-
-		if (parentComposite.isDisposed())
-			return;
-
+	@Override
+	public synchronized void notifyOfSelectionChange(final EventBasedSelectionManager selectionManager) {
 		parentComposite.getDisplay().asyncExec(new Runnable() {
 			@Override
 			public void run() {
-
-				IDType idType = selectionManager.getIDType();
-				IDCategory idCategory = idType.getIDCategory();
-
-				if (idType.getIDCategory().getPrimaryMappingType() == null)
-					return;
-
-				Set<Integer> mouseOverIDs = selectionManager.getElements(SelectionType.MOUSE_OVER);
-				Set<Integer> selectedIDs = selectionManager.getElements(SelectionType.SELECTION);
-
-				if (!selectionManagerToSubTree.containsKey(selectionManager)
-						&& (mouseOverIDs.size() > 0 || selectedIDs.size() > 0)) {
-
-					TreeItem idCategorySubTree = new TreeItem(selectionTree, SWT.NONE);
-					idCategorySubTree.setExpanded(true);
-					idCategorySubTree.setData(-1);
-					idCategorySubTree.setText(idCategory.getDenominationPlural());
-
-					selectionManagerToSubTree.put(selectionManager, idCategorySubTree);
-				}
-
-				TreeItem subTree = selectionManagerToSubTree.get(selectionManager);
-
-				if (subTree == null)
-					return;
-
-				// Flush old items from this selection type
-				for (TreeItem item : subTree.getItems()) {
-					item.dispose();
-				}
-				subTree.clearAll(true);
-
-				createItems(subTree, SelectionType.MOUSE_OVER, mouseOverIDs, idType);
-				createItems(subTree, SelectionType.SELECTION, selectedIDs, idType);
-
-				int numberElements = IDMappingManagerRegistry.get().getIDMappingManager(idType).getPrimaryTypeCounter();
-				int selectedRecords = selectionManager.getNumberOfElements(SelectionType.SELECTION);
-				float selectedRecordsPercentage = selectedRecords / (float) numberElements * 100f;
-
-				if (numberElements != 0) {
-					subTree.setText(idType.getIDCategory().getDenominationPlural() + " - " + selectedRecords + " of "
-							+ numberElements + " (" + Formatter.formatNumber(selectedRecordsPercentage) + "%)");
-				}
+				IDCategory target = selectionManager.getIDType().getIDCategory();
+				for (CategoryItem item : categories)
+					if (item.getCategory() == target) {
+						item.update();
+						// selectionTree.setHasChildren(item, item.getTotal() > 0);
+						break;
+					}
+				// selectionTree.setInput(categories);
+				selectionTree.refresh();
+				selectionTree.expandToLevel(2);
 			}
 		});
-	}
-
-	private synchronized void createItems(TreeItem tree, SelectionType selectionType, Set<Integer> IDs, IDType idType) {
-		Color color = selectionType.getColor().getSWTColor(parentComposite.getDisplay());
-
-		IDMappingManager idMappingManager = IDMappingManagerRegistry.get().getIDMappingManager(idType);
-
-		for (Integer id : IDs) {
-			String humanReadableID = "<unresolved>";
-			Set<Object> resolvedIDs = idMappingManager.getIDAsSet(idType, idType.getIDCategory()
-					.getHumanReadableIDType(), id);
-			if (resolvedIDs != null && resolvedIDs.size() > 0)
-				humanReadableID = (String) resolvedIDs.toArray()[0];
-
-			TreeItem item = new TreeItem(tree, SWT.NONE);
-			item.setText(humanReadableID + "");
-			item.setBackground(color);
-			item.setData(id);
-			item.setData("idType", idType);
-			item.setData("selection_type", selectionType);
-		}
-
-		tree.setExpanded(true);
-	}
-
-	@Override
-	public synchronized void notifyOfSelectionChange(EventBasedSelectionManager selectionManager) {
-		updateSubTree(selectionManager);
 	}
 }
