b5959b29ea2b8cb4c4e6b7d23f7dc63379a44f16
Samuel Gratzl <samuel_gratzl@gmx.at>
2013-06-24 17:27:46 +0200

    #1231 more space

--- org.caleydo.view.tourguide/src/org/caleydo/view/tourguide/internal/view/GLTourGuideView.java
+++ org.caleydo.view.tourguide/src/org/caleydo/view/tourguide/internal/view/GLTourGuideView.java
@@ -711,7 +711,7 @@ public class GLTourGuideView extends AGLElementView {
 		@Override
 		public void renderHeaderBackground(GLGraphics g, float w, float h, float labelHeight, ARankColumnModel model) {
 			g.color(0.96f).fillRect(0, 0, w, h);
-			g.color(model.getBgColor()).fillRect(0, labelHeight - 3, w, 2);
+			g.color(model.getBgColor()).fillRect(0, labelHeight - 2, w, 2);
 		}
 
 		@Override
