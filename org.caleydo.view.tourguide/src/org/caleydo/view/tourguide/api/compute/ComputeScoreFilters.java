d04dd41e0ff6d5652903e3f0fe2981d8e7acb5d1
Marc Streit <marc.streit@jku.at>
2013-07-01 21:02:35 +0200

    Updated license header

--- org.caleydo.view.tourguide/src/org/caleydo/view/tourguide/api/compute/ComputeScoreFilters.java
+++ org.caleydo.view.tourguide/src/org/caleydo/view/tourguide/api/compute/ComputeScoreFilters.java
@@ -1,3 +1,8 @@
+/*******************************************************************************
+ * Caleydo - Visualization for Molecular Biology - http://caleydo.org
+ * Copyright (c) The Caleydo Team. All rights reserved.
+ * Licensed under the new BSD license, available at http://caleydo.org/license
+ ******************************************************************************/
 package org.caleydo.view.tourguide.api.compute;
 
 import java.util.Objects;
--- org.caleydo.view.tourguide/src/org/caleydo/view/tourguide/impl/algorithm/MutualExclusiveScoreFilter.java
+++ org.caleydo.view.tourguide/src/org/caleydo/view/tourguide/impl/algorithm/MutualExclusiveScoreFilter.java
@@ -1,22 +1,8 @@
 /*******************************************************************************
- * Caleydo - visualization for molecular biology - http://caleydo.org
- *
- * Copyright(C) 2005, 2012 Graz University of Technology, Marc Streit, Alexander
- * Lex, Christian Partl, Johannes Kepler University Linz </p>
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>
- *******************************************************************************/
+ * Caleydo - Visualization for Molecular Biology - http://caleydo.org
+ * Copyright (c) The Caleydo Team. All rights reserved.
+ * Licensed under the new BSD license, available at http://caleydo.org/license
+ ******************************************************************************/
 package org.caleydo.view.tourguide.impl.algorithm;
 
 import java.util.List;
