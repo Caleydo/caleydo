5b34b79b070eb8ab4cae9c2788c3175f08cc4493
Alexander Lex <alex@seas.harvard.edu>
2013-07-02 19:26:12 -0400

    Example for transparent drawing in splash Changed background of lables to dark-gray from red

--- org.caleydo.core/src/org/caleydo/core/internal/InteractiveSplashHandler.java
+++ org.caleydo.core/src/org/caleydo/core/internal/InteractiveSplashHandler.java
@@ -8,6 +8,7 @@ package org.caleydo.core.internal;
 import org.caleydo.core.manager.GeneralManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.ProgressBar;
@@ -16,7 +17,7 @@ import org.eclipse.ui.splash.AbstractSplashHandler;
 
 /**
  * Caleydo splash screen with integrated progress bar.
- * 
+ *
  * @author Marc Streit
  */
 public class InteractiveSplashHandler extends AbstractSplashHandler {
@@ -34,28 +35,31 @@ public class InteractiveSplashHandler extends AbstractSplashHandler {
 	private void createUI() {
 		Shell splash = getSplash();
 
-		ProgressBar progressBar = new ProgressBar(splash, SWT.SMOOTH | SWT.BORDER);
+		ProgressBar progressBar = new ProgressBar(splash, SWT.NONE);
 		progressBar.setBounds(20, 200, splash.getSize().x - 40, 25);
 
 		splash.setBackgroundMode(SWT.INHERIT_DEFAULT);
 
+
 		Label progressMessageLabel = new Label(splash, SWT.NONE);
 		progressMessageLabel.setText(" Loading...");
 		final Display display = splash.getDisplay();
-		progressMessageLabel
-				.setFont(new Font(display, "Arial", 10, SWT.NONE));
+		GC gc = new GC(splash);
+		gc.drawString("TEST", 50, 50, true);
+		progressMessageLabel.setFont(new Font(display, "Arial", 10, SWT.NONE));
 		progressMessageLabel.setBounds(20, 230, splash.getSize().x - 40, 25);
-		progressMessageLabel.setForeground(display.getSystemColor(
-				SWT.COLOR_WHITE));
+		progressMessageLabel.setForeground(display.getSystemColor(SWT.COLOR_WHITE));
+		progressMessageLabel.setBackground(display.getSystemColor(SWT.COLOR_DARK_GRAY));
+
 
 		Label versionLabel = new Label(splash, SWT.NONE);
 		versionLabel.setText(" Version " + Activator.version);
 		versionLabel.setFont(new Font(display, "Arial", 9, SWT.NONE));
 		versionLabel.setBounds(336, 185, 150, 20);
 		versionLabel.setForeground(display.getSystemColor(SWT.COLOR_WHITE));
+		versionLabel.setBackground(display.getSystemColor(SWT.COLOR_DARK_GRAY));
 
-		GeneralManager.get().getSWTGUIManager()
-				.setExternalProgressBarAndLabel(progressBar, progressMessageLabel);
+		GeneralManager.get().getSWTGUIManager().setExternalProgressBarAndLabel(progressBar, progressMessageLabel);
 
 		splash.setText("Loading Caleydo...");
 	}
