2242079adf9d23f9d3f5a9004e2aa5c728e53474
Samuel Gratzl <samuel_gratzl@gmx.at>
2013-07-04 11:40:14 +0200

    simplify splash handler

--- org.caleydo.core/src/org/caleydo/core/startup/InteractiveSplashHandler.java
+++ org.caleydo.core/src/org/caleydo/core/startup/InteractiveSplashHandler.java
@@ -6,21 +6,35 @@
 package org.caleydo.core.startup;
 
 import org.caleydo.core.manager.GeneralManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.internal.splash.EclipseSplashHandler;
+import org.eclipse.ui.splash.BasicSplashHandler;
 
 /**
  * Caleydo splash screen with integrated progress bar.
  *
  * @author Marc Streit
  */
-@SuppressWarnings("restriction")
-public class InteractiveSplashHandler extends EclipseSplashHandler {
+public class InteractiveSplashHandler extends BasicSplashHandler {
 
 	@Override
 	public void init(Shell splash) {
 		super.init(splash);
 
+		setProgressRect(new Rectangle(20, 200, splash.getSize().x-40, 25));
+		setMessageRect(new Rectangle(20, 230,splash.getSize().x-40, 25));
+		setForeground(new RGB(255, 255, 255));
+
+		Label idLabel = new Label(getContent(), SWT.LEFT);
+		idLabel.setForeground(getForeground());
+		idLabel.setBounds(new Rectangle(splash.getSize().x-80, 185, 80, 18));
+		idLabel.setText("Version " + GeneralManager.VERSION);
+
+
+		// public the progress monitor
 		GeneralManager.get().setSplashProgressMonitor(this.getBundleProgressMonitor());
 	}
 }
