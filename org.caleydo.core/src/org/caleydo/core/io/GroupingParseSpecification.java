1e408aad264e4c12a7f116c1f10f226b8b464cae
Alexander Lex <alex@seas.harvard.edu>
2013-08-12 16:22:59 -0400

    Re-adding groupingName stuff _again_

--- org.caleydo.core/src/org/caleydo/core/io/GroupingParseSpecification.java
+++ org.caleydo.core/src/org/caleydo/core/io/GroupingParseSpecification.java
@@ -15,14 +15,12 @@ import org.caleydo.core.io.parser.ascii.GroupingParser;
 
 /**
  * <p>
- * Specifies how to parse a grouping file to ultimately create {@link GroupList}
- * The parsing according to the rules specified here is done in
- * {@link GroupingParser}.
+ * Specifies how to parse a grouping file to ultimately create {@link GroupList} The parsing according to the rules
+ * specified here is done in {@link GroupingParser}.
  * </p>
  * <p>
- * The data is assumed to be available in a delimited text file and follow the
- * contract specified in the base class {@link MatrixDefinition}. Here is an
- * example for a valid grouping file, with a header line:
+ * The data is assumed to be available in a delimited text file and follow the contract specified in the base class
+ * {@link MatrixDefinition}. Here is an example for a valid grouping file, with a header line:
  *
  * <pre>
  * {@code
@@ -38,57 +36,54 @@ import org.caleydo.core.io.parser.ascii.GroupingParser;
  * Groupings are based on the following assumptions:
  * </p>
  * <ul>
- * <li>One column of the source file contains an identifier of the ID to be
- * grouped (the first column in the above example).</li>
- * <li>Other columns contain a group association for every line. Group
- * associations are arbitrary strings (except for the delimiter).</li>
- * <li>If the group association for two IDs are identical (according to
- * {@link String#equals(Object)}), they belong to the same group</li>
+ * <li>One column of the source file contains an identifier of the ID to be grouped (the first column in the above
+ * example).</li>
+ * <li>Other columns contain a group association for every line. Group associations are arbitrary strings (except for
+ * the delimiter).</li>
+ * <li>If the group association for two IDs are identical (according to {@link String#equals(Object)}), they belong to
+ * the same group</li>
  * </ul>
  *
  * <p>
- * By default, it is assumed that the first column contains the row IDs and all
- * other columns contain groupings. If one of these assumptions does not hold,
- * both, the exact column of the ids ({@link #setColumnOfRowIds(Integer)}) and
- * the exact columns of the groupings ({@link #setColumns(ArrayList)} or
- * {@link #addColum(Integer)}) can be specified. Note that if you are not using
- * the default, you need to specify <b>both, the column of the ids and the ids
- * of all groupings.</b>
+ * By default, it is assumed that the first column contains the row IDs and all other columns contain groupings. If one
+ * of these assumptions does not hold, both, the exact column of the ids ({@link #setColumnOfRowIds(Integer)}) and the
+ * exact columns of the groupings ({@link #setColumns(ArrayList)} or {@link #addColum(Integer)}) can be specified. Note
+ * that if you are not using the default, you need to specify <b>both, the column of the ids and the ids of all
+ * groupings.</b>
  * </p>
  *
  * <p>
- * If the file contains multiple columns, all of them are loaded as a separate
- * grouping. This can be overridden to specific columns using the
- * {@link #columns} parameter.
+ * If the file contains multiple columns, all of them are loaded as a separate grouping. This can be overridden to
+ * specific columns using the {@link #columns} parameter.
  * </p>
  *
  * <p>
- * By default, it is assumed that the groupings contain a header inkluding a
- * name for the group (see the {@link MatrixDefinition} for details. This can be
- * changed by setting {@link #setContainsColumnIDs(boolean)} to false. In this
- * case the group names area automatically generated based on the number of
- * groups.
+ * By default, it is assumed that the groupings contain a header inkluding a name for the group (see the
+ * {@link MatrixDefinition} for details. This can be changed by setting {@link #setContainsColumnIDs(boolean)} to false.
+ * In this case the group names area automatically generated based on the number of groups.
  * </p>
  *
  * <p>
- * In case of automatically created grouping names a global name can be
- * specified using
+ * In case of automatically created grouping names a global name can be specified using
  * </p>
  *
  * @author Alexander Lex
  *
  */
 @XmlType
-public class GroupingParseSpecification
-	extends MatrixDefinition {
+public class GroupingParseSpecification extends MatrixDefinition {
 
 	/**
-	 * Optional parameter to specify the columns of the grouping in the
-	 * delimited file. If no value is set, all columns are loaded.
+	 * Optional parameter to specify the columns of the grouping in the delimited file. If no value is set, all columns
+	 * are loaded.
 	 */
 	private ArrayList<Integer> columns;
 
-
+	/**
+	 * Optional summary name for all groupings in a grouping file. If multiple groupings exist all groupings will have
+	 * the same name. Only used if {@link #groupingNames} is not null.
+	 */
+	private String groupingName;
 
 	/**
 	 * Optional name for all groupings in the specification. Only has an effect if {@link #isContainsColumnIDs()} is
@@ -112,7 +107,8 @@ public class GroupingParseSpecification
 	/**
 	 * Sets the columns to be parsed. Overwrites everything previously set
 	 *
-	 * @param columns setter, see {@link #columns}
+	 * @param columns
+	 *            setter, see {@link #columns}
 	 */
 	public void setColumns(ArrayList<Integer> columns) {
 		this.columns = columns;
@@ -146,6 +142,21 @@ public class GroupingParseSpecification
 	 */
 	public List<String> getGroupingNames() {
 		return groupingNames;
+	}
+
+	/**
+	 * @param groupingName
+	 *            setter, see {@link groupingName}
+	 */
+	public void setGroupingName(String groupingName) {
+		this.groupingName = groupingName;
+	}
+
+	/**
+	 * @return the groupingName, see {@link #groupingName}
+	 */
+	public String getGroupingName() {
+		return groupingName;
 	}
 
 }
