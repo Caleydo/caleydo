89e321e3969966a1fbf451b3ed9bac77a57b84cc
Samuel Gratzl <samuel_gratzl@gmx.at>
2013-09-09 17:21:11 +0200

    bugfixing

--- org.caleydo.data.importer/src/org/caleydo/data/importer/tcga/regular/TCGASettings.java
+++ org.caleydo.data.importer/src/org/caleydo/data/importer/tcga/regular/TCGASettings.java
@@ -25,6 +25,6 @@ public class TCGASettings extends Settings {
 	}
 
 	public boolean isSampleGenes() {
-		return !fullGenes || Boolean.parseBoolean(sampleGenes);
+		return Boolean.parseBoolean(sampleGenes) && !fullGenes;
 	}
 }
