99af1f69e2de1f9df86c821f7fb64e10408bd510
Samuel Gratzl <samuel_gratzl@gmx.at>
2013-10-03 10:51:52 +0200

    fix #1666 color animation preference

---
+++ org.caleydo.vis.lineup/src/main/java/org/caleydo/vis/lineup/internal/Activator.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Caleydo - Visualization for Molecular Biology - http://caleydo.org
+ * Copyright (c) The Caleydo Team. All rights reserved.
+ * Licensed under the new BSD license, available at http://caleydo.org/license
+ ******************************************************************************/
+package org.caleydo.vis.lineup.internal;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ *
+ * @author Samuel Gratzl
+ *
+ */
+public class Activator extends AbstractUIPlugin {
+	public static final String ID = "org.caleydo.vis.lineup";
+
+	// The shared instance
+	private static Activator plugin;
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return AbstractUIPlugin.imageDescriptorFromPlugin(ID, path);
+	}
+
+}
