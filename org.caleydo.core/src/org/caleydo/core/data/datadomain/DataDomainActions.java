8a635a39e8f845057b4b45a150b357bb4ce723bb
Samuel Gratzl <samuel_gratzl@gmx.at>
2014-03-17 10:42:21 +0100

    add TypedIDActions similar to TablePerspective and DataDomainActions

--- org.caleydo.core/src/org/caleydo/core/data/datadomain/DataDomainActions.java
+++ org.caleydo.core/src/org/caleydo/core/data/datadomain/DataDomainActions.java
@@ -12,6 +12,11 @@ import org.caleydo.core.util.ExtensionUtils;
 import org.caleydo.core.util.collection.Pair;
 import org.caleydo.core.view.contextmenu.ActionBasedContextMenuItem;
 import org.caleydo.core.view.contextmenu.ContextMenuCreator;
+import org.caleydo.core.view.contextmenu.GroupContextMenuItem;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
 
 /**
  * common actions for a data domain, that can be performed
@@ -34,25 +39,97 @@ public class DataDomainActions {
 	}
 
 	public static boolean add(ContextMenuCreator creator, IDataDomain dataDomain, Object sender, boolean separate) {
+		return add(creator, map(dataDomain, sender), separate);
+	}
+
+	private static Iterable<Collection<Pair<String, Runnable>>> map(IDataDomain dataDomain, Object sender) {
+		Collection<Collection<Pair<String, Runnable>>> r = new ArrayList<>(factories.size());
+		for (IDataDomainActionFactory factory : factories) {
+			r.add(factory.create(dataDomain, sender));
+		}
+		return r;
+	}
+
+	public interface IDataDomainActionFactory {
+		public Collection<Pair<String, Runnable>> create(IDataDomain dataDomain, Object sender);
+	}
+
+	static boolean add(ContextMenuCreator creator, Iterable<Collection<Pair<String, Runnable>>> items, boolean separate) {
 		boolean first = separate;
 		boolean added = false;
-		for (IDataDomainActionFactory factory : factories) {
-			Collection<Pair<String, Runnable>> create = factory.create(dataDomain, sender);
-			if (create.isEmpty())
+		for (Collection<Pair<String, Runnable>> create : items) {
+			if (create == null || create.isEmpty())
 				continue;
 			if (!first) {
 				creator.addSeparator();
 			}
 			first = false;
 			added = true;
-			for (Pair<String, Runnable> p : create)
+			for (Pair<String, Runnable> p : create) {
+				if (p.getSecond() instanceof ChooserRunnable) {
+					GroupContextMenuItem group = new GroupContextMenuItem(p.getFirst());
+					addAll(group, (ChooserRunnable) p.getSecond());
+					creator.add(group);
+				}
 				creator.add(new ActionBasedContextMenuItem(p.getFirst(), p.getSecond()));
+			}
 		}
 		return added;
 	}
 
-	public interface IDataDomainActionFactory {
-		public Collection<Pair<String, Runnable>> create(IDataDomain dataDomain, Object sender);
+	private static void addAll(GroupContextMenuItem creator, ChooserRunnable items) {
+		for (Pair<String, Runnable> p : items) {
+			if (p.getSecond() instanceof ChooserRunnable) {
+				GroupContextMenuItem group = new GroupContextMenuItem(p.getFirst());
+				addAll(group, (ChooserRunnable) p.getSecond());
+				creator.add(group);
+			}
+			creator.add(new ActionBasedContextMenuItem(p.getFirst(), p.getSecond()));
+		}
+	}
+
+	/**
+	 * a special runnable with included nesting
+	 * 
+	 * @author Samuel Gratzl
+	 * 
+	 */
+	public final static class ChooserRunnable extends ArrayList<Pair<String, Runnable>> implements Runnable {
+		private static final long serialVersionUID = -3564385524119059911L;
+
+		@Override
+		public void run() {
+			Display d = Display.getDefault();
+			// show a selection dialog
+			if (d.getThread() == Thread.currentThread())
+				showSelectionBox();
+			else
+				d.asyncExec(this);
+		}
+
+		private void showSelectionBox() {
+			ElementListSelectionDialog dialog = new ElementListSelectionDialog(null, new LabelProvider() {
+				@Override
+				public String getText(Object element) {
+					return ((Pair<?, ?>) element).getFirst().toString();
+				}
+			});
+			dialog.setMultipleSelection(false);
+			dialog.setElements(toArray());
+			dialog.setTitle("Select an action");
+			// user pressed cancel
+			if (dialog.open() != Window.OK) {
+				return;
+			}
+			Object[] result = dialog.getResult();
+			if (result.length == 0)
+				return;
+			for (Object r : result) {
+				@SuppressWarnings("unchecked")
+				Pair<String, Runnable> p = (Pair<String, Runnable>) r;
+				p.getSecond().run();
+			}
+		}
 	}
 
 }
