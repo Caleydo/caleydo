ffab46d7c90bad70566eac4af2bf5ae8ed17f0ca
Christian Partl <partl@icg.tugraz.at>
2014-08-08 13:02:58 +0200

    numerical summary renderers now indicate classification for fishers test (#1769)

--- org.caleydo.view.enroute/src/org/caleydo/view/enroute/mappeddataview/AColumnBasedDataRenderer.java
+++ org.caleydo.view.enroute/src/org/caleydo/view/enroute/mappeddataview/AColumnBasedDataRenderer.java
@@ -88,9 +88,7 @@ public abstract class AColumnBasedDataRenderer extends ADataRenderer {
 								contentRenderer.resolvedRowID, contentRenderer.resolvedColumnIDType, columnID);
 						SimpleCategory category = classifier.apply(rawValue);
 						if (category != null) {
-							renderColorColumn(gl,
-									new Color(category.color.r, category.color.g, category.color.b, 0.6f), xIncrement,
-									y);
+							renderColorColumn(gl, category.color.transparentCopy(0.6f), xIncrement, y);
 						}
 					}
 				}
